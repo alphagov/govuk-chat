1846a51a250195896a2f7dba1366439b076fd2ee
davidgisbey <david.gisbey@digital.cabinet-office.gov.uk>
2024-03-01 10:06:42 +0000

    Run rubocop -A

--- config/puma.rb
+++ config/puma.rb
@@ -1,2 +1,4 @@
+# frozen_string_literal: true
+
 require "govuk_app_config/govuk_puma"
 GovukPuma.configure_rails(self)
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,5 @@
+# frozen_string_literal: true
+
 Rails.application.routes.draw do
   get "/healthcheck/live", to: proc { [200, {}, %w[OK]] }
   get "/healthcheck/ready", to: GovukHealthcheck.rack_response
