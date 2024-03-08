a6ba37292cbe77a0a8e94f16acf686978bc49cde
Pete Goddard <pete@artofsoftware.co.uk>
2024-03-08 11:05:27 +0000

    Add login_anon filter to ApplicationController
    this sets session["user] and Current.user
    
    This means you can become a known AnonymousUser (to match actors configured in
    Flipper UI) by adding &user=with-open-ai.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,24 @@
-# frozen_string_literal: true
-
 class ApplicationController < ActionController::Base
+  before_action :login_anon
+
+private
+
+  def login_anon
+    if session["user_id"].present? && user_param.nil?
+      Current.user = AnonymousUser.new(session["user_id"])
+      return
+    end
+
+    login_user(user_param || SecureRandom.uuid)
+  end
+
+  def login_user(user_id)
+    session["user_id"] = user_id
+
+    Current.user = AnonymousUser.new(user_id)
+  end
+
+  def user_param
+    params.permit[:user]
+  end
 end
