e091ee13422815edd97a785be092d74334ccd80f
Pete Goddard <peter.goddard@digital.cabinet-office.gov.uk>
2024-03-21 08:49:22 +0000

    Add quick and dirty retriever for search-api (v1)
    So we can see how the retrieval hooks up to the RAG system,
    I've added a quick retreiver to query the search API so we have something to
    work with.

---
+++ lib/retrieval/search_api_v1_retriever.rb
@@ -0,0 +1,32 @@
+module Retrieval
+  class SearchApiV1Retriever
+    def self.call(...) = new(...).call
+
+    def initialize(query:)
+      @query = query
+      @search_api = GdsApi::Search.new("https://www.gov.uk/api")
+    end
+
+    def call
+      search_api.search(
+        q: query, fields: %w[description title indexable_content link],
+      )["results"].map(&method(:format_result))
+    end
+
+  private
+
+    attr_reader :query, :search_api
+
+    def format_result(result)
+      <<~OUTPUT
+        Title: #{result['title']}
+
+        Description: #{result['description']}
+
+        Uri: https://www.gov.uk#{result['link']}
+
+        Content: #{result['indexable_content']}
+      OUTPUT
+    end
+  end
+end
