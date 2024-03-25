545ab99e6aba5151d30afd4a55343699225ce2da
davidgisbey <david.gisbey@digital.cabinet-office.gov.uk>
2024-03-25 10:48:55 +0000

    Remove sidekiq inline config
    Now we're using using sidekiq.fake! for the system specs, we no longer
    need config for sikdekiq inline.

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -30,12 +30,4 @@ RSpec.configure do |config|
   config.before(:each, type: :system, js: true) do
     driven_by :selenium_chrome_headless
   end
-
-  config.around do |example|
-    if example.metadata[:sidekiq_inline] == true
-      Sidekiq::Testing.inline! { example.run }
-    else
-      example.run
-    end
-  end
 end
