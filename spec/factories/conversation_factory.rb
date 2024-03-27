ff36698550bba7872441aa4cbd187b986e4ce47e
Pete Goddard <peter.goddard@digital.cabinet-office.gov.uk>
2024-03-27 15:25:39 +0000

    Clean up redundant ENV and factory

--- spec/factories/conversation_factory.rb
+++ spec/factories/conversation_factory.rb
@@ -1,13 +1,4 @@
 FactoryBot.define do
   factory :conversation do
-    trait :with_history do
-      after(:create) do |conversation|
-        { "Hello" => "How can I help?", "Pay my tax" => "Which type of tax?" }.each do |q, a|
-          question = create(:question, conversation:, message: q)
-          create(:answer, question:, message: a)
-        end
-        create(:question, conversation:, message: "self assessment")
-      end
-    end
   end
 end
