23b1e79de7d766444f59ae97033284b4c922bf58
Pete Goddard <peter.goddard@digital.cabinet-office.gov.uk>
2024-03-28 13:40:13 +0000

    Add column for answer_strategy to questions

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2024_04_02_082414) do
+ActiveRecord::Schema[7.1].define(version: 2024_03_28_132338) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "pgcrypto"
   enable_extension "plpgsql"
@@ -63,6 +63,7 @@ ActiveRecord::Schema[7.1].define(version: 2024_04_02_082414) do
     t.string "message", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "answer_strategy", null: false
     t.index ["conversation_id"], name: "index_questions_on_conversation_id"
     t.index ["created_at"], name: "index_questions_on_created_at"
   end
