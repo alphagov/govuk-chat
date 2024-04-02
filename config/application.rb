65e1a6d7c62ebf5208a34ced3c401161e2897a91
Kevin Dew <kevindew@me.com>
2024-04-02 08:59:10 +0100

    Set time zone to London
    This is a GOV.UK convention as per: https://docs.publishing.service.gov.uk/manual/conventions-for-rails-applications.html#specify-london-as-the-timezone

--- config/application.rb
+++ config/application.rb
@@ -37,6 +37,7 @@ module GovukChat
     #
     # config.time_zone = "Central Time (US & Canada)"
     # config.eager_load_paths << Rails.root.join("extras")
+    config.time_zone = "London"
 
     # Don't generate system test files.
     config.generators.system_tests = nil
