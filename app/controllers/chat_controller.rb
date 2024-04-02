6d5e7afde88a0c182eaebcd23eb307f2e0f12caf
Alex Avlonitis <alex.avlonitis@digital.cabinet-office.gov.uk>
2024-04-02 15:11:50 +0100

    Add referrer redirection logic
    - If a user tries to access a restricted page before accepting the terms, a request referrer header will be set with the original requested url, before the redirection to the /chat/onboarding page. Once they have accepted the terms, the will be redirected back to the referrer url set previously, and at the same time the referrer will be deleted.
    - External malicious hosts for open redirect attacks will be blocked, Rails < 7.0 have that security option enabled by default: https://edgeguides.rubyonrails.org/configuring.html#config-action-controller-raise-on-open-redirects
    - We truncate the url referrer to 255 characters to avoid session overflow, either with malicious intent or by accident (analytics query strings, etc..)

--- app/controllers/chat_controller.rb
+++ app/controllers/chat_controller.rb
@@ -1,4 +1,9 @@
 class ChatController < ApplicationController
+  rescue_from ActionController::Redirecting::UnsafeRedirectError do |e|
+    logger.error("Unsuccessful unsafe redirect: #{e.message}")
+    redirect_to chat_path
+  end
+
   def index
     expires_in(5.minutes, public: true) unless Rails.env.development?
   end
@@ -7,7 +12,6 @@ class ChatController < ApplicationController
     @form = Form::ConfirmUnderstandRisk.new
   end
 
-  # TODO: redirect to the intially requested page - referrer
   def onboarding_confirm
     @form = Form::ConfirmUnderstandRisk.new(
       confirmation: params.dig("confirm_understand_risk", "confirmation"),
@@ -15,7 +19,7 @@ class ChatController < ApplicationController
 
     if @form.valid?
       session[:chat_risks_understood] = true
-      redirect_to new_conversation_path
+      redirect_to(session.delete(:referrer) || new_conversation_path)
     else
       render :onboarding, status: :unprocessable_entity
     end
--- spec/requests/chat_spec.rb
+++ spec/requests/chat_spec.rb
@@ -25,7 +25,7 @@ RSpec.describe "ChatController" do
     end
   end
 
-  describe "POST :confirmation" do
+  describe "POST :onboarding_confirm" do
     context "when the confirm_understand_risk[confirmation] param is present" do
       it "redirects to the new conversation page" do
         post onboarding_confirm_path, params: { confirm_understand_risk: { confirmation: "understand_risk" } }
@@ -38,6 +38,35 @@ RSpec.describe "ChatController" do
         post onboarding_confirm_path, params: { confirm_understand_risk: { confirmation: "understand_risk" } }
 
         expect(session[:chat_risks_understood]).to eq(true)
+      end
+
+      context "when the referrer is set in the session" do
+        let(:conversation) { create(:conversation) }
+        let(:question) { create(:question, conversation:) }
+
+        context "and is from the same host" do
+          it "redirects to the referrer" do
+            get answer_question_path(conversation, question) # Sets the referrer in the session
+            post onboarding_confirm_path, params: { confirm_understand_risk: { confirmation: "understand_risk" } }
+
+            expect(response).to have_http_status(:redirect)
+            expect(response).to redirect_to(answer_question_path(conversation, question))
+          end
+        end
+
+        context "and is from a different host" do
+          it "logs the error and redirects to the the chat page" do
+            expect(Rails.logger)
+              .to receive(:error)
+              .with("Unsuccessful unsafe redirect: Unsafe redirect to \"http://www.naughty.com/chat/conversations\", pass allow_other_host: true to redirect anyway.")
+
+            get new_conversation_path, headers: { HTTP_X_FORWARDED_HOST: "www.naughty.com" }
+            post onboarding_confirm_path, params: { confirm_understand_risk: { confirmation: "understand_risk" } }
+
+            expect(response).to have_http_status(:redirect)
+            expect(response).to redirect_to(chat_path)
+          end
+        end
       end
     end
 
