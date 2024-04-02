9ed4aeb59f297e02e9c7c98234b8b90869012e37
Kevin Dew <kevindew@me.com>
2024-04-02 09:23:03 +0100

    Make relevancy column not null
    There isn't a scenario where this application is running correctly and
    this field should have a null value. Best to fix this early before we
    have the performance penalties of adding a NOT NULL column to a large
    dataset.

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2024_03_28_132338) do
+ActiveRecord::Schema[7.1].define(version: 2024_04_02_082414) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "pgcrypto"
   enable_extension "plpgsql"
