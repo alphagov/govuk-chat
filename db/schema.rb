826debdd9592d904403dc9e8fc99a12ae8f218fa
Kevin Dew <kevindew@me.com>
2024-04-02 09:24:54 +0100

    Add back answer_id index for answers table
    This index was removed in [1] however it is still needed for looking up
    answer_sources quickly by answer_id. The composite index is only
    strictly useful if querying by answer_id and relevancy (the query plan
    will occasionally use it for other items if it calculates a benefit).
    
    [1]: https://github.com/alphagov/govuk-chat/commit/bd20f245dba931f9d8a35083cd3da70727ec8821

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2024_04_02_082056) do
+ActiveRecord::Schema[7.1].define(version: 2024_04_02_082414) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "pgcrypto"
   enable_extension "plpgsql"
@@ -22,6 +22,7 @@ ActiveRecord::Schema[7.1].define(version: 2024_04_02_082056) do
     t.datetime "updated_at", null: false
     t.integer "relevancy", null: false
     t.index ["answer_id", "relevancy"], name: "index_answer_sources_on_answer_id_and_relevancy", unique: true
+    t.index ["answer_id"], name: "index_answer_sources_on_answer_id"
     t.index ["created_at"], name: "index_answer_sources_on_created_at"
   end
 
