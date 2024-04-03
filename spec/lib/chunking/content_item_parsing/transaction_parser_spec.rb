a66e2223e28f21aa2afeaa60e63c1a8fc058c4fa
Kevin Dew <kevindew@me.com>
2024-04-03 21:27:33 +0100

    Parser class for transaction schema
    This parser class is for the transaction schema which has a few specific
    fields that can contain HTML.
    
    Oddly the schema is valid if all of these fields are empty so, while
    unlikely in practice, it is theoretically possible we receive content
    items matching this schema that have no content.

---
+++ spec/lib/chunking/content_item_parsing/transaction_parser_spec.rb
@@ -0,0 +1,99 @@
+RSpec.describe Chunking::ContentItemParsing::TransactionParser do
+  include ContentItemParserExamples
+
+  let(:content_item) do
+    schema = GovukSchemas::Schema.find(notification_schema: "transaction")
+    GovukSchemas::RandomExample.new(schema:).payload.tap do |item|
+      item["details"] = {
+        "introductory_paragraph" => [
+          {
+            "content_type" => "text/html",
+            "content" => "<p>Content/p>",
+          },
+        ],
+      }
+    end
+  end
+
+  it_behaves_like "a chunking content item parser"
+
+  describe ".call" do
+    it "uses the introductory_paragraph, more_information, other_ways_to_apply and what_you_need_to_know fields for chunks" do
+      content_item["details"] = {
+        "introductory_paragraph" => [
+          {
+            "content_type" => "text/html",
+            "content" => "<h2>Heading 1</h2><p>Content 1</p>",
+          },
+        ],
+        "more_information" => [
+          {
+            "content_type" => "text/html",
+            "content" => "<h2>Heading 2</h2><p>Content 2</p>",
+          },
+        ],
+        "other_ways_to_apply" => [
+          {
+            "content_type" => "text/html",
+            "content" => "<h2>Heading 3</h2><p>Content 3</p>",
+          },
+        ],
+        "what_you_need_to_know" => [
+          {
+            "content_type" => "text/html",
+            "content" => "<h2>Heading 4</h2><p>Content 4</p>",
+          },
+        ],
+      }
+
+      chunk_1, chunk_2, chunk_3, chunk_4 = described_class.call(content_item)
+
+      expect(chunk_1).to have_attributes(html_content: "<p>Content 1</p>")
+      expect(chunk_2).to have_attributes(html_content: "<p>Content 2</p>")
+      expect(chunk_3).to have_attributes(html_content: "<p>Content 3</p>")
+      expect(chunk_4).to have_attributes(html_content: "<p>Content 4</p>")
+    end
+
+    it "copes if fields are missing" do
+      content_item["details"] = {
+        "introductory_paragraph" => [
+          {
+            "content_type" => "text/html",
+            "content" => "<h2>Heading 1</h2><p>Content 1</p>",
+          },
+        ],
+        "more_information" => [
+          {
+            "content_type" => "text/html",
+            "content" => "<h2>Heading 2</h2><p>Content 2</p>",
+          },
+        ],
+      }
+
+      chunk_1, chunk_2 = described_class.call(content_item)
+
+      expect(chunk_1).to have_attributes(html_content: "<p>Content 1</p>")
+      expect(chunk_2).to have_attributes(html_content: "<p>Content 2</p>")
+    end
+
+    it "copes if all fields are missing" do
+      content_item["details"] = {}
+
+      expect(described_class.call(content_item)).to eq([])
+    end
+
+    it "raises an error if a field lacks a text/html content type" do
+      content_item["details"] = {
+        "introductory_paragraph" => [
+          {
+            "content_type" => "text/govspeak",
+            "content" => "Content",
+          },
+        ],
+      }
+
+      expect { described_class.call(content_item) }
+        .to raise_error("content type text/html not found in schema: transaction")
+    end
+  end
+end
