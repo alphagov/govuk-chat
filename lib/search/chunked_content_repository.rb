7460e0f62e762f1cd801c41519b8b86a852f8e0d
Kevin Dew <kevindew@me.com>
2024-04-04 15:12:56 +0100

    Add methods to create and recreate search index
    This uses the mappings that I spiked in [1]. I don't know yet if these
    are all the right fields, I'm just planning to have enough to get
    started with putting all the components together to have a
    proof-of-concept of search ingestion. I do expect there will be a bunch
    of changes.
    
    The use of text fields is for anything we might search against and are
    thus analyzed by OpenSearch, keyword fields are for exact matches only.
    
    I may consider moving this index management to a separate class
    (ChunkedContentRepository::Configuration) if the class becomes long and
    unwieldy.
    
    [1]: https://github.com/kevindew/govuk-semantic-search-experiments/blob/af1a1a0e4e61ea34e8af0d63d3f071c17b7ea18b/populate_opensearch.rb#L20-L51

--- lib/search/chunked_content_repository.rb
+++ lib/search/chunked_content_repository.rb
@@ -6,5 +6,47 @@ module Search
       @index = Rails.configuration.opensearch.chunked_content_index!
       @client = OpenSearch::Client.new(url: Rails.configuration.opensearch.url)
     end
+
+    def create_index
+      client.indices.create(
+        index:,
+        body: {
+          settings: {
+            index: {
+              knn: true,
+            },
+          },
+          mappings: {
+            properties: {
+              content_id: { type: "keyword" },
+              locale: { type: "keyword" },
+              base_path: { type: "keyword" },
+              document_type: { type: "keyword" },
+              title: { type: "text" },
+              url: { type: "keyword" },
+              chunk_index: { type: "keyword" },
+              heading_hierarchy: { type: "text" },
+              html_content: { type: "text" },
+              plain_content: { type: "text" },
+              openai_embedding: {
+                type: "knn_vector",
+                dimension: 1536, # expecting text-embedding-3-small model
+                method: {
+                  name: "hnsw",
+                  space_type: "l2",
+                  engine: "faiss",
+                },
+              },
+              digest: { type: "keyword" },
+            },
+          },
+        },
+      )
+    end
+
+    def create_index!
+      client.indices.delete(index:) if client.indices.exists?(index:)
+      create_index
+    end
   end
 end
