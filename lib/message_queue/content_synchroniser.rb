ddf585d17fe4b1118c359303c06c463a42f0340b
Kevin Dew <kevindew@me.com>
2024-04-05 10:49:29 +0100

    Class to synchronise a content item with search index
    This is a very minimal implementation that inserts content into the
    Search index with lots still to do.
    
    I expect the actual insertion will likely move to a dedicated class as
    that grows in complexity and this class will just deal with the decision
    of remove content (redirects etc) or upsert content.

---
+++ lib/message_queue/content_synchroniser.rb
@@ -0,0 +1,27 @@
+module MessageQueue
+  class ContentSynchroniser
+    def self.call(...) = new(...).call
+
+    def initialize(content_item)
+      @content_item = content_item
+    end
+
+    # TODO: delete redirects, gone, vanish, withdrawn schemas
+    # TODO: check for things already in the index
+    # TODO: apply open ai embeddings to data inserted
+    # TODO: create a result object for logging
+    def call
+      chunks = Chunking::ContentItemToChunks.call(content_item)
+      documents_to_index = chunks.map(&:to_opensearch_hash)
+      chunked_content_repository.bulk_index(documents_to_index:)
+    end
+
+    def chunked_content_repository
+      @chunked_content_repository ||= Search::ChunkedContentRepository.new
+    end
+
+  private
+
+    attr_reader :content_item
+  end
+end
