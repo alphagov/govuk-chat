7a8cec3c9e1553315c71b960a558194c9578a059
davidgisbey <david.gisbey@digital.cabinet-office.gov.uk>
2024-04-09 15:22:06 +0100

    Extend create conversation endpoint to handle "application/json" requests
    This commit adds js handling to the ConversationController#create
    endpoint.
    
    When a JS request comes in it should do the following:
    
    if the user input is valid:
    
    - create the question
    - respond with a 201 status code
    - send the answer_url so the client can poll for the answer
    - send the html generated by the conversation_message_component for the
    question in the body of the response
    
    if the user input is invalid:
    
    - respond with a 422 status code
    - send the errors in the body of the response

--- app/controllers/conversations_controller.rb
+++ app/controllers/conversations_controller.rb
@@ -19,11 +19,18 @@ class ConversationsController < ApplicationController
     if @create_question.valid?
       question = @create_question.submit
 
-      redirect_to answer_question_path(question.conversation, question)
+      respond_to do |format|
+        format.html { redirect_to answer_question_path(question.conversation, question) }
+        format.json { render json: question_success_json(question), status: :created }
+      end
     else
-      @conversation = @create_question.conversation
-
-      render :show, status: :unprocessable_entity
+      respond_to do |format|
+        format.html do
+          @conversation = @create_question.conversation
+          render :show, status: :unprocessable_entity
+        end
+        format.json { render json: question_error_json(@create_question), status: :unprocessable_entity }
+      end
     end
   end
 
@@ -47,5 +54,29 @@ private
 
   def find_conversation
     @conversation = Conversation.find(params[:id])
+  end
+
+  def question_success_json(question)
+    {
+      question_html: render_to_string(
+        partial: "components/conversation_message",
+        formats: :html,
+        locals: {
+          id: helpers.dom_id(question),
+          message: question.message,
+          is_question: true,
+        },
+      ),
+      answer_url: answer_question_path(question.conversation, question),
+      error_messages: [],
+    }
+  end
+
+  def question_error_json(create_question)
+    {
+      question_html: nil,
+      answer_url: nil,
+      error_messages: create_question.errors.map(&:message),
+    }
   end
 end
