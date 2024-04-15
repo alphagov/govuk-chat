9ca60122418007cae29d54d4cb47a94938517b97
Kevin Dew <kevindew@me.com>
2024-04-15 09:17:49 +0100

    Fix incorrect understanding of OpenAI Embedding endpoint
    I had two errors in my understanding of this endpoint:
    
    1) I thought the token limit applied to the combined input of the
       request - instead it applies per input
    2) I thought that if you sent too many tokens the API would just
       truncate your input and send you embeddings for the part it
       acknowledged - instead it returns a 400 Bad Request
    
    This commit changes this code to rectify this. We now reduce any inputs
    that exceed the token length to a size within it and then send batches
    to OpenAI of a fixed size.
    
    I couldn't find any information on whether there was a limit to how many
    inputs you could send OpenAI in a single request, so I ended up just
    choosing an arbitrary number of 50. With an expectation that for a
    chunked piece of content, it would be rare that one exceeds 50 chunks.
    
    This uses the tiktoken gem as a means of implementing the truncation.
    This gem allows the ability to convert a string into tokens and then
    truncate based off the number of tokens. I was a little hesitant to use
    the gem as it is flagged as in need of maintainers - but it seems to
    work well.
    
    There was some implementation options. I've maintained the approach that
    we send text to OpenAI and thus we decode the string we convert to
    tokens once truncated to do this. We could instead just send tokens to
    OpenAI though which would remove a couple of operations - I'm just not
    sure whether that carries any risks/downsides.

--- lib/search/text_to_embedding.rb
+++ lib/search/text_to_embedding.rb
@@ -3,7 +3,7 @@ module Search
     # TODO: confirm this model
     EMBEDDING_MODEL = "text-embedding-3-small".freeze
     INPUT_TOKEN_LIMIT = 8191
-    ROUGH_ALLOWED_TOKENS = (INPUT_TOKEN_LIMIT * 0.9).round # 90% of token limit
+    BATCH_SIZE = 50
 
     def self.call(...) = new(...).call
 
@@ -13,13 +13,9 @@ module Search
     end
 
     def call
-      embeddings = group_text_into_batches.flat_map do |batch|
-        response = openai_client.embeddings(
-          parameters: { model: EMBEDDING_MODEL, input: batch },
-        )
+      to_embed = text_collection.map(&method(:keep_input_within_token_limit))
 
-        response["data"].map { |data| data["embedding"] }
-      end
+      embeddings = convert_text_to_embeddings(to_embed)
 
       # return just first embedding rather than an array of embeddings if we
       # weren't given an array input
@@ -30,41 +26,32 @@ module Search
 
     attr_reader :string_input, :text_collection
 
-    # Converts an array of strings into an array that contains groups of strings.
-    # Where each group of strings represent a batch that are within the token
-    # limit and can be processed to embeddings in one request.
-    def group_text_into_batches
-      # we can skip the batching maths if there is only one item
-      return [text_collection] if text_collection.length == 1
+    def openai_client
+      @openai_client ||= OpenAIClient.build
+    end
 
-      rough_token_counts = text_collection.index_with do |text|
-        OpenAI.rough_token_count(text)
-      end
+    def keep_input_within_token_limit(text)
+      as_tokens = token_encoder.encode(text)
 
-      text_collection.each_with_object([]) do |text, batches|
-        text_tokens = rough_token_counts[text]
+      return text if as_tokens.length <= INPUT_TOKEN_LIMIT
 
-        empty_batch = batches.last.nil?
+      token_encoder.decode(as_tokens[...INPUT_TOKEN_LIMIT])
+    end
 
-        if empty_batch || text_tokens > ROUGH_ALLOWED_TOKENS
-          batches.append([text])
-          next
-        end
+    def convert_text_to_embeddings(to_embed_collection)
+      batches = to_embed_collection.each_slice(BATCH_SIZE).to_a
 
-        tokens_used_in_batch = batches.last.inject(0) do |memo, batch_text|
-          memo + rough_token_counts[batch_text]
-        end
+      batches.flat_map do |batch|
+        response = openai_client.embeddings(
+          parameters: { model: EMBEDDING_MODEL, input: batch },
+        )
 
-        if (tokens_used_in_batch + text_tokens) > ROUGH_ALLOWED_TOKENS
-          batches.append([text]) # create a new batch
-        else
-          batches.last.append(text) # add to batch
-        end
+        response["data"].map { |data| data["embedding"] }
       end
     end
 
-    def openai_client
-      @openai_client ||= OpenAIClient.build
+    def token_encoder
+      @token_encoder ||= Tiktoken.encoding_for_model(EMBEDDING_MODEL)
     end
   end
 end
