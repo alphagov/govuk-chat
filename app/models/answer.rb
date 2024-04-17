16ba8221a471370790ed37005ccd93cc462139b1
davidgisbey <david.gisbey@digital.cabinet-office.gov.uk>
2024-04-17 09:17:24 +0100

    Add abort_no_govuk_content to status enum
    When we hit search and no sources are returned, we still want to respond
    to the user with an answer. This answer will inform the user it in can't
    find any relevant information on GOV.UK for the user.
    
    We will build this answer with the abort_no_govuk_content status.

--- app/models/answer.rb
+++ app/models/answer.rb
@@ -4,11 +4,12 @@ class Answer < ApplicationRecord
 
   enum :status,
        {
-         success: "success",
-         error_non_specific: "error_non_specific",
-         error_answer_service_error: "error_answer_service_error",
          abort_forbidden_words: "abort_forbidden_words",
+         abort_no_govuk_content: "abort_no_govuk_content",
+         error_answer_service_error: "error_answer_service_error",
          error_context_length_exceeded: "error_context_length_exceeded",
+         error_non_specific: "error_non_specific",
+         success: "success",
        },
        prefix: true
 end
