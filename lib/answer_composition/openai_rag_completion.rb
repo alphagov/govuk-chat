088901785a98e54193ffcc5f98797c561e25a6df
Kevin Dew <kevindew@me.com>
2024-04-19 20:34:47 +0100

    Remove initialisation of class that is deleted
    This class was removed in: https://github.com/alphagov/govuk-chat/pull/121
    and causes test errors.
    
    I'm guessing this didn't show up as an issue on CI due to the issues
    resolved in the previous commits.

--- lib/answer_composition/openai_rag_completion.rb
+++ lib/answer_composition/openai_rag_completion.rb
@@ -11,7 +11,6 @@ module AnswerComposition
 
     def initialize(question)
       @question = question
-      @retriever = Retrieval::SearchApiV1Retriever
       @openai_client = OpenAIClient.build
     end
 
