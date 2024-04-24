c8251f35c4b252416b6c643721eb32941dd3a2fc
Pete Goddard <peter.goddard@digital.cabinet-office.gov.uk>
2024-04-24 11:46:33 +0100

    Add an assert to shared examples for allowed_schemas being non-empty
    We are relying on the method for BaseParser.supported_schema_and_document_type?
    
    An empty array would make the parser redundant, so asserting we can call it and
    it's non-empty.

--- spec/support/content_item_parser_examples.rb
+++ spec/support/content_item_parser_examples.rb
@@ -5,5 +5,11 @@ module ContentItemParserExamples
       expect(result).to be_an_instance_of(Array)
       expect(result).to all(be_a(Chunking::ContentItemChunk))
     end
+
+    it "responds to .allowed_schemas with a non-empty array" do
+      result = described_class.allowed_schemas
+      expect(result).to be_an_instance_of(Array)
+      expect(result).not_to be_empty
+    end
   end
 end
