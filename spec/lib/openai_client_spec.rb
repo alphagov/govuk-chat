6d41bd5b2f220ffc3db44720aac1ea0ca10f178c
Kevin Dew <kevindew@me.com>
2024-04-30 08:35:33 +0100

    Remove code for working around OpenAI Ruby stdout
    This code is no longer needed as this functionality is no longer applied
    in OpenAI Ruby 7.0.0

--- spec/lib/openai_client_spec.rb
+++ spec/lib/openai_client_spec.rb
@@ -11,14 +11,6 @@ RSpec.describe OpenAIClient do # rubocop:disable RSpec/FilePath
       expect(described_class.build).to be_an_instance_of(OpenAI::Client)
     end
 
-    it "configures the client to not write to stdout when an error occurs" do
-      stub_request(:post, /openai\.com/).to_return(status: 500)
-
-      expect { described_class.build.chat(parameters: chat_parameters) }
-        .to output_nothing.to_stdout
-        .and raise_error(StandardError)
-    end
-
     it "raises an OpenAIClient::ClientError when a client error occurs" do
       stub_request(:post, /openai\.com/).to_return(status: 400)
 
