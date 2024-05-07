ec6d2b27a333e65b03e5e20dd5820aa38358bee6
davidgisbey <david.gisbey@digital.cabinet-office.gov.uk>
2024-05-07 11:15:19 +0100

    Update Chat API integration to only persist the base path
    At the miment we persist a full URL for each source from Chat API, but persist
    a base_path with an optional fragment in our implementation.
    
    This causes rendering issues [here](https://github.com/alphagov/govuk-chat/blob/5baf29b70c5bac491ad3bcdeeeb5f85805051b9b/app/views/conversations/_question_and_answer.html.erb#L10)
    as we use plek to set prepend the base_path with https://www.gov.uk.
    
    This means that the Chat API soruces are rendered as
    
    ```
    https://www.gov.ukhttps://www.gov.uk/base-path
    ```
    
    This commit updates the Chat API integration to only persist the base path.
    
    I've not worried about persisting the fragment of the URL as i'm pretty sure
    there's no plan to implement chunk specific sources on Chat API.
    
    If that is the case i'll update this to persist the base path and
    fragment.

--- spec/lib/answer_composition/govuk_chat_api_spec.rb
+++ spec/lib/answer_composition/govuk_chat_api_spec.rb
@@ -14,7 +14,7 @@ RSpec.describe AnswerComposition::GovukChatApi do
   end
 
   describe ".call" do
-    let(:sources_list) { %w[https://example.com https://example2.org https://example3.org] }
+    let(:sources_list) { %w[https://gov.uk/taxes https://gov.uk/vat https://gov.uk/income-tax] }
 
     it "return unpersisted answer and sources returned from the chat api" do
       chat_api_response = {
@@ -24,6 +24,7 @@ RSpec.describe AnswerComposition::GovukChatApi do
       stub_chat_api_client(question.conversation_id, user_input, chat_api_response, chat_url)
 
       answer = described_class.call(question)
+
       expect(answer).to be_a(Answer)
       expect(answer).to have_attributes(
         question:,
@@ -31,6 +32,7 @@ RSpec.describe AnswerComposition::GovukChatApi do
         status: "success",
         persisted?: false,
       )
+      expect(answer.sources.map(&:url)).to match_array(["/taxes", "/vat", "/income-tax"])
     end
   end
 end
