9097b06ce388069de18c2fafe0cdf8dc9b3d1714
Pete Goddard <peter.goddard@digital.cabinet-office.gov.uk>
2024-05-08 13:56:14 +0100

    Move mapping data for the table component into the view
    Also more simple truncation of the text - not showing the end.

--- app/controllers/admin/search_controller.rb
+++ app/controllers/admin/search_controller.rb
@@ -7,23 +7,8 @@ class Admin::SearchController < Admin::BaseController
 private
 
   def search_results
-    return [[]] if @search_text.blank?
+    return [] if @search_text.blank?
 
-    Search::ResultsForQuestion.call(@search_text).map(&method(:map_result))
-  end
-
-  def map_result(result)
-    [
-      { text: result.title },
-      { text: result.base_path },
-      { text: format_content(result.plain_content) },
-      { text: result.score, format: "numeric" },
-    ]
-  end
-
-  def format_content(content)
-    return content if content.length < 50
-
-    "#{content.truncate_words(10)}#{content.last(50)}"
+    Search::ResultsForQuestion.call(@search_text)
   end
 end
