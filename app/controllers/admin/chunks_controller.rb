e4881e87c56c7395b3dbc723404b235b2640350c
Pete Goddard <peter.goddard@digital.cabinet-office.gov.uk>
2024-05-14 11:29:15 +0100

    Conditionally render the Back to results link
    Only render the link if `back_link` query param is provided and the path is
    under `/admin/search`

--- app/controllers/admin/chunks_controller.rb
+++ app/controllers/admin/chunks_controller.rb
@@ -1,5 +1,17 @@
 class Admin::ChunksController < Admin::BaseController
+  before_action :set_back_link
+
   def show
     @chunk = Search::ChunkedContentRepository.new.chunk(params[:id])
+  end
+
+private
+
+  def set_back_link
+    @back_link = params[:back_link] if back_link_allowed?
+  end
+
+  def back_link_allowed?
+    params[:back_link] && params[:back_link].start_with?(admin_search_path)
   end
 end
--- spec/requests/admin/chunk_spec.rb
+++ spec/requests/admin/chunk_spec.rb
@@ -18,10 +18,41 @@ RSpec.describe "Admin::ChunkController", :chunked_content_index do
       expect(response).to have_http_status(:not_found)
     end
 
+    context "when back_link is provided in query params" do
+      let(:back_link) { admin_search_path(params: { search_text: "pay my tax" }) }
+
+      it "renders a back_link" do
+        get admin_chunk_path(id: chunk_id, params: { back_link: })
+        expect(response.body).to render_back_link(back_link)
+      end
+    end
+
+    context "when back_link is not prefixed with /admin/search" do
+      let(:back_link) { "/some/other/path" }
+
+      it "hides the back link" do
+        get admin_chunk_path(id: chunk_id, params: { back_link: })
+        expect(response.body).not_to render_back_link
+      end
+    end
+
+    context "when back_link is not provided in query params" do
+      it "hides the back link" do
+        get admin_chunk_path(id: chunk_id)
+        expect(response.body).not_to render_back_link
+      end
+    end
+
   private
 
     def have_displayed_chunk
       have_selector(".govuk-summary-list", text: Regexp.new(content_chunk[:title]))
+    end
+
+    def render_back_link(back_link = nil)
+      return have_selector("a.govuk-back-link", text: "Back to results") if back_link.nil?
+
+      have_selector("a.govuk-back-link[href='#{back_link}']", text: "Back to results")
     end
   end
 end
