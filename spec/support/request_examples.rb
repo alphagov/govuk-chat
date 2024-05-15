be86d26af62c7ec28a2e5afc446f487ea890b227
davidgisbey <david.gisbey@digital.cabinet-office.gov.uk>
2024-05-15 09:32:24 +0100

    Add before_action redirects onboarding endpoints
    We're adding in some redirects to the onboarding endpoints to ensure that
    the user is redirected to the appropriate part of the onbarding flow or
    to their conversation if they've already completed the onboarding.
    
    These should behave in the following way for the limitation endpoints:
    
    ```
    if conversation_id is present on the cookie
      redirect_to conversation show page if
    elsif cookie is not present && session[:onboarding] == "conversation"
      redirect to new conversation page if
    elsif session[:onboarding] == "privacy"
      redirect to privacy page
    end
    ````
    
    and the following for the privacy endpoints:
    
    ```
    if conversation_id is present on the cookie
      redirect_to conversation show page if
    elsif cookie is not present && session[:onboarding] == "conversation"
      redirect to new conversation page if
    elsif session[:onboarding] != "privacy" || "conversation"
      redirect to limitations page
    end
    ```
    
    These will able to be simplified once we have merged the new and show page
    because the first two redirects will be merged and always redirect to
    the show page
    
    I've pulled out as much of the logic as possible into shared example.
    Some of these will be reused be used for the conversation request spec
    when we migrate the conversations controller to using the cookie approach.

--- spec/support/request_examples.rb
+++ spec/support/request_examples.rb
@@ -22,9 +22,83 @@ module RequestExamples
     end
   end
 
+  shared_examples "redirects user to the conversation when conversation_id is set on cookie" do |routes:|
+    routes.each do |path, methods|
+      describe "Redirects user to the conversation when conversation_id is set on cookie" do
+        methods.each do |method|
+          it "redirects user to the conversation when conversation_id is present for #{method} #{path}" do
+            conversation = create(:conversation)
+            cookies[:conversation_id] = conversation.id
+
+            process(method.to_sym, public_send(path.to_sym))
+
+            expect(response).to have_http_status(:redirect)
+            expect(response).to redirect_to(show_conversation_path(conversation))
+          end
+        end
+      end
+    end
+  end
+
+  shared_examples "redirects user to the new conversation page when onboarded and no conversation cookie" do |routes:|
+    include_context "with onboarding completed"
+
+    routes.each do |path, methods|
+      describe "Redirects user to the new conversation page when session[:onboarding] is 'conversation'" do
+        methods.each do |method|
+          it "redirects user to the new conversation page when session[:onboarding] is 'conversation' for #{method} #{path}" do
+            process(method.to_sym, public_send(path.to_sym))
+
+            expect(response).to have_http_status(:redirect)
+            expect(response).to redirect_to(new_conversation_path)
+          end
+        end
+      end
+    end
+  end
+
+  shared_examples "redirects user to the privacy page when onboarding limitations has been completed" do |routes:|
+    include_context "with onboarding limitations completed"
+
+    routes.each do |path, methods|
+      describe "Redirects user to the privacy page when session[:onboarding] is 'privacy'" do
+        methods.each do |method|
+          it "redirects user to the privacy page when session[:onboarding] is 'privacy' for #{method} #{path}" do
+            process(method.to_sym, public_send(path.to_sym))
+
+            expect(response).to have_http_status(:redirect)
+            expect(response).to redirect_to(onboarding_privacy_path)
+          end
+        end
+      end
+    end
+  end
+
+  shared_examples "redirects user to the onboarding limitations page when onboarding not started" do |routes:|
+    routes.each do |path, methods|
+      describe "Redirects user to the limitations page when session[:onboarding] isn't 'privacy' or 'conversation'" do
+        methods.each do |method|
+          it "redirects user to the limitations page when session[:onboarding] isn't 'privacy' or 'conversation' for #{method} #{path}" do
+            process(method.to_sym, public_send(path.to_sym))
+
+            expect(response).to have_http_status(:redirect)
+            expect(response).to redirect_to(onboarding_limitations_path)
+          end
+        end
+      end
+    end
+  end
+
   shared_context "with onboarding completed" do
     before do
-      post onboarding_privacy_path
+      post onboarding_limitations_confirm_path
+      post onboarding_privacy_confirm_path
+    end
+  end
+
+  shared_context "with onboarding limitations completed" do
+    before do
+      post onboarding_limitations_confirm_path
     end
   end
 end
