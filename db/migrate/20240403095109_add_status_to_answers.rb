b62c451b3e224d61cf64c769fa48d89a7a9537bd
Kevin Dew <kevindew@me.com>
2024-05-20 09:03:33 +0100

    Ignore not null column rubocop violation
    We were aware and accepted the risks of this :-)

--- db/migrate/20240403095109_add_status_to_answers.rb
+++ db/migrate/20240403095109_add_status_to_answers.rb
@@ -3,7 +3,7 @@ class AddStatusToAnswers < ActiveRecord::Migration[7.1]
     create_enum :status, %w[success error_non_specific error_answer_service_error]
 
     change_table :answers, bulk: true do |t|
-      t.enum :status, enum_type: "status", null: false
+      t.enum :status, enum_type: "status", null: false # rubocop:disable Rails/NotNullColumn
       t.string :error_message
     end
   end
