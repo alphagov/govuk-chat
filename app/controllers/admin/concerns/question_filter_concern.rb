e23e948284a3942558fd462d0ee599f7bdb9f2a4
davidgisbey <david.gisbey@digital.cabinet-office.gov.uk>
2024-05-29 17:25:19 +0100

    Add start and end date filters to view
    This adds the filters to the conversation show and questions index pages.
    
    It also adds an error_summary_component content block to the layout and
    handles rendering the error summary component when the form has errors.
    
    View

--- app/controllers/admin/concerns/question_filter_concern.rb
+++ app/controllers/admin/concerns/question_filter_concern.rb
@@ -4,10 +4,18 @@ module Admin::Concerns::QuestionFilterConcern
 private
 
   def questions_filter(conversation = nil)
-    filter_params = params.permit(:search, :status, :page)
+    filter_params = params.permit(
+      :search,
+      :status,
+      { start_date_params: %i[day month year], end_date_params: %i[day month year] },
+      :page,
+    )
+
     Admin::Form::QuestionsFilter.new(
       search: filter_params[:search],
       status: filter_params[:status],
+      start_date_params: filter_params[:start_date_params] || {},
+      end_date_params: filter_params[:end_date_params] || {},
       page: filter_params[:page],
       conversation:,
     )
