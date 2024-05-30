a060b7aecc2aa2c95a363e831660d92f5d0a655a
James Chan <james.chan@digital.cabinet-office.gov.uk>
2024-05-30 12:25:40 +0100

    Add tests

---
+++ spec/views/components/_chat_header.html.erb_spec.rb
@@ -0,0 +1,41 @@
+RSpec.describe "components/_chat_header.html.erb" do
+  it "renders the chat header component correctly" do
+    render("components/chat_header")
+
+    expect(rendered)
+      .to have_selector(".app-c-header")
+      .and have_selector(".app-c-header__container")
+      .and have_selector(".app-c-header__logo")
+      .and have_selector(".app-c-header__link.app-c-header__link--homepage")
+      .and have_selector(".app-c-header__logotype")
+      .and have_selector(".app-c-header__product-name")
+  end
+
+  it "renders the chat header with a tag when tag_text is specified" do
+    render("components/chat_header", {
+      tag_text: "Tag text",
+    })
+
+    expect(rendered).to have_selector(".app-c-header__tag", text: "Tag text")
+  end
+
+  it "renders the chat header with links when navigation_items are specified" do
+    render("components/chat_header", {
+      navigation_items: [
+        {
+          text: "Item 1",
+          href: "/item-1",
+        },
+        {
+          text: "Item 2",
+          href: "/item-2",
+        },
+      ],
+    })
+
+    expect(rendered).to have_selector(".govuk-header__navigation")
+      .and have_selector(".govuk-header__navigation .govuk-header__link[href='/item-1']", text: "Item 1")
+      .and have_selector(".govuk-header__navigation .govuk-header__link[href='/item-2']", text: "Item 2")
+      .and have_selector(".govuk-header__navigation[aria-label='Top level']")
+  end
+end
