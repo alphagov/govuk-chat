3dc70645a387c0d8eb90968b95855f058e9ba8b9
Kevin Dew <kevindew@me.com>
2024-06-03 17:59:32 +0100

    Group scopes before instance methods
    This follows the convention that the class configuration is done before
    instance method definitions.

--- app/models/question.rb
+++ app/models/question.rb
@@ -10,12 +10,12 @@ class Question < ApplicationRecord
   has_one :answer
   scope :unanswered, -> { where.missing(:answer) }
 
-  def answer_status
-    answer&.status || "pending"
-  end
-
   scope :active, lambda {
     max_age = Rails.configuration.conversations.max_question_age_days.days.ago
     where("questions.created_at >= :max_age", max_age:)
   }
+
+  def answer_status
+    answer&.status || "pending"
+  end
 end
--- spec/models/question_spec.rb
+++ spec/models/question_spec.rb
@@ -8,6 +8,17 @@ RSpec.describe Question do
     end
   end
 
+  describe ".active" do
+    it "returns questions newer than the configured max_question_age" do
+      freeze_time do
+        allow(Rails.configuration.conversations).to receive(:max_question_age_days).and_return(1)
+        to_find = create(:question, created_at: 1.day.ago)
+        create(:question, created_at: 1.day.ago - 1.second)
+        expect(described_class.active).to eq([to_find])
+      end
+    end
+  end
+
   describe "#answer_status" do
     it "returns the status of the answer" do
       question = create(:question, :with_answer)
@@ -17,17 +28,6 @@ RSpec.describe Question do
     it "returns 'pending' if the question has no answer" do
       question = create(:question)
       expect(question.answer_status).to eq "pending"
-    end
-  end
-
-  describe ".active" do
-    it "returns questions newer than the configured max_question_age" do
-      freeze_time do
-        allow(Rails.configuration.conversations).to receive(:max_question_age_days).and_return(1)
-        to_find = create(:question, created_at: 1.day.ago)
-        create(:question, created_at: 1.day.ago - 1.second)
-        expect(described_class.active).to eq([to_find])
-      end
     end
   end
 end
