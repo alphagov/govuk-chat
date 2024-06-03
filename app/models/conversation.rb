1d7c4f911e8a15dc94f990fecd37a63ebd53417f
Kevin Dew <kevindew@me.com>
2024-06-03 18:12:01 +0100

    Replace without_questions scope with where.missing
    I think we only used a scope here due to the verbosity of the query,
    with the simpler Rails approach we can drop the need for a scope.

--- app/models/conversation.rb
+++ app/models/conversation.rb
@@ -1,10 +1,6 @@
 class Conversation < ApplicationRecord
   has_many :questions
 
-  scope :without_questions, lambda {
-    left_outer_joins(:questions).where(questions: { conversation_id: nil })
-  }
-
   scope :active, -> { where(Question.active.where("questions.conversation_id = conversations.id").arel.exists) }
 
   def questions_for_showing_conversation
