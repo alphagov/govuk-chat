046d17f5f49282d52a4debf38d74bf009903f098
Pete Goddard <peter.goddard@digital.cabinet-office.gov.uk>
2024-06-07 12:01:02 +0100

    Use the ResultSet in the admin search endpoint.
    Assign the new ResultSet class returned by ResultsForQuestion directly in the
    controller.
    
    Use @result_set.results in the view to render the current set of chunks.

--- app/controllers/admin/search_controller.rb
+++ app/controllers/admin/search_controller.rb
@@ -1,14 +1,14 @@
 class Admin::SearchController < Admin::BaseController
   def index
     @search_text = params[:search_text]
-    @search_results = search_results
+    @result_set = perform_search
   end
 
 private
 
-  def search_results
-    return [] if @search_text.blank?
+  def perform_search
+    return Search::ResultsForQuestion::ResultSet.empty if @search_text.blank?
 
-    Search::ResultsForQuestion.call(@search_text).results
+    Search::ResultsForQuestion.call(@search_text)
   end
 end
