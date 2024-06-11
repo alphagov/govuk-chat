2ace70062db8ce00e4f8db29618283efa2080ac9
Pete Goddard <peter.goddard@digital.cabinet-office.gov.uk>
2024-06-11 14:35:58 +0100

    Add a spec for ResultSet

---
+++ spec/lib/search/results_for_question/result_set_spec.rb
@@ -0,0 +1,35 @@
+RSpec.describe Search::ResultsForQuestion::ResultSet do
+  describe ".empty" do
+    it "returns an empty ResultSet" do
+      result_set = described_class.empty
+      expect(result_set).to be_a(described_class)
+      expect(result_set).to have_attributes(
+        results: [],
+        rejected_results: [],
+      )
+    end
+  end
+
+  describe "#empty?" do
+    context "when both results and rejected_results are empty" do
+      it "returns true" do
+        result_set = described_class.new(results: [], rejected_results: [])
+        expect(result_set.empty?).to eq(true)
+      end
+    end
+
+    context "when results is not empty" do
+      it "returns false" do
+        result_set = described_class.new(results: [{}], rejected_results: [])
+        expect(result_set.empty?).to eq(false)
+      end
+    end
+
+    context "when rejected_results is not empty" do
+      it "returns false" do
+        result_set = described_class.new(results: [], rejected_results: [{}])
+        expect(result_set.empty?).to eq(false)
+      end
+    end
+  end
+end
