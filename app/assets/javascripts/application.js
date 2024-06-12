4178e8cd92218a7108d92a041d0bdeba4f6af9d0
Jon Kirwan <87758239+jon-kirwan@users.noreply.github.com>
2024-06-12 14:30:38 +0100

    Add onboarding JS modules
    Adds two new modules for onboarding.
    
    The first one of these, ConversationOnboardingFlow, is a module that is
    used to manage the transition between steps in the onboarding process.
    It has the role of replacing HTML in the view, updating the URL,
    updating the data attributes of the wrapper module.
    
    The second one of these, Onboarding, is a module for handling an
    onboarding view. It is to intercept a form submission and instead make
    the submission via a fetch request. The JSON response of this response
    is used to fire an event that ConversationOnboardingFlow can receive in
    order to transition to the next view.
    
    As we are not replacing all of the HTML that is used in the conversation
    layout we need a mechanism to remove event listeners otherwise dead
    modules will still listen to events. To do this I've set up an array on
    the onboarding module that stores the event listeners so that these can
    be removed when the module receives an event of `deinit`

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -3,3 +3,5 @@
 //= require components/answer-feedback
 //= require components/conversation-form
 //= require modules/chat-conversation
+//= require modules/conversation-onboarding-flow
+//= require modules/onboarding
---
+++ app/assets/javascripts/modules/onboarding.js
@@ -0,0 +1,70 @@
+window.GOVUK = window.GOVUK || {}
+window.GOVUK.Modules = window.GOVUK.Modules || {};
+
+(function (Modules) {
+  class Onboarding {
+    constructor (module) {
+      this.module = module
+      this.form = this.module.querySelector('.js-onboarding-form')
+      this.eventListeners = []
+    }
+
+    init () {
+      this.addEventListener(this.module, 'submit', e => this.handleSubmit(e))
+      this.addEventListener(this.module, 'deinit', () => this.deinit())
+    }
+
+    async handleSubmit (event) {
+      event.preventDefault()
+
+      try {
+        const formData = new FormData(this.form)
+        formData.append(event.submitter.name, event.submitter.value)
+
+        const response = await fetch(this.form.action, {
+          method: this.form.method,
+          body: formData,
+          headers: {
+            Accept: 'application/json'
+          }
+        })
+        await this.handleFormResponse(response)
+      } catch (error) {
+        console.error(error)
+        this.form.submit()
+      }
+    }
+
+    async handleFormResponse (response) {
+      if (response.status !== 200) {
+        throw new Error(`Unexpected response status: ${response.status}`)
+      }
+
+      const responseJson = await response.json()
+
+      const url = new URL(response.url)
+
+      const eventDetail = {
+        path: url.pathname, // just use pathname as URL could have fragment, unclear if supposed to: https://github.com/whatwg/fetch/issues/214
+        fragment: responseJson.fragment, // ideally we'd get fragment from URL but browsers seem to strip it
+        conversationData: responseJson.conversation_data,
+        conversationAppendHtml: responseJson.conversation_append_html,
+        formHtml: responseJson.form_html
+      }
+      const event = new CustomEvent('onboarding-transition', { detail: eventDetail })
+      this.module.dispatchEvent(event)
+    }
+
+    deinit () {
+      this.eventListeners.forEach(([element, event, handler]) => {
+        element.removeEventListener(event, handler)
+      })
+    }
+
+    addEventListener (element, event, handler) {
+      element.addEventListener(event, handler)
+      this.eventListeners.push([element, event, handler])
+    }
+  }
+  Modules.Onboarding = Onboarding
+})(window.GOVUK.Modules)
