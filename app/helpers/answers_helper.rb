af304ed997d0eca8718fa5f1a5b8adf072c3dbd8
davidgisbey <david.gisbey@digital.cabinet-office.gov.uk>
2024-07-09 11:55:25 +0100

    Convert message markdown to html in answer helper
    Now we're persisting the LLMs response as markdown to the database. We
    need to convert the markdown to HTML before rendering it to the user.
    
    This updates the '#render_answer_message' which handles this in the
    chat and admin UIs.

--- app/helpers/answers_helper.rb
+++ app/helpers/answers_helper.rb
@@ -1,7 +1,8 @@
 module AnswersHelper
   def render_answer_message(message)
+    message_to_html = Kramdown::Document.new(message).to_html
     render "govuk_publishing_components/components/govspeak" do
-      sanitize(message)
+      sanitize(message_to_html)
     end
   end
 end
