824eba24d2dff5dff515e7c01c85d9c744684372
davidgisbey <david.gisbey@digital.cabinet-office.gov.uk>
2024-07-16 12:22:43 +0100

    Implement QuestionRephraser step of pipeline
    This moves the QuestionRephraser into the Pipeline directory. No core
    functionality has changed for this step, but just to outline what happens:
    
    - When it's the first question in the conversation we return nil and don't
    do anything with the context
    - When multiple questions have been asked it rephrases the question
      - rephrases the question
      - updates the question message on the context
    - If OpenAI returns the same message as the original question, we also
    update teh question message on the context but it has no effect
    - If OpenAI throws an error
      - we catch the error
      - set a descriptive error
      - raise the error which is caught by the OpenAIAnswer which builds
      the correct answer on the context and returns it to the composer

--- lib/answer_composition/openai_unstructured_answer.rb
+++ lib/answer_composition/openai_unstructured_answer.rb
@@ -5,32 +5,21 @@ module AnswerComposition
     def self.call(...) = new(...).call
 
     def call
-      @question_message = QuestionRephraser.call(question:)
-
-      return build_answer(Answer::CannedResponses::FORBIDDEN_WORDS_RESPONSE, "abort_forbidden_words") if question_contains_forbidden_words?
-      return build_answer(Answer::CannedResponses::NO_CONTENT_FOUND_REPONSE, "abort_no_govuk_content") if search_results.blank?
-
-      message = openai_response.dig("choices", 0, "message", "content")
-      build_answer(message, "success", build_sources)
-    rescue OpenAIClient::ContextLengthExceededError => e
-      GovukError.notify(e)
-      question.build_answer(
-        message: Answer::CannedResponses::CONTEXT_LENGTH_EXCEEDED_RESPONSE,
-        status: "error_context_length_exceeded",
-        error_message: error_message(e),
+      answer_pipeline(
+        Pipeline::QuestionRephraser,
+        # Pipeline::ForbiddenWordsChecker,
+        # Pipeline::SearchResultFetcher,
+        # method(:compose_answer)
       )
-    rescue OpenAIClient::RequestError => e
-      GovukError.notify(e)
-      question.build_answer(
-        message: Answer::CannedResponses::OPENAI_CLIENT_ERROR_RESPONSE,
-        status: "error_answer_service_error",
-        error_message: error_message(e),
-      )
+
+      # return build_answer(Answer::CannedResponses::FORBIDDEN_WORDS_RESPONSE, "abort_forbidden_words") if question_contains_forbidden_words?
+      # return build_answer(Answer::CannedResponses::NO_CONTENT_FOUND_REPONSE, "abort_no_govuk_content") if search_results.blank?
+
+      # message = openai_response.dig("choices", 0, "message", "content")
+      # build_answer(message, "success", build_sources)
     end
 
   private
-
-    attr_reader :question, :question_message
 
     def openai_response
       openai_client.chat(
@@ -46,17 +35,13 @@ module AnswerComposition
       [
         { role: "system", content: system_prompt },
         few_shots,
-        { role: "user", content: question_message },
+        { role: "user", content: context.question_message },
       ]
       .flatten
     end
 
     def system_prompt
       sprintf(llm_prompts.answer_composition.compose_answer.system_prompt, context: system_prompt_context)
-    end
-
-    def rephrased_question
-      question_message unless question_message == question.message
     end
 
     def system_prompt_context
@@ -74,32 +59,32 @@ module AnswerComposition
       .join("\n\n")
     end
 
-    def question_contains_forbidden_words?
-      words = question_message.downcase.split(/\b/)
-      Rails.configuration.question_forbidden_words.intersection(words).any?
-    end
+    # def question_contains_forbidden_words?
+    #   words = question_message.downcase.split(/\b/)
+    #   Rails.configuration.question_forbidden_words.intersection(words).any?
+    # end
 
     def search_results
-      @search_results ||= Search::ResultsForQuestion.call(question_message).results
+      @search_results ||= Search::ResultsForQuestion.call(context.question_message).results
     end
 
-    def build_answer(message, status, sources = [])
-      question.build_answer(message:, rephrased_question:, status:, sources:)
-    end
+    # def build_answer(message, status, sources = [])
+    #   question.build_answer(message:, rephrased_question:, status:, sources:)
+    # end
 
-    def build_sources
-      search_results.map.with_index do |result, relevancy|
-        AnswerSource.new(
-          exact_path: result.url,
-          base_path: result.base_path,
-          title: result.title,
-          relevancy:,
-          content_chunk_id: result._id,
-          content_chunk_digest: result.digest,
-          heading: result.heading_hierarchy.last,
-        )
-      end
-    end
+    # def build_sources
+    #   search_results.map.with_index do |result, relevancy|
+    #     AnswerSource.new(
+    #       exact_path: result.url,
+    #       base_path: result.base_path,
+    #       title: result.title,
+    #       relevancy:,
+    #       content_chunk_id: result._id,
+    #       content_chunk_digest: result.digest,
+    #       heading: result.heading_hierarchy.last,
+    #     )
+    #   end
+    # end
 
     def few_shots
       llm_prompts.answer_composition.compose_answer.few_shots.flat_map do |few_shot|
