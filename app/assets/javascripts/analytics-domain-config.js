92b0d62b1d9be483c7560eb54b1a33b745d04427
Kevin Dew <kevindew@me.com>
2024-07-22 15:54:58 +0100

    Configure Google Analytics
    This uses the configuration information that Daniel provided for the ids
    of production and integration.
    
    It populates this data prior to calling the load-analytics script.
    
    I don't really know how all our analytics works, however I'm expecting
    that by configuring this we should get a page view event or similar that
    can be used to confirm if analytics are working.

---
+++ app/assets/javascripts/analytics-domain-config.js
@@ -0,0 +1,18 @@
+window.GOVUK = window.GOVUK || {}
+window.GOVUK.vars = window.GOVUK.vars || {}
+window.GOVUK.vars.extraDomains = [
+  {
+    name: 'production',
+    domains: ['chat.publishing.service.gov.uk'],
+    initialiseGA4: true,
+    id: 'GTM-MG7HG5W'
+  },
+  {
+    name: 'integration',
+    domains: ['chat.integration.publishing.service.gov.uk'],
+    initialiseGA4: true,
+    id: 'GTM-MG7HG5W',
+    auth: 'C7iYdcsOlYgGmiUJjZKrHQ',
+    preview: 'env-4'
+  }
+]
---
+++ app/assets/javascripts/analytics.js
@@ -0,0 +1,2 @@
+//= require analytics-domain-config
+//= require govuk_publishing_components/load-analytics
