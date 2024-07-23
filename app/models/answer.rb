cec554a2712829e870e49190be71e2924d3ad3ac
Pete Goddard <peter.goddard@digital.cabinet-office.gov.uk>
2024-07-23 11:19:16 +0100

    Add error_output_guardrails enum to answer status

--- app/models/answer.rb
+++ app/models/answer.rb
@@ -30,6 +30,7 @@ class Answer < ApplicationRecord
          abort_timeout: "abort_timeout",
          error_answer_service_error: "error_answer_service_error",
          error_context_length_exceeded: "error_context_length_exceeded",
+         error_output_guardrails: "error_output_guardrails",
          error_non_specific: "error_non_specific",
          success: "success",
        },
