f80809c395831f2b139198226f156b297fe62dee
davidgisbey <david.gisbey@digital.cabinet-office.gov.uk>
2024-07-30 11:12:04 +0100

    Add .instance method to the Setting class
    This method returns the singleton instance of the Setting class if present.
    The first time this method is called, it will create a new instance of the
    Setting class and return it.

--- app/models/settings.rb
+++ app/models/settings.rb
@@ -1,3 +1,7 @@
 class Settings < ApplicationRecord
   validates :singleton_guard, inclusion: { in: [0] }, strict: true
+
+  def self.instance
+    first_or_create!
+  end
 end
--- spec/models/settings_spec.rb
+++ spec/models/settings_spec.rb
@@ -8,4 +8,15 @@ RSpec.describe Settings do
       expect { invalid_instance.valid? }.to raise_error(ActiveModel::StrictValidationFailed)
     end
   end
+
+  describe ".instance" do
+    it "returns the first settings record if one is present" do
+      instance = create(:settings)
+      expect(described_class.instance).to eq(instance)
+    end
+
+    it "creates a new settings record if one is not present" do
+      expect { described_class.instance }.to change(described_class, :count).by(1)
+    end
+  end
 end
