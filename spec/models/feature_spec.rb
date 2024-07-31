858c28bc8ca88c8b67d6405f52c0fed90f6d96ed
Jack Weeden <jack@ajack.org>
2024-07-31 12:09:39 +0100

    Change feature name in spec
    This spec just asserts a bunch of stuff around feature flags.
    
    The whole feature flag implementation was added when we wanted to test a
    new answer strategy, and so the name of the feature used in the test was
    the name of that first feature.
    
    Really all we care about in this test is that the feature mechanism is
    working, so we can just use a fake name for the feature instead. It
    removes any confusion about what the test is doing. It also removes
    any reference to "chat_api" from the codebase, which is old answer
    strategy we were using but aren't any more.

--- spec/models/feature_spec.rb
+++ spec/models/feature_spec.rb
@@ -1,55 +1,56 @@
 RSpec.describe Feature do
   let(:user) { build(:user) }
+  let(:feature) { :my_feature }
 
   context "when the feature is enabled for everyone" do
-    before { Flipper.enable(:chat_api) }
+    before { Flipper.enable(feature) }
 
     it "returns true for a user" do
-      expect(described_class.enabled?(:chat_api, user)).to be(true)
+      expect(described_class.enabled?(feature, user)).to be(true)
     end
 
     it "returns true for no user" do
-      expect(described_class.enabled?(:chat_api, nil)).to be(true)
+      expect(described_class.enabled?(feature, nil)).to be(true)
     end
   end
 
   context "when the feature is disabled for everyone" do
-    before { Flipper.disable(:chat_api) }
+    before { Flipper.disable(feature) }
 
     it "returns false for a user" do
-      expect(described_class.enabled?(:chat_api, user)).to be(false)
+      expect(described_class.enabled?(feature, user)).to be(false)
     end
 
     it "returns false for no user" do
-      expect(described_class.enabled?(:chat_api, nil)).to be(false)
+      expect(described_class.enabled?(feature, nil)).to be(false)
     end
   end
 
   context "when the feature is enabled for a user" do
-    before { Flipper.enable(:chat_api, user) }
+    before { Flipper.enable(feature, user) }
 
     it "returns true for the user" do
-      expect(described_class.enabled?(:chat_api, user)).to be(true)
+      expect(described_class.enabled?(feature, user)).to be(true)
     end
 
     it "returns true when the user is Current.user" do
       allow(Current).to receive(:user).and_return(user)
-      expect(described_class.enabled?(:chat_api)).to be(true)
+      expect(described_class.enabled?(feature)).to be(true)
     end
 
     it "returns false when a different user is Current.user" do
       allow(Current).to receive(:user).and_return(build(:user))
-      expect(described_class.enabled?(:chat_api)).to be(false)
+      expect(described_class.enabled?(feature)).to be(false)
     end
 
     it "returns true when a different user is Current.user but the actual user is passed in" do
       allow(Current).to receive(:user).and_return(build(:user))
-      expect(described_class.enabled?(:chat_api, user)).to be(true)
+      expect(described_class.enabled?(feature, user)).to be(true)
     end
 
     it "returns false when a nil user argument is passed in to replace Current.user" do
       allow(Current).to receive(:user).and_return(user)
-      expect(described_class.enabled?(:chat_api, nil)).to be(false)
+      expect(described_class.enabled?(feature, nil)).to be(false)
     end
   end
 end
