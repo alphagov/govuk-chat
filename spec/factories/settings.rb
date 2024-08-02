ea811ffc4dfc03e63a31465e1c2d68ee95cbb170
davidgisbey <david.gisbey@digital.cabinet-office.gov.uk>
2024-08-02 15:16:10 +0100

    Update settings factory to use find_or_create_by
    Since this is a Singleton it should update the current record if one
    exists for whatever reason, rather than attempting to create a new one
    and blow up.

--- spec/factories/settings.rb
+++ spec/factories/settings.rb
@@ -4,5 +4,7 @@ FactoryBot.define do
     instant_access_places { 100 }
     delayed_access_places { 100 }
     sign_up_enabled { true }
+
+    initialize_with { Settings.find_or_create_by(singleton_guard: 0) }
   end
 end
