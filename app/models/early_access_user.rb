e2c5bdc2fd191d8bf03d5520535830276248d50f
Pete Goddard <peter.goddard@digital.cabinet-office.gov.uk>
2024-08-08 16:02:31 +0100

    Fix spec broken by column change

--- app/models/early_access_user.rb
+++ app/models/early_access_user.rb
@@ -2,7 +2,7 @@ class EarlyAccessUser < ApplicationRecord
   passwordless_with :email
 
   def sign_in(session)
-    update!(last_login: Time.zone.now)
+    update!(last_login_at: Time.zone.now)
     passwordless_sessions.where.not(id: session.id).delete_all
   end
 end
