2b1c88d807cb9a7099e2309d25e349a583a12b35
Pete Goddard <peter.goddard@digital.cabinet-office.gov.uk>
2024-08-09 11:44:30 +0100

    Remove context block

--- spec/requests/early_access_entry_point_spec.rb
+++ spec/requests/early_access_entry_point_spec.rb
@@ -2,29 +2,27 @@ RSpec.describe "early access entry point" do
   let(:early_access_user) { create :early_access_user }
 
   describe "POST :create" do
-    context "when instant access is allowed for existing user" do
-      it "redirects to chat" do
+    it "redirects to chat" do
+      post early_access_entry_path(
+        form_early_access_entry: { email: early_access_user.email },
+      )
+      expect(response).to redirect_to(early_access_entry_email_sent_path)
+    end
+
+    it "creates a session" do
+      expect {
         post early_access_entry_path(
           form_early_access_entry: { email: early_access_user.email },
         )
-        expect(response).to redirect_to(early_access_entry_email_sent_path)
-      end
+      }.to change(Passwordless::Session, :count).by(1)
+    end
 
-      it "creates a session" do
-        expect {
-          post early_access_entry_path(
-            form_early_access_entry: { email: early_access_user.email },
-          )
-        }.to change(Passwordless::Session, :count).by(1)
-      end
-
-      it "sends an email" do
-        expect {
-          post early_access_entry_path(
-            form_early_access_entry: { email: early_access_user.email },
-          )
-        }.to change(EarlyAccessAuthMailer.deliveries, :count).by(1)
-      end
+    it "sends an email" do
+      expect {
+        post early_access_entry_path(
+          form_early_access_entry: { email: early_access_user.email },
+        )
+      }.to change(EarlyAccessAuthMailer.deliveries, :count).by(1)
     end
   end
 end
