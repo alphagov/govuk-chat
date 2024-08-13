c14936c7e5d580c4dbd3bf4c34579095ac4dc775
davidgisbey <david.gisbey@digital.cabinet-office.gov.uk>
2024-08-13 10:55:17 +0100

    Update EarlyAccessEntry form access controls functionality
    We're moving to a new signup flow where we want to handle the following
    situation when a user input their email address:
    
    - If the email address is already assoiciated with an account, we want to
    create a passworldless session for the user and email them a magic link
    - If the email address is not associated with an account, we want to
    retain the users email address in a session variable and redirect them
    to the first user question
    - If the email address is not valid, we want to show an error message
    - If the users access have been revoked, we want to redirect them to a
    revoked access page
    
    Due to this, we need to update the form to reflect the new flow. This:
    
    - adds validations for presence, char count and email format
    - adds a presence validation for the source of signup
    - adds a Result data class to handle the different outcomes of the form
    - updates the #submit method to return if the form is invalid
    - returns a Result object with the outcome of the form submission
    - only creates a session and sends a magic link email for existing users

--- spec/factories/early_access_user_factory.rb
+++ spec/factories/early_access_user_factory.rb
@@ -4,5 +4,9 @@ FactoryBot.define do
     source { "instant_signup" }
     user_description { "business_owner_or_self_employed" }
     reason_for_visit { "find_specific_answer" }
+
+    trait :revoked do
+      revoked_at { Time.zone.now }
+    end
   end
 end
--- spec/models/form/early_access_entry_spec.rb
+++ spec/models/form/early_access_entry_spec.rb
@@ -1,34 +1,57 @@
 RSpec.describe Form::EarlyAccessEntry do
+  describe "validations" do
+    it "is invalid without an email address" do
+      form = described_class.new(email: nil)
+      expect(form).to be_invalid
+      expect(form.errors.messages[:email])
+        .to eq([described_class::EMAIL_ADDRESS_PRESENCE_ERROR_MESSAGE])
+    end
+
+    it "is invalid if email is more than 512 characters" do
+      form = described_class.new(email: "#{'a' * 512}@example.com")
+      expect(form).to be_invalid
+      expect(form.errors.messages[:email])
+        .to eq([sprintf(described_class::EMAIL_ADDRESS_LENGTH_ERROR_MESSAGE, count: 512)])
+    end
+
+    it "is invalid if email address is not a valid email" do
+      form = described_class.new(email: "non.existent.email")
+      expect(form).to be_invalid
+      expect(form.errors.messages[:email])
+        .to eq([described_class::EMAIL_ADDRESS_FORMAT_ERROR_MESSAGE])
+    end
+  end
+
   describe "#submit" do
     before do
       allow(EarlyAccessAuthMailer).to receive(:sign_in).and_call_original
     end
 
-    context "when the user exists" do
-      let(:user) { create :early_access_user }
-      let(:form) { described_class.new(email: user.email, source: user.source) }
+    it "raises an error when the form object is invalid" do
+      form = described_class.new(email: "")
+      expect { form.submit }.to raise_error(ActiveModel::ValidationError)
+    end
 
-      it "creates a session" do
-        expect { form.submit }.to change(Passwordless::Session, :count).by(1)
+    context "when the user doesn't have an early access account" do
+      let(:form) do
+        described_class.new(email: "non.existent.email@example.com")
       end
 
-      it "assigns the early access user" do
-        form.submit
-        expect(Passwordless::Session.last.authenticatable).to eq(user)
-      end
-
-      it "calls the mailer with the new session" do
-        expect { form.submit }.to change(EarlyAccessAuthMailer.deliveries, :count).by(1)
-        created_session = Passwordless::Session.last
-        expect(EarlyAccessAuthMailer).to have_received(:sign_in).with(created_session)
+      it "returns a result object with the correct attributes" do
+        result = form.submit
+        expect(result)
+          .to be_a(described_class::Result)
+          .and have_attributes(
+            outcome: :new_user,
+            email: form.email,
+            user: nil,
+          )
       end
     end
 
-    ## TODO this will change - we will be asking some questions as eligibility check
-    context "with a new user" do
-      let(:form) do
-        described_class.new(email: "non.existent.email@example.com", source: "instant_signup")
-      end
+    context "when the user has an early access account" do
+      let(:user) { create :early_access_user, email: "existing.email@example.com" }
+      let(:form) { described_class.new(email: user.email) }
 
       it "creates a session and early access user" do
         expect { form.submit }.to change(Passwordless::Session, :count).by(1)
@@ -44,7 +67,7 @@ RSpec.describe Form::EarlyAccessEntry do
         form.submit
 
         expect(EarlyAccessUser.last).to have_attributes(
-          email: "non.existent.email@example.com",
+          email: "existing.email@example.com",
           source: "instant_signup",
         )
       end
@@ -53,6 +76,33 @@ RSpec.describe Form::EarlyAccessEntry do
         expect { form.submit }.to change(EarlyAccessAuthMailer.deliveries, :count).by(1)
         created_session = Passwordless::Session.last
         expect(EarlyAccessAuthMailer).to have_received(:sign_in).with(created_session)
+      end
+
+      it "returns a Result instance with the correct attributes" do
+        result = form.submit
+        expect(result)
+          .to be_a(described_class::Result)
+          .and have_attributes(
+            outcome: :existing_user,
+            email: user.email,
+            user:,
+          )
+      end
+    end
+
+    context "when the user has an account that has been revoked" do
+      let(:user) { create :early_access_user, :revoked }
+      let(:form) { described_class.new(email: user.email) }
+
+      it "returns a result object with the correct attributes" do
+        result = form.submit
+        expect(result)
+          .to be_a(described_class::Result)
+          .and have_attributes(
+            outcome: :user_revoked,
+            email: user.email,
+            user:,
+          )
       end
     end
   end
