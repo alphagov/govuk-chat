2540b2b8f0477b7b0afa6b943e5fc8f36e29ff32
davidgisbey <david.gisbey@digital.cabinet-office.gov.uk>
2024-08-13 19:30:01 +0100

    Add reason for visit form
    This handles taking the eligibility question choice and validating it's
    presence. If valid then it creates the new EarlyAccessUser, using the
    choice selected as the reason for visit.
    
    It takes two additional attributes, email and user description. I did
    think about adding validations for these but decided against it as they
    will always be passed into the form in the controller.
    
    If the values not present in the session then the user will be redirected
    to the correct endpoint so they won't ever not be present in the form.
    
    Once the user is created it creates a passswordless session and emails
    the user the magic link.

---
+++ spec/models/form/early_access/reason_for_visit_spec.rb
@@ -0,0 +1,59 @@
+RSpec.describe Form::EarlyAccess::ReasonForVisit do
+  describe "validations" do
+    it "is valid with a choice" do
+      form = described_class.new(choice: "find_specific_answer")
+      expect(form).to be_valid
+    end
+
+    it "is invalid without a choice" do
+      form = described_class.new(choice: "")
+      expect(form).to be_invalid
+      expect(form.errors.messages[:choice])
+        .to eq([described_class::CHOICE_PRESENCE_ERROR_MESSAGE])
+    end
+  end
+
+  describe "#submit" do
+    let(:form) do
+      described_class.new(
+        choice: "find_specific_answer",
+        email: "email@test.com",
+        user_description: "business_owner_or_self_employed",
+      )
+    end
+
+    before do
+      allow(EarlyAccessAuthMailer).to receive(:sign_in).and_call_original
+    end
+
+    it "raises an error when the form object is invalid" do
+      form = described_class.new(choice: "")
+      expect { form.submit }.to raise_error(ActiveModel::ValidationError)
+    end
+
+    it "creates an EarlyAccessUser with the correct attributes" do
+      expect { form.submit }.to change(EarlyAccessUser, :count).by(1)
+      expect(EarlyAccessUser.last).to have_attributes(
+        reason_for_visit: "find_specific_answer",
+        email: "email@test.com",
+        user_description: "business_owner_or_self_employed",
+        source: "instant_signup",
+      )
+    end
+
+    it "creates a session" do
+      expect { form.submit }.to change(Passwordless::Session, :count).by(1)
+    end
+
+    it "assigns the early access user to the session" do
+      form.submit
+      expect(Passwordless::Session.last.authenticatable).to eq(EarlyAccessUser.last)
+    end
+
+    it "calls the mailer with the new session" do
+      expect { form.submit }.to change(EarlyAccessAuthMailer.deliveries, :count).by(1)
+      created_session = Passwordless::Session.last
+      expect(EarlyAccessAuthMailer).to have_received(:sign_in).with(created_session)
+    end
+  end
+end
