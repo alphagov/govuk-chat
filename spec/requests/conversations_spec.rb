8126c56b6da80bf25db70a981b89faa0b7f6d628
James Chan <james.chan@digital.cabinet-office.gov.uk>
2024-08-18 21:49:17 +0100

    Make button type submit by default
    I tried options[:type] = "submit" unless href.present? to maintain consistency with how we're checking links in this file however the linter complained and wanted if href.blank?

--- spec/requests/conversations_spec.rb
+++ spec/requests/conversations_spec.rb
@@ -38,7 +38,7 @@ RSpec.describe "ConversationsController" do
             "fragment" => "start-chatting",
             "conversation_data" => { "module" => "chat-conversation" },
             "conversation_append_html" => /<p>Thanks! To get started, ask me a question.<\/p>/,
-            "form_html" => /<button class="app-c-blue-button govuk-button app-c-blue-button--conversation-form js-conversation-form-button">\s*Send\s*\n\s*<span class="govuk-visually-hidden">message to GOV.UK Chat<\/span>\n<\/button/,
+            "form_html" => /<button class="app-c-blue-button govuk-button app-c-blue-button--conversation-form js-conversation-form-button" type="submit">\s*Send\s*\n\s*<span class="govuk-visually-hidden">message to GOV.UK Chat<\/span>\n<\/button/,
           })
         end
       end
