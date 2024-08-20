9ab62ab622b486a3947b5e546ceeafa9794e5b9f
davidgisbey <david.gisbey@digital.cabinet-office.gov.uk>
2024-08-20 12:57:22 +0100

    Update early access error messsage content
    I ran the current flow by Aisling who has given me some updated
    content for these error messages.

--- app/models/form/early_access/user_description.rb
+++ app/models/form/early_access/user_description.rb
@@ -2,7 +2,7 @@ class Form::EarlyAccess::UserDescription
   include ActiveModel::Model
   include ActiveModel::Attributes
 
-  CHOICE_PRESENCE_ERROR_MESSAGE = "Choose which option best describes you".freeze
+  CHOICE_PRESENCE_ERROR_MESSAGE = "Select an option that best describes you".freeze
 
   attribute :choice
 
