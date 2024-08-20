f0e672ba11f1c681eb380b966e8a1b3616dffdf7
davidgisbey <david.gisbey@digital.cabinet-office.gov.uk>
2024-08-20 12:32:34 +0100

    Extend chat_introduction component to cover early access landing page
    We're goin to have 2 landing pages, 1 for heroku where the user doesn't
    need an account and 1 on production where a user is asked to input their
    email and go through a sign up flow.
    
    This extends the introduction component to work in both cases. It's
    a bit clunky as there's a few extra fields that need to be passed into
    the input (error items and value), but there's so much shared styling
    this felt like the lesser evil than a brand new component.
    
    I ran into an issue with the current implementation when errors were
    triggered. There was 100px applied to the wrapper which shoved it miles
    down the page.
    
    I've added an override class when errors are present which strips the
    top padding. This leaves 40px of margin on the bottom of the error summary
    component and looks decent to me. But it'll need a some FE eyes.
    
    Also the reason i've used
    'Rails.application.routes.url_helpers.early_access_entry_sign_in_or_up_path'
    for the form url is because the component guide blows up without it.

--- spec/views/components/_chat_introduction.html.erb_spec.rb
+++ spec/views/components/_chat_introduction.html.erb_spec.rb
@@ -11,4 +11,38 @@ RSpec.describe "components/_chat_introduction.html.erb" do
       .and have_link("Try GOV.UK Chat", href: "/start")
       .and have_selector("#info-text", text: "Your chat history will be available for #{max_days} days")
   end
+
+  context "when early_access is true" do
+    it "renders the chat introduction component with an email address form" do
+      render("components/chat_introduction", early_access: true)
+
+      expect(rendered)
+        .to have_selector(".app-c-chat-introduction")
+        .and have_selector(".app-c-chat-introduction__svg-container")
+        .and have_selector(".app-c-chat-introduction__title", text: "Try GOV.UK Chat")
+        .and have_selector(".app-c-chat-introduction__lead-paragraph", text: "An experimental new way to find answers to your business questions, powered by AI")
+        .and have_selector(".app-c-chat-introduction__form")
+        .and have_selector(".app-c-chat-introduction__form label", text: "To get started, enter your email address.")
+        .and have_selector(".app-c-chat-introduction__form input[name='sign_in_or_up_form[email]']")
+        .and have_selector(".app-c-blue-button[type='submit']", text: "Get started")
+        .and have_selector("#info-text", text: "If you’ve already signed up, you’ll be able to request new links to access GOV.UK Chat.")
+    end
+
+    it "renders the email input with error items when 'input_error_items' are passed in" do
+      render("components/chat_introduction", {
+        early_access: true,
+        input_error_items: [{ text: "Enter an email address" }],
+      })
+
+      expect(rendered)
+        .to have_selector(".app-c-chat-introduction__form .govuk-error-message", text: /Enter an email address/)
+    end
+
+    it "sets the value of the email address input when an input_value is passed in" do
+      render("components/chat_introduction", early_access: true, input_value: "test@email.com")
+
+      expect(rendered)
+        .to have_selector(".app-c-chat-introduction__form input[value='test@email.com']")
+    end
+  end
 end
