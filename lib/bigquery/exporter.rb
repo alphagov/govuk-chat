668d3e48c94573bb5ca4dbf95a223cbfa80e7bfa
Chae Cramb <chae.cramb@digital.cabinet-office.gov.uk>
2024-08-21 15:57:45 +0100

    Add bigquery exporter class
    `Bigquery::Exporter.call` will query for all new records since the last export, upload
    them to BigQuery and log a new export in the `BigqueryExport` table.
    
    On first upload to BigQuery it will create and configure relevant tables if they don't
    already exist.

---
+++ lib/bigquery/exporter.rb
@@ -0,0 +1,83 @@
+require "google/cloud/bigquery"
+
+module Bigquery
+  class Exporter
+    class UploadFailedError < StandardError; end
+
+    def self.call(...) = new(...).call
+
+    def initialize
+      bigquery = Google::Cloud::Bigquery.new
+      dataset_id = Rails.configuration.bigquery_dataset_id
+      @dataset = bigquery.dataset(dataset_id)
+    end
+
+    def call
+      last_export = BigqueryExport.last || BigqueryExport.create!(exported_until: "2024-01-01")
+      exported = {}
+      last_export.with_lock do
+        exported[:from] = last_export.exported_until
+        exported[:until] = Time.current
+        exported[:tables] = export(exported[:from], exported[:until])
+        BigqueryExport.create!(exported_until: exported[:until])
+      end
+      exported
+    end
+
+  private
+
+    attr_reader :dataset
+
+    def export(export_from, export_until)
+      exported_records = {}
+      TOP_LEVEL_MODELS_TO_EXPORT.each do |model|
+        records_to_export = model.exportable(export_from, export_until).map(&:serialize_for_export)
+        table_id = model.table_name
+        exported_records[table_id.to_sym] = records_to_export.size
+
+        next unless records_to_export.any?
+
+        tempfile = save_tables_to_json(records_to_export)
+        upload_to_bigquery(tempfile, table_id)
+      end
+      exported_records
+    end
+
+    def save_tables_to_json(records_to_export)
+      tempfile = Tempfile.new
+
+      records_to_export.each do |record|
+        tempfile.puts(record.to_json)
+      end
+
+      tempfile.rewind
+      tempfile
+    end
+
+    def upload_to_bigquery(tempfile, table_id)
+      table = dataset.table(table_id)
+      create_bigquery_table(table_id) unless table
+
+      load_job = dataset.load_job(table_id, tempfile) do |config|
+        config.autodetect = true
+        config.format = "json"
+        config.schema_update_options = %w[ALLOW_FIELD_ADDITION ALLOW_FIELD_RELAXATION]
+      end
+
+      load_job.wait_until_done!
+
+      raise UploadFailedError, "BigQuery upload failed: #{load_job.error}" if load_job.failed?
+    end
+
+    def create_bigquery_table(table_id)
+      expiration_duration = 1.year.to_i
+
+      dataset.create_table(table_id) do |table|
+        table.schema { |schema| schema.timestamp "created_at", mode: :required }
+        table.time_partitioning_type = "DAY"
+        table.time_partitioning_field = "created_at"
+        table.time_partitioning_expiration = expiration_duration
+      end
+    end
+  end
+end
