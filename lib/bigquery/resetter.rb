f50ce2ad8c090c7a7557633262d4c4d8af487bed
Chae Cramb <chae.cramb@digital.cabinet-office.gov.uk>
2024-08-21 15:56:28 +0100

    Add bigquery resetter class
    Adds a class to completely reset the state of BigQuery exports, primarily for use in
    development and testing.
    
    `Bigquery::Resetter.call` will delete all records in the `BigqueryExport` table,
    as well as delete all tables that have been created in the associated BigQuery dataset
    in Google Cloud.

---
+++ lib/bigquery/resetter.rb
@@ -0,0 +1,26 @@
+require "google/cloud/bigquery"
+
+module Bigquery
+  class Resetter
+    def self.call(...) = new(...).call
+
+    def initialize
+      bigquery = Google::Cloud::Bigquery.new
+      dataset_id = Rails.configuration.bigquery_dataset_id
+      @dataset = bigquery.dataset(dataset_id)
+    end
+
+    def call
+      TOP_LEVEL_MODELS_TO_EXPORT.each do |model|
+        table_id = model.table_name
+
+        dataset.table(table_id)&.delete
+        BigqueryExport.delete_all
+      end
+    end
+
+  private
+
+    attr_reader :dataset
+  end
+end
