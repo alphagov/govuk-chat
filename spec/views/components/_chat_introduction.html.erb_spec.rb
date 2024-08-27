2f4ec6b4a7684e63eab41fb79b57ca9c650af941
James Chan <james.chan@digital.cabinet-office.gov.uk>
2024-08-27 23:18:56 +0100

    Store form url and start button href in variables

--- spec/views/components/_chat_introduction.html.erb_spec.rb
+++ spec/views/components/_chat_introduction.html.erb_spec.rb
@@ -1,6 +1,6 @@
 RSpec.describe "components/_chat_introduction.html.erb" do
   it "renders the chat introduction component correctly" do
-    render("components/chat_introduction", start_button_href: "/start")
+    render("components/chat_introduction")
 
     max_days = Rails.configuration.conversations.max_question_age_days
     expect(rendered)
@@ -8,7 +8,7 @@ RSpec.describe "components/_chat_introduction.html.erb" do
       .and have_selector(".app-c-chat-introduction__svg-container")
       .and have_selector(".app-c-chat-introduction__title", text: "GOV.UK Chat")
       .and have_selector(".app-c-chat-introduction__lead-paragraph", text: "An experimental new way to find answers to your business questions, powered by AI")
-      .and have_link("Try GOV.UK Chat", href: "/start")
+      .and have_link("Try GOV.UK Chat")
       .and have_selector("#info-text", text: "Your chat history will be available for #{max_days} days")
   end
 
