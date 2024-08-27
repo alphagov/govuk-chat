237b45a4f49614914e632467e693e6c86b36cebd
Jack Weeden <jack@ajack.org>
2024-08-27 12:42:02 +0100

    Add existing user validation when creating WaitingListUser
    This ensures a pilot user doesn't already exist - either a
    `WaitingListUser` or an `EarlyAccessUser`.

--- app/models/admin/form/waiting_list_users/create_waiting_list_user_form.rb
+++ app/models/admin/form/waiting_list_users/create_waiting_list_user_form.rb
@@ -9,10 +9,19 @@ class Admin::Form::WaitingListUsers::CreateWaitingListUserForm
   validates :email, presence: { message: "Enter an email address" }
   validates :email, format: { with: URI::MailTo::EMAIL_REGEXP, message: "Enter a valid email address" },
                     if: -> { email.present? }
+  validate :pilot_user_does_not_exist, if: -> { email.present? }
 
   def submit
     validate!
 
     WaitingListUser.create!(email:, user_description:, reason_for_visit:, source: :admin_added)
+  end
+
+private
+
+  def pilot_user_does_not_exist
+    return unless WaitingListUser.exists?(email:) || EarlyAccessUser.exists?(email:)
+
+    errors.add(:email, "There is already a pilot user with this email address")
   end
 end
--- spec/models/admin/form/waiting_list_users/create_waiting_list_user_form_spec.rb
+++ spec/models/admin/form/waiting_list_users/create_waiting_list_user_form_spec.rb
@@ -15,6 +15,26 @@ RSpec.describe Admin::Form::WaitingListUsers::CreateWaitingListUserForm do
       expect(form.errors.count).to eq(1)
       expect(form.errors.messages[:email]).to eq(["Enter a valid email address"])
     end
+
+    it "returns false if there is already a waiting list user with the same email" do
+      create(:waiting_list_user, email: "foo@bar.com")
+
+      form = described_class.new(email: "foo@bar.com")
+
+      expect(form).to be_invalid
+      expect(form.errors.count).to eq(1)
+      expect(form.errors.messages[:email]).to eq(["There is already a pilot user with this email address"])
+    end
+
+    it "returns false if there is already an early access user with the same email" do
+      create(:early_access_user, email: "foo@bar.com")
+
+      form = described_class.new(email: "foo@bar.com")
+
+      expect(form).to be_invalid
+      expect(form.errors.count).to eq(1)
+      expect(form.errors.messages[:email]).to eq(["There is already a pilot user with this email address"])
+    end
   end
 
   describe "#submit" do
