b351a46b380c1b1f475b289630b50ed39f986ecb
James Chan <james.chan@digital.cabinet-office.gov.uk>
2024-08-27 22:56:45 +0100

    Update test

--- spec/views/components/_chat_introduction.html.erb_spec.rb
+++ spec/views/components/_chat_introduction.html.erb_spec.rb
@@ -18,14 +18,12 @@ RSpec.describe "components/_chat_introduction.html.erb" do
 
       expect(rendered)
         .to have_selector(".app-c-chat-introduction")
-        .and have_selector(".app-c-chat-introduction__svg-container")
-        .and have_selector(".app-c-chat-introduction__title", text: "Try GOV.UK Chat")
-        .and have_selector(".app-c-chat-introduction__lead-paragraph", text: "An experimental new way to find answers to your business questions, powered by AI")
+        .and have_text("Try GOV.UK Chat")
         .and have_selector(".app-c-chat-introduction__form")
         .and have_selector(".app-c-chat-introduction__form label", text: "To get started, enter your email address.")
         .and have_selector(".app-c-chat-introduction__form input[name='sign_in_or_up_form[email]']")
         .and have_selector(".app-c-blue-button[type='submit']", text: "Get started")
-        .and have_selector("#info-text", text: "If you’ve already signed up, you’ll be able to request new links to access GOV.UK Chat.")
+        .and have_text("If you’ve already signed up, you’ll be able to request new links to access GOV.UK Chat.")
     end
 
     it "renders the email input with error items when 'input_error_items' are passed in" do
