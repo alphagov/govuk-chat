c47633fdc9658f0d196c8b30104fa4f92464017d
Kevin Dew <kevindew@me.com>
2024-08-28 09:34:11 +0100

    Remove nested module for passwordless request helpers
    I had created the nested RequestSpecHelpers module to allow for
    extensibility with System specs. However we approached this a different
    way and didn't use it. Therefore I've removed the nesting and renamed
    the module.

--- spec/support/passwordless_helpers.rb
+++ spec/support/passwordless_helpers.rb
@@ -1,17 +1,15 @@
-module PasswordlessHelpers
-  module RequestSpecHelpers
-    shared_context "when signed in" do
-      let(:user) { create(:early_access_user) }
-      before { sign_in_early_access_user(user) }
-    end
+module PasswordlessRequestHelpers
+  shared_context "when signed in" do
+    let(:user) { create(:early_access_user) }
+    before { sign_in_early_access_user(user) }
+  end
 
-    def sign_in_early_access_user(early_access_user)
-      session = create(:passwordless_session, authenticatable: early_access_user)
+  def sign_in_early_access_user(early_access_user)
+    session = create(:passwordless_session, authenticatable: early_access_user)
 
-      magic_link = magic_link_path(session.to_param, session.token)
+    magic_link = magic_link_path(session.to_param, session.token)
 
-      get(magic_link)
-      follow_redirect!
-    end
+    get(magic_link)
+    follow_redirect!
   end
 end
