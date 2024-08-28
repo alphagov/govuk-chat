38715cdb833b800fa940f617ca99cc6c1771fd97
Jack Weeden <jack@ajack.org>
2024-08-28 16:18:40 +0100

    Add system spec for waiting list unsubscribing

---
+++ spec/system/waiting_list_user_unsubscribes_spec.rb
@@ -0,0 +1,25 @@
+RSpec.describe "Waiting list user unsubscribes" do
+  scenario do
+    given_i_have_signed_up_for_the_waiting_list
+    when_i_unsubscribe
+    then_my_user_is_deleted
+    and_i_see_a_confirmation_page
+  end
+
+  def given_i_have_signed_up_for_the_waiting_list
+    @user = create(:waiting_list_user)
+  end
+
+  def when_i_unsubscribe
+    visit waiting_list_user_unsubscribe_path(@user.id, @user.unsubscribe_token)
+    click_button "Unsubscribe"
+  end
+
+  def then_my_user_is_deleted
+    expect(WaitingListUser.exists?(@user.id)).to be(false)
+  end
+
+  def and_i_see_a_confirmation_page
+    expect(page).to have_content("You have been removed from the waiting list")
+  end
+end
