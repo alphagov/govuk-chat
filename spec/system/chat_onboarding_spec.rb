197390612e079e4f375f0cae6f2275326faccc77
Daniel Karaj <daniel.karaj@digital.cabinet-office.gov.uk>
2024-08-29 00:25:26 +0100

    Finalise onboarding messages
    - Use govspeak to write all onboarding messages.
    - Separate out the first message into three paragraphs
    - Make the last paragraph bold for readability and emphasis.
    - If a user has asked for more information, do not display the limitations link with the privacy message.
    - Use rails methods to get paths for about and privacy links

--- spec/system/chat_onboarding_spec.rb
+++ spec/system/chat_onboarding_spec.rb
@@ -2,10 +2,9 @@ RSpec.describe "Chat Onboarding" do
   scenario "without JS" do
     given_i_am_a_signed_in_early_access_user
     when_i_visit_the_onboarding_limitations_page
-    and_i_click_tell_me_more
-    then_i_see_additional_information
 
     when_i_click_that_i_understand
+    then_i_see_the_onboarding_limitations_page
     then_i_see_the_onboarding_privacy_page
 
     when_i_click_on_the_start_chatting_button
@@ -15,10 +14,37 @@ RSpec.describe "Chat Onboarding" do
   scenario "with JS", :dismiss_cookie_banner, :js do
     given_i_am_a_signed_in_early_access_user
     when_i_visit_the_onboarding_limitations_page
+
+    when_i_click_that_i_understand
+    then_i_see_the_onboarding_limitations_page
+    then_i_see_the_onboarding_privacy_page
+
+    when_i_click_on_the_start_chatting_button
+    then_i_see_the_chat_prompt
+  end
+
+  scenario "tell me more without JS" do
+    given_i_am_a_signed_in_early_access_user
+    when_i_visit_the_onboarding_limitations_page
     and_i_click_tell_me_more
     then_i_see_additional_information
 
     when_i_click_that_i_understand
+    then_i_do_not_see_the_onboarding_limitations_page
+    then_i_see_the_onboarding_privacy_page
+
+    when_i_click_on_the_start_chatting_button
+    then_i_see_the_conversation_page
+  end
+
+  scenario "tell me more with JS", :dismiss_cookie_banner, :js do
+    given_i_am_a_signed_in_early_access_user
+    when_i_visit_the_onboarding_limitations_page
+    and_i_click_tell_me_more
+    then_i_see_additional_information
+
+    when_i_click_that_i_understand
+    then_i_do_not_see_the_onboarding_limitations_page
     then_i_see_the_onboarding_privacy_page
 
     when_i_click_on_the_start_chatting_button
@@ -41,8 +67,16 @@ RSpec.describe "Chat Onboarding" do
     click_on "I understand"
   end
 
-  def then_i_see_the_onboarding_privacy_page
+  def then_i_see_the_onboarding_limitations_page
     expect(page).to have_content(/Great. You can always find information about my limitations in about GOV.UK Chat/)
+  end
+
+  def then_i_do_not_see_the_onboarding_limitations_page
+    expect(page).not_to have_content(/Great. You can always find information about my limitations in about GOV.UK Chat/)
+  end
+
+  def then_i_see_the_onboarding_privacy_page
+    expect(page).to have_content(/One more thing - please do not include any sensitive or personal information in your questions./)
   end
 
   def when_i_click_on_the_start_chatting_button
