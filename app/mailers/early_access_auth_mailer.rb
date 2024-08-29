d68d9e9426e12e785932d2a173472627b2035ff7
Pete Goddard <peter.goddard@digital.cabinet-office.gov.uk>
2024-08-29 13:11:18 +0100

    Add mailer for user promoted from waitlist

--- app/mailers/early_access_auth_mailer.rb
+++ app/mailers/early_access_auth_mailer.rb
@@ -7,4 +7,11 @@ class EarlyAccessAuthMailer < ApplicationMailer
   def waitlist(user)
     view_mail(template_id, to: user.email, subject: "Thanks for joining the waitlist")
   end
+
+  def waitlist_promoted(session)
+    @session_timeout_days = 30
+    @magic_link_timeout_hours = 24
+    @magic_link = magic_link_url(session.to_param, session.token)
+    view_mail(template_id, to: session.authenticatable.email, subject: "You can now access GOV.UK Chat")
+  end
 end
