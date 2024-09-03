a68ad92be34f2876f29375a53a71d57860839ade
Jack Weeden <jack@ajack.org>
2024-09-03 15:01:29 +0100

    Waiting list: one-click unsubscribe
    It's been decided that "unsubscribing" from the waiting list should be a
    one-click thing, rather than first taking the user to a confirmation
    page where they click a button to actually confirm.
    
    This removes the `POST` endpoint and essentially moves that logic into
    the `GET` endpoint instead.
    
    We need to return early for `HEAD` requests as typically email clients
    will make this request and we don't want to unsubscribe people when all
    they've done is open the email.

--- spec/requests/waiting_list_unsubscribe_spec.rb
+++ spec/requests/waiting_list_unsubscribe_spec.rb
@@ -1,37 +1,44 @@
 RSpec.describe "WaitingListUnsubscribeController" do
-  shared_examples "renders a 404 for invalid params" do |method, path|
+  describe "GET :unsubscribe" do
     it "returns a 404 if user ID is invalid" do
-      process(method.to_sym, public_send(path.to_sym, -1, "token"))
+      get waiting_list_user_unsubscribe_path(-1, "token")
+
       expect(response).to have_http_status(:not_found)
     end
 
     it "returns a 404 if the token is invalid" do
       user = create(:waiting_list_user)
-      process(method.to_sym, public_send(path.to_sym, user.id, "invalid-token"))
+
+      get waiting_list_user_unsubscribe_path(user.id, "invalid-token")
+
       expect(response).to have_http_status(:not_found)
     end
-  end
 
-  describe "GET :unsubscribe" do
-    it_behaves_like "renders a 404 for invalid params", :get, :waiting_list_user_unsubscribe_path
-
-    it "renders the unsubscribe page if the token is valid" do
+    it "does not unsubscribe for a HEAD request" do
       user = create(:waiting_list_user)
-      get waiting_list_user_unsubscribe_path(user.id, user.unsubscribe_token)
 
-      expect(response).to have_http_status(:success)
-      expect(response.body).to have_content "Unsubscribe"
+      head waiting_list_user_unsubscribe_path(user.id, user.unsubscribe_token)
+
+      expect(WaitingListUser.exists?(user.id)).to be(true)
+      expect(response).to have_http_status(:ok)
+      expect(response.body).to be_empty
     end
-  end
 
-  describe "POST :unsubscribe_confirm" do
-    it_behaves_like "renders a 404 for invalid params", :post, :waiting_list_user_unsubscribe_confirm_path
+    context "when the token is valid" do
+      let(:user) { create(:waiting_list_user) }
 
-    it "deletes the user if the token is valid" do
-      user = create(:waiting_list_user)
-      post waiting_list_user_unsubscribe_confirm_path(user.id, user.unsubscribe_token)
+      it "deletes the user" do
+        get waiting_list_user_unsubscribe_path(user.id, user.unsubscribe_token)
 
-      expect(WaitingListUser.exists?(user.id)).to be(false)
+        expect(WaitingListUser.exists?(user.id)).to be(false)
+      end
+
+      it "renders the unsubscribe page" do
+        get waiting_list_user_unsubscribe_path(user.id, user.unsubscribe_token)
+
+        expect(response).to have_http_status(:ok)
+        expect(response.body).to have_content "You’ve opted out of GOV.UK Chat"
+      end
     end
   end
 end
