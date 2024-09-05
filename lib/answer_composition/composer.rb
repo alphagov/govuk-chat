d0fc8fdf872c672ecbe63a3db0da7942ec0bd81a
Jack Weeden <jack@ajack.org>
2024-09-05 15:27:53 +0100

    Extract answer composition into its own method

--- lib/answer_composition/composer.rb
+++ lib/answer_composition/composer.rb
@@ -11,31 +11,11 @@ module AnswerComposition
     def call
       start_time = AnswerComposition.monotonic_time
 
-      answer = case answer_strategy
-               when "open_ai_rag_completion"
-                 OpenAIAnswer.call(question:, pipeline: [
-                   Pipeline::QuestionRephraser,
-                   Pipeline::SearchResultFetcher,
-                   Pipeline::OpenAIUnstructuredAnswerComposer,
-                   Pipeline::OutputGuardrails,
-                 ])
-               when "openai_structured_answer"
-                 OpenAIAnswer.call(question:, pipeline: [
-                   Pipeline::QuestionRephraser,
-                   Pipeline::QuestionRouter,
-                   Pipeline::SearchResultFetcher,
-                   Pipeline::OpenAIStructuredAnswerComposer,
-                   Pipeline::OutputGuardrails,
-                 ])
-               else
-                 raise "Answer strategy #{answer_strategy} not configured"
-               end
-
-      answer.assign_metrics("answer_composition", {
-        duration: AnswerComposition.monotonic_time - start_time,
-      })
-
-      answer
+      compose_answer.tap do |answer|
+        answer.assign_metrics("answer_composition", {
+          duration: AnswerComposition.monotonic_time - start_time,
+        })
+      end
     rescue StandardError => e
       GovukError.notify(e)
       question.build_answer(
@@ -48,5 +28,27 @@ module AnswerComposition
   private
 
     attr_reader :question
+
+    def compose_answer
+      case answer_strategy
+      when "open_ai_rag_completion"
+        OpenAIAnswer.call(question:, pipeline: [
+          Pipeline::QuestionRephraser,
+          Pipeline::SearchResultFetcher,
+          Pipeline::OpenAIUnstructuredAnswerComposer,
+          Pipeline::OutputGuardrails,
+        ])
+      when "openai_structured_answer"
+        OpenAIAnswer.call(question:, pipeline: [
+          Pipeline::QuestionRephraser,
+          Pipeline::QuestionRouter,
+          Pipeline::SearchResultFetcher,
+          Pipeline::OpenAIStructuredAnswerComposer,
+          Pipeline::OutputGuardrails,
+        ])
+      else
+        raise "Answer strategy #{answer_strategy} not configured"
+      end
+    end
   end
 end
