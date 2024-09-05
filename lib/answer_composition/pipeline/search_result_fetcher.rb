c644feb8fe9387601cb3d798fc2c9ff10c99f03b
Jack Weeden <jack@ajack.org>
2024-09-05 08:42:30 +0100

    Assign metrics to answer on search results fetcher

--- lib/answer_composition/pipeline/search_result_fetcher.rb
+++ lib/answer_composition/pipeline/search_result_fetcher.rb
@@ -10,13 +10,18 @@ module AnswerComposition
       end
 
       def call
+        start_time = context.current_time
+
         if search_results.blank?
           context.abort_pipeline!(
             message: Answer::CannedResponses::NO_CONTENT_FOUND_REPONSE,
             status: "abort_no_govuk_content",
+            metrics: { "search_results" => build_metrics(start_time) },
           )
         else
           context.search_results = search_results
+
+          context.answer.assign_metrics("search_results", build_metrics(start_time))
         end
       end
 
@@ -26,6 +31,10 @@ module AnswerComposition
 
       def search_results
         @search_results ||= Search::ResultsForQuestion.call(question_message).results
+      end
+
+      def build_metrics(start_time)
+        { duration: context.current_time - start_time }
       end
     end
   end
