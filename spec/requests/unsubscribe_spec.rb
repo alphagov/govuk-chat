4fd22a216fa3d9d40a5bc3cb0f9540d3fae4a215
Kevin Dew <kevindew@me.com>
2024-09-09 22:13:33 +0100

    Combine unsubscribe controllers
    These controllers were very similar in functionality and both had
    somewhat unconventional names.
    
    I've combined them so their functionality is grouped in the same place
    and given them a more conventional name.

---
+++ spec/requests/unsubscribe_spec.rb
@@ -0,0 +1,84 @@
+RSpec.describe "UnsubscribeController" do
+  shared_examples "returns 404 for invalid user id or token" do |path|
+    it "returns a 404 if user id is invalid" do
+      get public_send(path, SecureRandom.uuid, user.unsubscribe_token)
+
+      expect(response).to have_http_status(:not_found)
+    end
+
+    it "returns a 404 if the token is invalid" do
+      get waiting_list_user_unsubscribe_path(user.id, "invalid-token")
+      get public_send(path.to_sym, user.id, SecureRandom.uuid)
+
+      expect(response).to have_http_status(:not_found)
+    end
+  end
+
+  describe "HEAD :waiting_list_user" do
+    it "returns a success without unsubscribing the user" do
+      user = create(:waiting_list_user)
+
+      expect { head waiting_list_user_unsubscribe_path(user.id, user.unsubscribe_token) }
+        .not_to change(WaitingListUser, :count)
+
+      expect(response).to have_http_status(:ok)
+    end
+  end
+
+  describe "GET :waiting_list_user" do
+    include_examples "returns 404 for invalid user id or token", :waiting_list_user_unsubscribe_path do
+      let(:user) { create(:waiting_list_user) }
+    end
+
+    context "when the token is valid" do
+      let(:user) { create(:waiting_list_user) }
+
+      it "deletes the user" do
+        get waiting_list_user_unsubscribe_path(user.id, user.unsubscribe_token)
+
+        expect(WaitingListUser.exists?(user.id)).to be(false)
+      end
+
+      it "renders a confirmation view" do
+        get waiting_list_user_unsubscribe_path(user.id, user.unsubscribe_token)
+
+        expect(response).to have_http_status(:ok)
+        expect(response.body).to have_content("You’ve been removed from the waitlist")
+      end
+    end
+  end
+
+  describe "HEAD :early_access_user" do
+    it "returns a success without unsubscribing the user" do
+      user = create(:early_access_user)
+
+      expect { head early_access_user_unsubscribe_path(user.id, user.unsubscribe_token) }
+        .not_to change(EarlyAccessUser, :count)
+
+      expect(response).to have_http_status(:ok)
+    end
+  end
+
+  describe "GET :early_access_user" do
+    include_examples "returns 404 for invalid user id or token", :early_access_user_unsubscribe_path do
+      let(:user) { create(:early_access_user) }
+    end
+
+    context "when the token is valid" do
+      let(:user) { create(:early_access_user) }
+
+      it "deletes the user" do
+        get early_access_user_unsubscribe_path(user.id, user.unsubscribe_token)
+
+        expect(EarlyAccessUser.exists?(user.id)).to be(false)
+      end
+
+      it "renders a confirmation view" do
+        get early_access_user_unsubscribe_path(user.id, user.unsubscribe_token)
+
+        expect(response).to have_http_status(:ok)
+        expect(response.body).to have_content("Thanks for helping to test GOV.UK Chat")
+      end
+    end
+  end
+end
