1414386690e8cd1677c68ef6158d2191143c5f53
Chae Cramb <chae.cramb@digital.cabinet-office.gov.uk>
2024-09-09 14:56:00 +0100

    Remove flipper tables
    Removes the DB tables associated with the flipper gem which was
    already removed in a previous commit.

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.2].define(version: 2024_09_06_090730) do
+ActiveRecord::Schema[7.2].define(version: 2024_09_09_134950) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "citext"
   enable_extension "pgcrypto"
@@ -125,22 +125,6 @@ ActiveRecord::Schema[7.2].define(version: 2024_09_06_090730) do
     t.integer "question_limit"
     t.string "unsubscribe_access_token", default: -> { "gen_random_uuid()" }, null: false
     t.index ["email"], name: "index_early_access_users_on_email", unique: true
-  end
-
-  create_table "flipper_features", force: :cascade do |t|
-    t.string "key", null: false
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-    t.index ["key"], name: "index_flipper_features_on_key", unique: true
-  end
-
-  create_table "flipper_gates", force: :cascade do |t|
-    t.string "feature_key", null: false
-    t.string "key", null: false
-    t.text "value"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-    t.index ["feature_key", "key", "value"], name: "index_flipper_gates_on_feature_key_and_key_and_value", unique: true
   end
 
   create_table "passwordless_sessions", id: :uuid, default: -> { "gen_random_uuid()" }, force: :cascade do |t|
