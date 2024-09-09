ca75fdcc56e142d40efd96764689ef473a7d4d0c
Kevin Dew <kevindew@me.com>
2024-09-09 22:53:57 +0100

    Refactor unsubscribe system tests
    This replaces the distinct unsubscribe system tests with scenarios in
    the sign ups spec. The motivation for this is to make these tests more
    thorough by working from the email rather than a route and to increase
    consistency across our system tests.
    
    I had to add in the unsubscribe link to the sign_in email so that the
    test will work, however I know this will be shortly be removed as per
    [1] and this may need rebasing depending on what is merged first.
    
    There is also another conflict looming with [2] where we're splitting up
    the sign-up and sign-in tests. So may need to rebase against that also.
    
    [1]: https://github.com/alphagov/govuk-chat/pull/655
    [2]: https://github.com/alphagov/govuk-chat/pull/652

--- spec/system/sign_ups_spec.rb
+++ spec/system/sign_ups_spec.rb
@@ -24,6 +24,18 @@ RSpec.describe "Sign ups" do
     then_i_arrive_on_the_onboarding_limitations_page
   end
 
+  scenario "early access user unsubscribes" do
+    given_sign_ups_are_enabled
+    and_i_am_a_returning_user
+
+    when_i_visit_the_homepage
+    and_i_enter_my_email_address
+    then_i_am_told_i_have_been_sent_an_email
+
+    when_i_click_the_unsubscribe_link_in_the_email
+    then_i_see_my_early_access_account_has_been_removed
+  end
+
   scenario "revoked user attempts to sign in" do
     given_sign_ups_are_enabled
     and_i_am_a_returning_user_with_revoked_access
@@ -53,6 +65,20 @@ RSpec.describe "Sign ups" do
 
     then_i_am_told_i_have_been_added_to_the_waitlist
     and_i_receive_an_email_telling_me_i_am_on_the_waitlist
+  end
+
+  scenario "waiting list user unsubscribes" do
+    given_sign_ups_are_enabled
+    and_there_are_no_instant_access_places_available
+
+    when_i_visit_the_homepage
+    and_i_enter_my_email_address
+    and_i_choose_my_description
+    and_i_choose_my_reason_for_visit
+    then_i_am_told_i_have_been_added_to_the_waitlist
+
+    when_i_click_the_unsubscribe_link_in_the_email
+    then_i_see_my_waiting_list_place_has_been_removed
   end
 
   scenario "answering the first question incorrectly" do
@@ -101,6 +127,11 @@ RSpec.describe "Sign ups" do
     visit email_links.first
   end
 
+  def when_i_click_the_unsubscribe_link_in_the_email
+    email_links = extract_links_from_last_email
+    visit email_links.last
+  end
+
   def then_i_arrive_on_the_onboarding_limitations_page
     expect(page).to have_content("Introduction to GOV.UK Chat and its limitations")
   end
@@ -113,6 +144,16 @@ RSpec.describe "Sign ups" do
   def and_i_am_a_returning_user_with_revoked_access
     user = create(:early_access_user, :revoked)
     @email = user.email
+  end
+
+  def then_i_see_my_early_access_account_has_been_removed
+    expect(page).to have_content("Your access has been removed")
+    expect(EarlyAccessUser.exists?(email: @email)).to be(false)
+  end
+
+  def then_i_see_my_waiting_list_place_has_been_removed
+    expect(page).to have_content("You’ve been removed from the waitlist")
+    expect(WaitingListUser.exists?(email: @email)).to be(false)
   end
 
   def then_i_am_told_i_do_not_have_access
