f090152cdd24ec0fac045d8726e5e8a69a22a3cb
Jack Weeden <jack@ajack.org>
2024-09-10 15:40:13 +0100

    Move auth spec into sign_in_spec

--- spec/system/sign_in_spec.rb
+++ spec/system/sign_in_spec.rb
@@ -36,6 +36,19 @@ RSpec.describe "Sign in" do
     then_i_see_my_early_access_account_has_been_removed
   end
 
+  scenario "returning user signs in and can clear chat" do
+    given_i_am_not_signed_in
+    and_i_have_an_active_conversation_with_an_answered_question
+
+    when_i_sign_in_via_magic_link
+    and_i_choose_to_start_a_new_chat
+    then_i_cannot_see_my_previous_questions_and_answer
+  end
+
+  def given_i_am_not_signed_in
+    @user = create :early_access_user
+  end
+
   def given_sign_ups_are_enabled
     @settings = create(:settings, sign_up_enabled: true)
   end
@@ -101,5 +114,27 @@ RSpec.describe "Sign in" do
   def then_i_see_my_early_access_account_has_been_removed
     expect(page).to have_content("Your access has been removed")
     expect(EarlyAccessUser.exists?(email: @email)).to be(false)
+  end
+
+  def and_i_have_an_active_conversation_with_an_answered_question
+    @conversation = create(:conversation, user: @user)
+    set_rack_cookie(:conversation_id, @conversation.id)
+    answer = build(:answer, message: "Example answer")
+    create(:question, answer:, conversation: @conversation, message: "Example question")
+  end
+
+  def when_i_sign_in_via_magic_link
+    session = create :passwordless_session, authenticatable: @user
+    magic_link = magic_link_path(session.to_param, session.token)
+    visit magic_link
+  end
+
+  def and_i_choose_to_start_a_new_chat
+    click_button "Start a new chat (clears history)"
+  end
+
+  def then_i_cannot_see_my_previous_questions_and_answer
+    expect(page).not_to have_content("Example question")
+    expect(page).not_to have_content("Example answer")
   end
 end
