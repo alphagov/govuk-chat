c1f34e74e399fc04b6e68f2e964c065cd96b9e62
Kevin Dew <kevindew@me.com>
2024-09-19 15:12:23 +0100

    Add user stats to admin homepage
    This exposes counts of the amounts of users currently in the database
    and number of users deleted.
    
    This is to provide an overall insight into how many users have been
    involved in the pilot.
    
    As this relies on count queries there is a risk that this could become
    slow if any of the tables involved becomes particularly large. I imagine
    the queries that utilise enums could be sped up with indexes when there
    is a lot of data.

--- app/controllers/admin/homepage_controller.rb
+++ app/controllers/admin/homepage_controller.rb
@@ -1,7 +1,34 @@
 class Admin::HomepageController < Admin::BaseController
   def index
     @questions_total = Question.count
-    @early_access_users_total = EarlyAccessUser.count
-    @waiting_list_users_total = WaitingListUser.count
+    early_access_users_current = EarlyAccessUser.count
+    waiting_list_users_current = WaitingListUser.count
+
+    unsubscribed_early_access_users = DeletedEarlyAccessUser.deletion_type_unsubscribe.count
+    admin_deleted_early_access_users = DeletedEarlyAccessUser.deletion_type_admin.count
+
+    @early_access_user_stats = {
+      current: early_access_users_current,
+      unsubscribed: unsubscribed_early_access_users,
+      admin_deleted: admin_deleted_early_access_users,
+      total: [early_access_users_current,
+              unsubscribed_early_access_users,
+              admin_deleted_early_access_users].sum,
+    }
+
+    promoted_waiting_list_users = DeletedWaitingListUser.deletion_type_promotion.count
+    unsubscribed_waiting_list_users = DeletedWaitingListUser.deletion_type_unsubscribe.count
+    admin_deleted_waiting_list_users = DeletedWaitingListUser.deletion_type_admin.count
+
+    @waiting_list_user_stats = {
+      current: waiting_list_users_current,
+      promoted: promoted_waiting_list_users,
+      unsubscribed: unsubscribed_waiting_list_users,
+      admin_deleted: admin_deleted_waiting_list_users,
+      total: [waiting_list_users_current,
+              promoted_waiting_list_users,
+              unsubscribed_waiting_list_users,
+              admin_deleted_waiting_list_users].sum,
+    }
   end
 end
