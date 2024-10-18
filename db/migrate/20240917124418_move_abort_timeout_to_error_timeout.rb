f7dced34efeb003b9891683fe9e84a7609c6f7a1
davidgisbey <david.gisbey@digital.cabinet-office.gov.uk>
2024-10-18 11:10:43 +0100

    Define answer class in data migrations that update statuses
    When these migrations run, the correspoding statuses have already been
    removed from the enum defined in the answer model and blow up.
    
    This adds an Answer class within the migration to ensure that if the
    application codes enum is out of sync with the database, we still
    have access to the functionality provided by the Rails enum.

--- db/migrate/20240917124418_move_abort_timeout_to_error_timeout.rb
+++ db/migrate/20240917124418_move_abort_timeout_to_error_timeout.rb
@@ -1,4 +1,12 @@
 class MoveAbortTimeoutToErrorTimeout < ActiveRecord::Migration[7.2]
+  class Answer < ApplicationRecord
+    enum :status,
+         {
+           abort_timeout: "abort_timeout",
+           error_timeout: "error_timeout",
+         }
+  end
+
   def up
     Answer.where(status: :abort_timeout).update_all(status: :error_timeout)
   end
