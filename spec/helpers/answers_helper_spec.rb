b171a34a9ab98db7aa69bed5db43a925c2e2c3ce
Kevin Dew <kevindew@me.com>
2024-10-21 19:35:24 +0100

    Apply naming of individual_question_limit
    This updates the codebase to reflect the new individual_question_limit
    field on EarlyAccessUser. It also updates usages of `question_limit` to
    be either `individual_question_limit` or the more generic
    `question_limit`.

--- spec/helpers/answers_helper_spec.rb
+++ spec/helpers/answers_helper_spec.rb
@@ -83,19 +83,19 @@ RSpec.describe AnswersHelper do
     end
 
     it "returns false if the user has unlimited questions allowance" do
-      user = build(:early_access_user, question_limit: 0)
+      user = build(:early_access_user, individual_question_limit: 0)
       expect(helper.show_question_limit_system_message?(user)).to be(false)
     end
 
-    it "returns true if the user has reached the question limit" do
-      user = build(:early_access_user, question_limit: 1, questions_count: 1)
+    it "returns true if the user has reached their question limit" do
+      user = build(:early_access_user, individual_question_limit: 1, questions_count: 1)
       expect(helper.show_question_limit_system_message?(user)).to be(true)
     end
 
     it "returns true if the user's question count is within the question warning threshold" do
       allow(Rails.configuration.conversations).to receive(:question_warning_threshold).and_return(5)
 
-      user = build(:early_access_user, question_limit: 50, questions_count: 45)
+      user = build(:early_access_user, individual_question_limit: 50, questions_count: 45)
       expect(helper.show_question_limit_system_message?(user)).to be(true)
     end
   end
