3588d50546457afb98fb869fe97d70633a21eb10
Chae Cramb <chae.cramb@digital.cabinet-office.gov.uk>
2024-10-22 12:46:02 +0100

    Move admin filter tests from system to request level
    As per this [comment](https://github.com/alphagov/govuk-chat/pull/812#discussion_r1809026607) excessive tests have crept in at the
    system level here. This commit removes system tests related to
    filtering and creates equivalent tests at the requests level.
    
    This allows specific individual users to be setup for the purpose of
    each test, rather than needing a bunch of users to serve the needs of
    multiple tests

--- spec/system/admin/user_filters_early_access_users_spec.rb
+++ spec/system/admin/user_filters_early_access_users_spec.rb
@@ -15,23 +15,12 @@ RSpec.describe "Admin user filters early access users" do
 
     when_i_clear_the_filters
     then_i_see_all_the_users
-
-    when_i_filter_by_source
-    then_i_see_the_admin_added_user
-
-    when_i_clear_the_filters
-    and_i_filter_by_revoked
-    then_i_see_the_revoked_user
-
-    when_i_clear_the_filters
-    and_i_filter_by_at_question_limit
-    then_i_see_the_user_at_their_question_limit
   end
 
   def and_there_are_early_access_users
-    create(:early_access_user, email: "alice@example.com", last_login_at: 1.minute.ago, source: :instant_signup, questions_count: 70, question_limit: nil)
-    create(:early_access_user, email: "betty@example.com", last_login_at: nil, source: :admin_added, revoked_at: nil, questions_count: 70, question_limit: 0)
-    create(:early_access_user, email: "clive@example.com", last_login_at: 1.hour.ago, source: :instant_signup, revoked_at: 1.minute.ago, questions_count: 3, question_limit: nil)
+    create(:early_access_user, email: "alice@example.com", last_login_at: 1.minute.ago)
+    create(:early_access_user, email: "betty@example.com", last_login_at: nil)
+    create(:early_access_user, email: "clive@example.com", last_login_at: 1.hour.ago)
   end
 
   def when_i_visit_the_admin_area
@@ -73,44 +62,5 @@ RSpec.describe "Admin user filters early access users" do
 
   def when_i_clear_the_filters
     click_on "Clear all filters"
-  end
-
-  def when_i_filter_by_source
-    select "Admin added", from: "source"
-    click_button "Filter"
-  end
-
-  def and_i_filter_by_revoked
-    select "Revoked", from: "revoked"
-    click_button "Filter"
-  end
-
-  def and_i_filter_by_at_question_limit
-    select "Yes", from: "at_question_limit"
-    click_button "Filter"
-  end
-
-  def then_i_see_the_admin_added_user
-    within(".govuk-table") do
-      expect(page).to have_content("betty@example.com")
-      expect(page).not_to have_content("alice@example.com")
-      expect(page).not_to have_content("clive@example.com")
-    end
-  end
-
-  def then_i_see_the_revoked_user
-    within(".govuk-table") do
-      expect(page).to have_content("clive@example.com")
-      expect(page).not_to have_content("alice@example.com")
-      expect(page).not_to have_content("betty@example.com")
-    end
-  end
-
-  def then_i_see_the_user_at_their_question_limit
-    within(".govuk-table") do
-      expect(page).to have_content("alice@example.com")
-      expect(page).not_to have_content("clive@example.com")
-      expect(page).not_to have_content("betty@example.com")
-    end
   end
 end
