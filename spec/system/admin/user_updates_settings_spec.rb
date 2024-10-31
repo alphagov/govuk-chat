3d8f51995cc5e7c1ed0e4f271ca45001ed32a6ee
Kevin Dew <kevindew@me.com>
2024-10-31 22:23:59 +0000

    Reorder admin settings
    This orders the settings to be more logically connected. This has both
    the significant enabling settings up top, instant access on it's own
    ending with the three related to waiting list at the bottom.
    
    The tests have been re-ordered to follow the same flow as the view. This
    has no material impact on the application, it's just done to feel
    logical to developers. Previously they were ordered based on which
    setting was added first.

--- spec/system/admin/user_updates_settings_spec.rb
+++ spec/system/admin/user_updates_settings_spec.rb
@@ -4,12 +4,20 @@ RSpec.describe "Admin user updates settings" do
     and_a_settings_instance_exists
 
     when_i_visit_the_settings_page
+    and_i_should_see_the_public_access_enabled_setting_is_enabled
+    and_i_should_see_the_sign_up_enabled_setting_is_disabled
     then_i_should_see_there_are_ten_instant_access_places
     and_i_should_see_there_are_ten_delayed_access_places
-    and_i_should_see_the_sign_up_enabled_setting_is_disabled
-    and_i_should_see_the_public_access_enabled_setting_is_enabled
-    and_i_should_see_the_max_waiting_list_places_setting_is_ten
     and_i_should_see_the_waiting_list_promotions_per_run_setting_is_twenty_five
+    and_i_should_see_the_max_waiting_list_places_setting_is_ten
+
+    when_i_click_the_edit_link_for_public_access
+    and_i_disable_public_access
+    then_i_see_that_public_access_is_disabled
+
+    when_i_click_the_edit_link_for_sign_up_enabled
+    and_i_choose_to_enable_signups
+    then_i_see_that_signups_are_enabled
 
     when_i_click_the_edit_link_for_instant_access_places
     and_i_add_five_instant_access_places
@@ -19,21 +27,13 @@ RSpec.describe "Admin user updates settings" do
     and_i_add_five_delayed_access_places
     then_i_see_there_are_fifteen_delayed_access_places
 
-    when_i_click_the_edit_link_for_sign_up_enabled
-    and_i_choose_to_enable_signups
-    then_i_see_that_signups_are_enabled
-
-    when_i_click_the_edit_link_for_public_access
-    and_i_disable_public_access
-    then_i_see_that_public_access_is_disabled
+    when_i_click_the_edit_link_for_waiting_list_promotions_per_run
+    and_i_set_the_waiting_list_promotions_per_run_to_fifty
+    and_i_should_see_the_waiting_list_promotions_per_run_setting_is_fifty
 
     when_i_click_the_edit_link_for_max_waiting_list_places
     and_i_set_the_max_waiting_list_places_to_fifteen
     and_i_should_see_the_max_waiting_list_places_setting_is_fifteen
-
-    when_i_click_the_edit_link_for_waiting_list_promotions_per_run
-    and_i_set_the_waiting_list_promotions_per_run_to_fifty
-    and_i_should_see_the_waiting_list_promotions_per_run_setting_is_fifty
 
     when_i_click_on_the_audits_link
     then_i_can_see_the_audits_for_my_changes
@@ -44,16 +44,28 @@ RSpec.describe "Admin user updates settings" do
       :settings,
       public_access_enabled: true,
       downtime_type: "temporary",
+      sign_up_enabled: false,
       instant_access_places: 10,
       delayed_access_places: 10,
-      sign_up_enabled: false,
-      max_waiting_list_places: 10,
       waiting_list_promotions_per_run: 25,
+      max_waiting_list_places: 10,
     )
   end
 
   def when_i_visit_the_settings_page
     visit admin_settings_path
+  end
+
+  def and_i_should_see_the_public_access_enabled_setting_is_enabled
+    within("#public-access") do
+      expect(page).to have_selector(".govuk-summary-list__row", text: "Enabled Yes")
+    end
+  end
+
+  def and_i_should_see_the_sign_up_enabled_setting_is_disabled
+    within("#sign-up-enabled") do
+      expect(page).to have_selector(".govuk-summary-list__row", text: "Enabled No")
+    end
   end
 
   def then_i_should_see_there_are_ten_instant_access_places
@@ -64,24 +76,43 @@ RSpec.describe "Admin user updates settings" do
     expect(page).to have_content("Available delayed access places 10")
   end
 
-  def and_i_should_see_the_sign_up_enabled_setting_is_disabled
-    within("#sign-up-enabled") do
-      expect(page).to have_selector(".govuk-summary-list__row", text: "Enabled No")
-    end
-  end
-
-  def and_i_should_see_the_public_access_enabled_setting_is_enabled
-    within("#public-access") do
-      expect(page).to have_selector(".govuk-summary-list__row", text: "Enabled Yes")
-    end
+  def and_i_should_see_the_waiting_list_promotions_per_run_setting_is_twenty_five
+    expect(page).to have_content("Promotions per run 25")
   end
 
   def and_i_should_see_the_max_waiting_list_places_setting_is_ten
     expect(page).to have_content("Maximum places 10")
   end
 
-  def and_i_should_see_the_waiting_list_promotions_per_run_setting_is_twenty_five
-    expect(page).to have_content("Promotions per run 25")
+  def when_i_click_the_edit_link_for_public_access
+    within("#public-access") { click_on "Edit Enabled" }
+  end
+
+  def and_i_disable_public_access
+    choose "No"
+    choose "Permanent"
+    click_on "Submit"
+  end
+
+  def then_i_see_that_public_access_is_disabled
+    within("#public-access") do
+      expect(page).to have_selector(".govuk-summary-list__row", text: "Enabled No - permanently offline")
+    end
+  end
+
+  def when_i_click_the_edit_link_for_sign_up_enabled
+    within("#sign-up-enabled") { click_on "Edit Enabled" }
+  end
+
+  def and_i_choose_to_enable_signups
+    choose "Yes"
+    click_on "Submit"
+  end
+
+  def then_i_see_that_signups_are_enabled
+    within("#sign-up-enabled") do
+      expect(page).to have_selector(".govuk-summary-list__row", text: "Enabled Yes")
+    end
   end
 
   def when_i_click_the_edit_link_for_instant_access_places
@@ -106,35 +137,17 @@ RSpec.describe "Admin user updates settings" do
     expect(page).to have_content("Available delayed access places 15")
   end
 
-  def when_i_click_the_edit_link_for_sign_up_enabled
-    within("#sign-up-enabled") { click_on "Edit Enabled" }
+  def when_i_click_the_edit_link_for_waiting_list_promotions_per_run
+    click_on "Edit Promotions per run"
   end
 
-  def and_i_choose_to_enable_signups
-    choose "Yes"
+  def and_i_set_the_waiting_list_promotions_per_run_to_fifty
+    fill_in "Promotions per run", with: 50
     click_on "Submit"
   end
 
-  def then_i_see_that_signups_are_enabled
-    within("#sign-up-enabled") do
-      expect(page).to have_selector(".govuk-summary-list__row", text: "Enabled Yes")
-    end
-  end
-
-  def when_i_click_the_edit_link_for_public_access
-    within("#public-access") { click_on "Edit Enabled" }
-  end
-
-  def and_i_disable_public_access
-    choose "No"
-    choose "Permanent"
-    click_on "Submit"
-  end
-
-  def then_i_see_that_public_access_is_disabled
-    within("#public-access") do
-      expect(page).to have_selector(".govuk-summary-list__row", text: "Enabled No - permanently offline")
-    end
+  def and_i_should_see_the_waiting_list_promotions_per_run_setting_is_fifty
+    expect(page).to have_content("Promotions per run 50")
   end
 
   def when_i_click_the_edit_link_for_max_waiting_list_places
@@ -150,30 +163,17 @@ RSpec.describe "Admin user updates settings" do
     expect(page).to have_content("Maximum places 15")
   end
 
-  def when_i_click_the_edit_link_for_waiting_list_promotions_per_run
-    click_on "Edit Promotions per run"
-  end
-
-  def and_i_set_the_waiting_list_promotions_per_run_to_fifty
-    fill_in "Promotions per run", with: 50
-    click_on "Submit"
-  end
-
-  def and_i_should_see_the_waiting_list_promotions_per_run_setting_is_fifty
-    expect(page).to have_content("Promotions per run 50")
-  end
-
   def when_i_click_on_the_audits_link
     click_on "Audits"
   end
 
   def then_i_can_see_the_audits_for_my_changes
     expect(page)
-      .to have_content("Added 5 instant access places")
-      .and have_content("Added 5 delayed access places")
+      .to have_content("Public access enabled set to false, downtime type permanent")
       .and have_content("Sign up enabled set to true")
-      .and have_content("Public access enabled set to false, downtime type permanent")
-      .and have_content("Updated maximum waiting list places to 15")
+      .and have_content("Added 5 instant access places")
+      .and have_content("Added 5 delayed access places")
       .and have_content("Updated waiting list promotions per run to 50")
+      .and have_content("Updated maximum waiting list places to 15")
   end
 end
