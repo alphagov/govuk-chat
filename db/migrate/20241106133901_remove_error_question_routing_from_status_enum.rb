bbbeb65fbd0a4fab297a09304207cecd2f422e5d
Chae Cramb <chae.cramb@digital.cabinet-office.gov.uk>
2024-11-06 13:57:03 +0000

    Remove error_question_routing from status enum
    The `error_question_rounting` status is no longer used and all
    references have been removed from the code.
    
    If any Answers in the database have a status of `error_question_routing`
    this PR updates them to a status of `error_non_specific`. It then
    recreates the status enum without `error_question_routing`.

---
+++ db/migrate/20241106133901_remove_error_question_routing_from_status_enum.rb
@@ -0,0 +1,30 @@
+class RemoveErrorQuestionRoutingFromStatusEnum < ActiveRecord::Migration[7.2]
+  def up
+    execute <<-SQL
+    ALTER TYPE status RENAME TO status_old;
+    CREATE TYPE status AS ENUM(
+                            'success',
+                            'error_non_specific',
+                            'error_answer_service_error',
+                            'error_context_length_exceeded',
+                            'abort_no_govuk_content',
+                            'abort_llm_cannot_answer',
+                            'abort_question_routing',
+                            'error_timeout',
+                            'abort_forbidden_terms',
+                            'abort_jailbreak_guardrails',
+                            'error_jailbreak_guardrails',
+                            'abort_answer_guardrails',
+                            'error_answer_guardrails',
+                            'abort_question_routing_token_limit',
+                            'abort_question_routing_guardrails',
+                            'error_question_routing_guardrails');
+    ALTER TABLE answers ALTER COLUMN status TYPE status USING status::text::status;
+    DROP TYPE status_old;
+    SQL
+  end
+
+  def down
+    raise ActiveRecord::IrreversibleMigration
+  end
+end
