0956df45fe80a99b663ae6483a261140e9ff392d
davidgisbey <david.gisbey@digital.cabinet-office.gov.uk>
2024-11-07 13:58:20 +0000

    Update multiple_checker to use new config format
    This updates the checker handle config use the new combined config.
    
    The main changes are that the guardrails are now stored in an array.
    This is uses to dynmically create the guardrails for the prompt by:
    
    1. Grabbing the guardrails from the config
    2. mapping across them and creating the content for the prompt for each
    guardrail by:
      a. looking up the guardrail definition in the config
      b. prepending the index + 1 of the guardrail to the content
    
    When the #extract_guardrails method is called, it now infers which
    guardrail has been triggered by the index of the guardrail in the
    the config + 1 so it matches the index of the guardrail in the prompt.

--- spec/lib/guardrails/multiple_checker_spec.rb
+++ spec/lib/guardrails/multiple_checker_spec.rb
@@ -1,26 +1,31 @@
 RSpec.describe Guardrails::MultipleChecker do
   describe ".call" do
-    let(:guardrail_mappings) { { "1" => "COSTS", "5" => "PERSONAL" } }
     let(:input) { "This is a test input." }
-
     let(:formatted_date) { Date.current.strftime("%A %d %B %Y") }
     let(:llm_prompt_name) { :answer_guardrails }
-    let(:system_prompt) do
-      Rails.configuration.llm_prompts.dig(llm_prompt_name, :system_prompt)
-        .gsub("{date}", formatted_date)
-    end
-
-    before do
-      allow(Rails.logger).to receive(:error)
-      allow(Rails.configuration.llm_prompts.answer_guardrails).to receive(:fetch).and_call_original
-      allow(Rails.configuration.llm_prompts.answer_guardrails).to receive(:fetch).with(:guardrail_mappings)
-                                                              .and_return(guardrail_mappings)
-    end
 
     context "when the request is successful" do
-      it "calls OpenAI to check for guardrail violations, including the date in the system prompt and the input in the user prompt" do
+      let(:guardrails_config) { Rails.configuration.llm_prompts.public_send(llm_prompt_name) }
+      let(:guardrail_definitions) do
+        {
+          "costs" => "This is a costs guardrail",
+          "personal" => "This is a personal guardrail",
+          "unique_answer_guardrail" => "This is a unique answer guardrail",
+        }
+      end
+      let(:guardrails) { %w[costs personal unique_answer_guardrail] }
+
+      before do
+        allow(Rails.logger).to receive(:error)
+        allow(guardrails_config).to receive(:fetch).and_call_original
+        allow(guardrails_config).to receive(:fetch).with(:guardrails).and_return(guardrails)
+        allow(guardrails_config).to receive(:fetch).with(:guardrail_definitions).and_return(guardrail_definitions)
+      end
+
+      it "calls OpenAI to check for guardrail violations with the correct system prompt and the input in the user prompt" do
+        expected_answer_guardrails = "1. This is a costs guardrail\n2. This is a personal guardrail\n3. This is a unique answer guardrail"
         messages = array_including(
-          { "role" => "system", "content" => a_string_including(formatted_date) },
+          { "role" => "system", "content" => a_string_including(expected_answer_guardrails, formatted_date) },
           { "role" => "user", "content" => a_string_including(input) },
         )
         openai_request = stub_openai_chat_completion(messages, answer: "False | None", chat_options: {
@@ -32,17 +37,17 @@ RSpec.describe Guardrails::MultipleChecker do
       end
 
       it "returns triggered: true with human readable guardrails" do
-        guardrail_result = 'True | "1, 5"'
+        guardrail_result = 'True | "1, 2"'
         stub_openai_output_guardrail(input, guardrail_result)
         expect(described_class.call(input, llm_prompt_name)).to be_a(described_class::Result)
           .and(having_attributes(
-                triggered: true,
-                guardrails: %w[COSTS PERSONAL],
-                llm_response: a_hash_including(
-                  "message" => a_hash_including("content" => guardrail_result),
-                ),
-                llm_guardrail_result: guardrail_result,
-              ))
+                 triggered: true,
+                 guardrails: %w[costs personal],
+                 llm_response: a_hash_including(
+                   "message" => a_hash_including("content" => guardrail_result),
+                 ),
+                 llm_guardrail_result: guardrail_result,
+               ))
       end
 
       it "returns triggered: false with empty guardrails" do
@@ -50,13 +55,13 @@ RSpec.describe Guardrails::MultipleChecker do
         stub_openai_output_guardrail(input, guardrail_result)
         expect(described_class.call(input, llm_prompt_name)).to be_a(described_class::Result)
           .and(having_attributes(
-                triggered: false,
-                guardrails: [],
-                llm_response: a_hash_including(
-                  "message" => a_hash_including("content" => guardrail_result),
-                ),
-                llm_guardrail_result: guardrail_result,
-              ))
+                 triggered: false,
+                 guardrails: [],
+                 llm_response: a_hash_including(
+                   "message" => a_hash_including("content" => guardrail_result),
+                 ),
+                 llm_guardrail_result: guardrail_result,
+               ))
       end
 
       it "returns the LLM token usage" do
@@ -72,7 +77,7 @@ RSpec.describe Guardrails::MultipleChecker do
       end
 
       it "calculates the max_tokens param from the guardrail config" do
-        longest_possible_response_string = %(True | "#{guardrail_mappings.keys.join(', ')}")
+        longest_possible_response_string = %(True | "#{(1..guardrails.count).to_a.join(', ')}")
         token_count = Tiktoken
                         .encoding_for_model(described_class::OPENAI_MODEL)
                         .encode(longest_possible_response_string)
@@ -91,15 +96,26 @@ RSpec.describe Guardrails::MultipleChecker do
         expect(openai_request).to have_been_made
       end
 
-      context "with a question routing guardrail LLM prompt" do
-        it "makes the request" do
+      context "when :question_routing_guardrails is passed in as the llm_prompt_name" do
+        let(:llm_prompt_name) { :question_routing_guardrails }
+        let(:guardrail_definitions) do
+          {
+            "costs" => "This is a costs guardrail",
+            "personal" => "This is a personal guardrail",
+            "unique_question_routing_guardrail" => "This is a unique question routing guardrail",
+          }
+        end
+        let(:guardrails) { %w[costs personal unique_question_routing_guardrail] }
+
+        it "uses the correct guardrails for question routing" do
+          expected_question_routing_guardrails = "1. This is a costs guardrail\n2. This is a personal guardrail\n3. This is a unique question routing guardrail"
           messages = array_including(
-            { "role" => "system", "content" => a_string_including(formatted_date) },
+            { "role" => "system", "content" => a_string_including(expected_question_routing_guardrails, formatted_date) },
             { "role" => "user", "content" => a_string_including(input) },
           )
           openai_request = stub_openai_chat_completion(messages, answer: "False | None", chat_options: {})
 
-          described_class.call(input, :question_routing_guardrails)
+          described_class.call(input, llm_prompt_name)
 
           expect(openai_request).to have_been_made
         end
@@ -113,7 +129,7 @@ RSpec.describe Guardrails::MultipleChecker do
             .to raise_error(
               an_instance_of(described_class::ResponseError)
                 .and(having_attributes(message: "Error parsing guardrail response",
-                                      llm_response: guardrail_result)),
+                                       llm_response: guardrail_result)),
             )
         end
       end
@@ -126,7 +142,7 @@ RSpec.describe Guardrails::MultipleChecker do
             .to raise_error(
               an_instance_of(described_class::ResponseError)
                 .and(having_attributes(message: "Error parsing guardrail response",
-                                      llm_response: guardrail_result)),
+                                       llm_response: guardrail_result)),
             )
         end
       end
