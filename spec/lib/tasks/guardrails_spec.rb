6e7972ddf6d38cbfcb6aad219ce0fa3597e9f5d3
Jack Weeden <jack@ajack.org>
2024-11-07 09:27:58 +0000

    Reduce mocking in guardrails rake spec
    We had an issue recently where an additional mandatory argument was
    added to the `Guardrails::MultipleChecker.call` method.
    
    The `guardrails.rake` file wasn't updated to include this new argument
    though, but the tests still passed so it wasn't picked up until somebody
    tried to actually run the rake task.
    
    The problem is the amount of mocking that's happening in the spec,
    specifically the line which mocks the actual guardrail checking:
    
    ```
    allow(Guardrails::MultipleChecker).to receive(:call).and_return(false_response, true_response)
    ```
    
    This commit does a bit of refactoring to reduce the amount of mocking so
    we hopefully won't see this problem again.
    
    Really all we care about testing here is that the rake task performs the
    evaluation and either prints the results to stdout or to a file. The
    actual guardrailing logic is well tested elsewhere, in both the
    `Guardrails::Evaluation.call` method and the
    `Guardrails::MultipleChecker.call`.
    
    So we don't really need to make the test produce a mix of true and false
    responses. We just want to assert that:
    
    * the right number of results are produced based on the number of lines
      in the input CSV file
    * the results are merged with the model name,
      `average_prompt_token_count` and `max_prompt_token_count`
    * the results contain the output string of the guardrailing (e.g. `True
      | "1"`)
    
    So to make it simple, this commit stubs the guardrailing at the OpenAI
    level so that the actual application code is tested, then verifies the
    output as it did before.

--- spec/lib/tasks/guardrails_spec.rb
+++ spec/lib/tasks/guardrails_spec.rb
@@ -1,53 +1,34 @@
 RSpec.describe "rake guardrails tasks" do
   describe "guardrails:evaluate_answer_guardrails" do
     let(:task_name) { "guardrails:evaluate_answer_guardrails" }
-    let(:false_response) do
-      Guardrails::MultipleChecker::Result.new(
-        llm_response: llm_response_json("False | None"),
-        llm_guardrail_result: "False | None",
-        triggered: false,
-        guardrails: [],
-        llm_token_usage: { "prompt_tokens" => 1000 },
-      )
-    end
-    let(:true_response) do
-      Guardrails::MultipleChecker::Result.new(
-        llm_response: llm_response_json('True | "1"'),
-        llm_guardrail_result: 'True | "1"',
-        triggered: true,
-        guardrails: %w[sensitive_financial_matters],
-        llm_token_usage: { "prompt_tokens" => 2000 },
-      )
-    end
-    let(:model_name) { Guardrails::MultipleChecker::OPENAI_MODEL }
 
     before do
       Rake::Task[task_name].reenable
-      allow(Guardrails::MultipleChecker).to receive(:call).and_return(false_response, true_response)
+      stub_openai_output_guardrail("", 'True | "1"')
     end
 
     context "when given an output path" do
       it "runs successfully, outputs summary, and saves the results to a file with the given path" do
         temp = Tempfile.new
         begin
+          examples = CSV.read(Rails.root.join("lib/data/output_guardrails/answer_guardrails_examples.csv"), headers: true).length
+          expect(Guardrails::MultipleChecker).to receive(:call).exactly(examples).times.and_call_original
+
           expect { Rake::Task[task_name].invoke(temp.path) }.to output(/count=>[\s\S]*Full results/).to_stdout
           results = JSON.parse(File.read(temp.path))
 
           expect(results).to be_a(Hash)
           expect(results).to include("count", "model")
-          expect(results["model"]).to eq(model_name)
+          expect(results["model"]).to eq(Guardrails::MultipleChecker::OPENAI_MODEL)
 
           # Average token count depends on the number of examples, so we'll just
           # check the presence of a value here so the tests won't fail when the
           # CSV file changes
           expect(results["average_prompt_token_count"]).to be_a(Integer)
-          expect(results["max_prompt_token_count"]).to eq(2000)
+          expect(results["max_prompt_token_count"]).to eq(13)
 
           first_example = results["false_positives"][0]
-          expect(first_example["actual"]).to eq(true_response.llm_guardrail_result)
-
-          examples = CSV.read(Rails.root.join("lib/data/output_guardrails/answer_guardrails_examples.csv"), headers: true).length
-          expect(Guardrails::MultipleChecker).to have_received(:call).exactly(examples).times
+          expect(first_example["actual"]).to eq('True | "1"')
         ensure
           temp.close
           temp.unlink
@@ -59,17 +40,6 @@ RSpec.describe "rake guardrails tasks" do
       it "outputs the full structure to the console" do
         expect { Rake::Task[task_name].invoke }.to output(/count=>[\s\S]*failures=>/).to_stdout
       end
-    end
-
-    def llm_response_json(guardrail_result)
-      {
-        "message": {
-          "role": "assistant",
-          "content": guardrail_result,
-          "refusal": nil,
-        },
-        "finish_reason": "stop",
-      }
     end
   end
 end
