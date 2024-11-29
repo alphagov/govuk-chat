ee3186ed5e5e3e4702fa2d32bfbb5c6e31b1c707
James Chan <james.chan@digital.cabinet-office.gov.uk>
2024-11-29 14:38:15 +0000

    Update get started messages

--- spec/system/chat_onboarding_spec.rb
+++ spec/system/chat_onboarding_spec.rb
@@ -89,7 +89,8 @@ RSpec.describe "Chat Onboarding" do
 
   def then_i_see_the_chat_prompt
     expect(page).to have_content(/Okay/)
-    expect(page).to have_content(/Thanks! To get started, ask me a question/)
+    expect(page).to have_content("Share feedback (opens in a new tab) when you're done chatting.")
+    expect(page).to have_content("To get started, ask a question.")
 
     expect(page).to have_css(".js-question-form-group")
   end
