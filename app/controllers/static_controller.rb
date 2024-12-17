8d012d3f7436b7047c8c68a336adc69b7445ab64
davidgisbey <david.gisbey@digital.cabinet-office.gov.uk>
2024-12-17 10:26:06 +0000

    Redirect users away from support page when chat is disabled
    We don't want to show the chat support page when chat is disabled. This
    adds a before action to the static controller to redirect users to the
    homepage if they visit the support page when chat is disabled.

--- app/controllers/static_controller.rb
+++ app/controllers/static_controller.rb
@@ -1,6 +1,7 @@
 class StaticController < BaseController
   skip_before_action :ensure_early_access_user_if_required
-  skip_before_action :check_chat_public_access
-
+  skip_before_action :check_chat_public_access, except: %i[support]
   before_action :cache_if_not_logged_in
+
+  def support; end
 end
--- spec/requests/static_spec.rb
+++ spec/requests/static_spec.rb
@@ -68,16 +68,33 @@ RSpec.describe "StaticController" do
   end
 
   describe "GET :support" do
-    it "renders the view correctly" do
-      get support_path
-      expect(response).to have_http_status(:ok)
-      expect(response.body)
-        .to include("If you have a problem accessing GOV.UK Chat")
-        .and have_link("Back to start page", href: homepage_path)
+    context "when public access is enabled" do
+      before { Settings.instance.update!(public_access_enabled: true) }
+
+      it "renders the view correctly" do
+        get support_path
+        expect(response).to have_http_status(:ok)
+        expect(response.body)
+          .to include("If you have a problem accessing GOV.UK Chat")
+          .and have_link("Back to start page", href: homepage_path)
+      end
+
+      include_examples "caches the page", :support_path
     end
 
-    include_examples "caches the page", :support_path
-    include_examples "operates when public access is disabled", :support_path
+    context "when public access is disabled" do
+      before { Settings.instance.update!(public_access_enabled: false, downtime_type: :permanent) }
+
+      it "returns a :gone status code" do
+        get support_path
+        expect(response).to have_http_status(:gone)
+      end
+
+      it "renders the unavailable template" do
+        get support_path
+        expect(response.body).to include("GOV.UK Chat is not currently available")
+      end
+    end
   end
 
   describe "GET :accessibility" do
