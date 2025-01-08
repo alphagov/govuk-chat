460c0fa9601f9a25527e9e7e1e23289abe5c2613
Kevin Dew <kevindew@me.com>
2025-01-08 17:32:37 +0000

    Remove feedback email
    This removes the sending of an email to a user 10 minutes after their
    first question. The motivation for removing this is because the survey
    it links to is now switched off and we are removing other references to
    this link.

--- app/mailers/early_access_user_feedback_mailer.rb
+++
@@ -1,8 +0,0 @@
-class EarlyAccessUserFeedbackMailer < ApplicationMailer
-  def request_feedback(user)
-    @user_id = user.id
-    @unsubscribe_token = user.unsubscribe_token
-    @survey_url = "https://surveys.publishing.service.gov.uk/s/govuk-chat-beta?user=#{@user_id}&source=request_feedback_email"
-    view_mail(template_id, to: user.email, subject: "Share your experience of GOV.UK Chat")
-  end
-end
