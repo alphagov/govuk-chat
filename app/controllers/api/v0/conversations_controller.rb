4812166b41820ef9f8e690ede9810ce16185be08
David Gisbey <david.gisbey@digital.cabinet-office.gov.uk>
2025-04-10 16:10:20 +0100

    WIP - signon integration

--- app/controllers/api/v0/conversations_controller.rb
+++ app/controllers/api/v0/conversations_controller.rb
@@ -1,4 +1,6 @@
 class Api::V0::ConversationsController < ApplicationController
+  before_action :authorise_user
+
   def show
     conversation = Conversation.includes(questions: { answer: %i[sources feedback] })
                                 .find(params[:id])
@@ -8,5 +10,11 @@ class Api::V0::ConversationsController < ApplicationController
     render json: ConversationBlueprint.render(conversation, answered_questions:, pending_question:), status: :ok
   rescue ActiveRecord::RecordNotFound => e
     render json: { error: ErrorBlueprint.render_as_hash({ message: e.message }) }, status: :not_found
+  end
+
+private
+
+  def authorise_user
+    authorise_user!("api-user")
   end
 end
