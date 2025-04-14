d4e3266be30a9affd7859b7788c3cd814bcd00b3
David Gisbey <david.gisbey@digital.cabinet-office.gov.uk>
2025-04-14 14:12:44 +0100

    Extract OpenAPI spec functionality into shared example
    Down the line this will need to be updated to handle post endpoints too,
    but i've kept it simple for now.
    
    Docs on setting up Committee here https://github.com/interagent/committee?tab=readme-ov-file#test-assertions

--- spec/requests/api/v0/conversations_spec.rb
+++ spec/requests/api/v0/conversations_spec.rb
@@ -1,32 +1,10 @@
 RSpec.describe "Api::V0::ConversationsController" do
-  include Committee::Test::Methods
   let(:conversation) { create(:conversation) }
-
-  let(:committee_options) do
-    schema = Committee::Drivers.load_from_file(
-      Rails.root.join("spec/support/api/openapi.yaml").to_s, parser_options: { strict_reference_validation: true }
-    )
-
-    {
-      schema: schema,
-      prefix: "/api/v0",
-      validate_success_only: true,
-    }
-  end
-
-  def request_object
-    response.request
-  end
-
-  def response_data
-    [response.status, response.headers, response.body]
-  end
 
   describe "GET :show" do
     context "when a conversation exists with the given ID" do
-      it "is valid against the OpenAPI spec" do
-        get api_conversation_path(conversation)
-        assert_response_schema_confirm(200)
+      it_behaves_like "adheres to the OpenAPI specification", :api_conversation_path do
+        let(:route_params) { [create(:conversation)] }
       end
 
       it "returns a 200 response" do
@@ -40,8 +18,13 @@ RSpec.describe "Api::V0::ConversationsController" do
       end
 
       context "when the conversation has answered questions" do
+        let!(:answered_question) { create(:question, :with_answer, conversation:) }
+
+        it_behaves_like "adheres to the OpenAPI specification", :api_conversation_path do
+          let(:route_params) { [create(:conversation)] }
+        end
+
         it "returns the answered questions in the JSON response" do
-          answered_question = create(:question, :with_answer, conversation:)
           eager_loaded_answered_question = Question.includes(answer: %i[sources feedback]).find(answered_question.id)
           get api_conversation_path(conversation)
 
@@ -53,9 +36,8 @@ RSpec.describe "Api::V0::ConversationsController" do
       context "when the conversation has a pending question" do
         let!(:pending_question) { create(:question, conversation:) }
 
-        it "is valid against the OpenAPI spec" do
-          get api_conversation_path(conversation)
-          assert_response_schema_confirm(200)
+        it_behaves_like "adheres to the OpenAPI specification", :api_conversation_path do
+          let(:route_params) { [create(:conversation)] }
         end
 
         it "returns the pending question in the JSON response" do
@@ -70,9 +52,8 @@ RSpec.describe "Api::V0::ConversationsController" do
     end
 
     context "when a conversation does not exist with the given ID" do
-      it "is valid against the OpenAPI spec" do
-        get api_conversation_path(id: "invalid-id")
-        assert_response_schema_confirm(404)
+      it_behaves_like "adheres to the OpenAPI specification", :api_conversation_path, 404 do
+        let(:route_params) { %w[invalid_id] }
       end
 
       it "returns a 404 response" do
