bbdf7eb2ce9e6d6727cbcfc8efac5de7b095bbbe
David Gisbey <david.gisbey@digital.cabinet-office.gov.uk>
2025-04-16 09:53:37 +0100

    Add blueprints required for the GET answer endpoint
    This commit introduces the first blueprints we will use. The AnswerBlueprint
    and GenericErrorBlueprint.
    
    The AnswerBlueprint is responsible for serializing the answer object to
    JSON before returning it to the client.
    
    It only includes sources that have been used and uses the same logic that
    chat does by using the Answer#group_used_answer_sources_by_base_path
    method.
    
    If answer feedback has been provided on the answer, it will be included
    in response as laid out in the OpenAPI spec.
    
    However, since this endpoint should only be used prior to feedback being given,
    since it's a polling endpoint. I've still included it in the blueprint though
    since it's present in the specification.
    
    The GenericErrorBlueprint will be used when a non 422 exception is raised
    to return a generic error message to the client.

---
+++ spec/blueprints/answer_blueprint_spec.rb
@@ -0,0 +1,56 @@
+RSpec.describe AnswerBlueprint do
+  let(:answer) { create(:answer) }
+
+  describe ".render_as_json" do
+    it "generates the correct JSON for an Answer with no sources" do
+      expected_json = {
+        id: answer.id,
+        created_at: answer.created_at.iso8601,
+        message: answer.message,
+      }.as_json
+      output_json = described_class.render_as_json(Answer.includes(%i[sources feedback]).find(answer.id))
+
+      expect(output_json).to eq(expected_json)
+    end
+
+    it "generates the correct JSON for an Answer with used sources" do
+      answer_source = create(:answer_source, answer:)
+
+      expected_json = {
+        id: answer.id,
+        created_at: answer.created_at.iso8601,
+        message: answer.message,
+        sources: [
+          {
+            title: "#{answer_source.title}: #{answer_source.heading}",
+            url: answer_source.url,
+          },
+        ],
+      }.as_json
+      output_json = described_class.render_as_json(Answer.includes(%i[sources feedback]).find(answer.id))
+
+      expect(output_json).to eq(expected_json)
+    end
+
+    it "does not include unused sources in the JSON" do
+      create(:answer_source, answer:, used: false)
+      output_json = described_class.render_as_json(Answer.includes(%i[sources feedback]).find(answer.id))
+      expect(output_json.keys).not_to include("sources")
+    end
+
+    context "when answer feedback is present" do
+      it "includes it in the JSON" do
+        create(:answer_feedback, answer:)
+        expected_json = {
+          id: answer.id,
+          created_at: answer.created_at.iso8601,
+          message: answer.message,
+          useful: true,
+        }.as_json
+        output_json = described_class.render_as_json(Answer.includes(%i[sources feedback]).find(answer.id))
+
+        expect(output_json).to eq(expected_json)
+      end
+    end
+  end
+end
