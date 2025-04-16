ff8d663d6ce99a0d0837ef0b9e8f69dd6e6c70d4
David Gisbey <david.gisbey@digital.cabinet-office.gov.uk>
2025-04-16 11:11:23 +0100

    Add GET answer endpoint
    Since this is the first endpoint added i've done the normal:
    
    - add route
    - add controller
    - add new endpoint to controller
    
    This endpoint should respond in the following way:
    
    - return an answer that adheres to the Answer object in the specification
    if one is present with a 200
    - return a 202 if no answer is present
    - return a 404 if the conversation or question is not found
    
    When we get a 404 i'd adhered to the same response we send in Chat i.e.
    
    "Conversation not found" rather than using the error objects messsage.
    
    I'll ensure the endpoint is behind auth and that the response adheres to
    the OpenAPI specification in follow up commits.

---
+++ app/controllers/api/v0/conversations_controller.rb
@@ -0,0 +1,29 @@
+class Api::V0::ConversationsController < ApplicationController
+  before_action :find_conversation
+  before_action :find_question
+
+  def answer
+    answer = @question.answer
+
+    if answer.present?
+      render json: AnswerBlueprint.render(answer), status: :ok
+    else
+      render json: {}, status: :accepted
+    end
+  end
+
+private
+
+  def find_conversation
+    @conversation = Conversation.includes(questions: { answer: %i[sources feedback] })
+                                .find(params[:conversation_id])
+  rescue ActiveRecord::RecordNotFound
+    render json: GenericErrorBlueprint.render(message: "Conversation not found"), status: :not_found
+  end
+
+  def find_question
+    @question = @conversation.questions.find(params[:question_id])
+  rescue ActiveRecord::RecordNotFound
+    render json: GenericErrorBlueprint.render(message: "Question not found"), status: :not_found
+  end
+end
