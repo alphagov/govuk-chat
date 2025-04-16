647b8c15c871273c844f60853cd2b603d6c029af
David Gisbey <david.gisbey@digital.cabinet-office.gov.uk>
2025-04-16 13:36:24 +0100

    Move group_used_answer_sources_by_base_path to Answer model
    Now we're going to expose used sources on the Chat API, we need to move
    the #group_used_answer_sources_by_base_path method out of the view helper
    to the Answer model so that it can be utilised by both the view and the API.
    
    This method will be used by the AnswerBlueprint in the next commit.

--- spec/helpers/answers_helper_spec.rb
+++ spec/helpers/answers_helper_spec.rb
@@ -23,67 +23,6 @@ RSpec.describe AnswersHelper do
     end
   end
 
-  describe "#group_used_answer_sources_by_base_path" do
-    context "when there is one source per base path" do
-      let(:answer) do
-        create(:answer, sources: [
-          create(
-            :answer_source,
-            base_path: "/childcare-provider",
-            exact_path: "/childcare-provider/how-to-get-a-childcare-provider",
-            title: "Childcare providers",
-            heading: "How to get a childcare provider",
-          ),
-        ])
-      end
-
-      it "builds the sources using the exact path and including the heading" do
-        expect(helper.group_used_answer_sources_by_base_path(answer)).to contain_exactly(
-          {
-            href: "#{Plek.website_root}/childcare-provider/how-to-get-a-childcare-provider",
-            title: "Childcare providers: How to get a childcare provider",
-          },
-        )
-      end
-
-      it "filters out unused sources" do
-        answer.sources << create(:answer_source, used: false, answer:)
-
-        expect(helper.group_used_answer_sources_by_base_path(answer).length).to eq 1
-      end
-    end
-
-    context "when there are multiple sources per base path" do
-      let(:answer) do
-        create(:answer, sources: [
-          create(
-            :answer_source,
-            base_path: "/childcare-provider",
-            exact_path: "/childcare-provider/how-to-get-a-childcare-provider",
-            title: "Childcare providers",
-            heading: "How to get a childcare provider",
-          ),
-          create(
-            :answer_source,
-            base_path: "/childcare-provider",
-            exact_path: "/childcare-provider/how-much-it-costs",
-            title: "Childcare providers",
-            heading: "How much it costs",
-          ),
-        ])
-      end
-
-      it "builds the sources using the base path and excluding the heading" do
-        expect(helper.group_used_answer_sources_by_base_path(answer)).to contain_exactly(
-          {
-            href: "#{Plek.website_root}/childcare-provider",
-            title: "Childcare providers",
-          },
-        )
-      end
-    end
-  end
-
   describe "#show_question_limit_system_message?" do
     it "returns false when no user is present" do
       expect(helper.show_question_limit_system_message?(nil)).to be(false)
