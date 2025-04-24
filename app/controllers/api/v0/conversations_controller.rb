551c0d1f95df628e908746d2f797c232bd97a3e7
Jack Weeden <jack@ajack.org>
2025-04-24 14:09:39 +0100

    Extract out conversation query into private method
    We've got duplication in both of these controller actions, so it makes
    sense to extract this into a private method.

--- app/controllers/api/v0/conversations_controller.rb
+++ app/controllers/api/v0/conversations_controller.rb
@@ -1,22 +1,19 @@
 class Api::V0::ConversationsController < ApplicationController
   before_action { authorise_user!(SignonUser::Permissions::CONVERSATION_API) }
+  before_action :find_conversation, only: %i[show answer]
 
   def show
-    conversation = Conversation.includes(questions: { answer: %i[sources feedback] })
-                               .find(params[:conversation_id])
-    answered_questions = conversation.questions.joins(:answer)
-    pending_question = conversation.questions.unanswered.last
+    answered_questions = @conversation.questions.joins(:answer)
+    pending_question = @conversation.questions.unanswered.last
 
     render(
-      json: ConversationBlueprint.render(conversation, answered_questions:, pending_question:),
+      json: ConversationBlueprint.render(@conversation, answered_questions:, pending_question:),
       status: :ok,
     )
   end
 
   def answer
-    conversation = Conversation.includes(questions: { answer: %i[sources feedback] })
-                               .find(params[:conversation_id])
-    question = conversation.questions.find(params[:question_id])
+    question = @conversation.questions.find(params[:question_id])
     answer = question.answer
 
     if answer.present?
@@ -24,5 +21,13 @@ class Api::V0::ConversationsController < ApplicationController
     else
       render json: {}, status: :accepted
     end
+  end
+
+private
+
+  def find_conversation
+    @conversation = Conversation
+                    .includes(questions: { answer: %i[sources feedback] })
+                    .find(params[:conversation_id])
   end
 end
