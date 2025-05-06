7c18d0f2bc5d4ed8d960b48f29f0c1e125c8bc19
David Gisbey <david.gisbey@digital.cabinet-office.gov.uk>
2025-05-06 15:18:43 +0100

    Add source filter to questions index page
    This builds on the last commit, by adding the filter to the questions index
    page.
    
    It:
    
    - updates the filter_params method to include the source filter
    - adds the source filter to the questions index page
    - updates the system test to includes steps which test the source filter

--- spec/system/admin/user_filters_questions_spec.rb
+++ spec/system/admin/user_filters_questions_spec.rb
@@ -21,8 +21,12 @@ RSpec.describe "Admin user filters questions" do
     then_i_see_questions_related_to_my_search
 
     when_i_clear_the_filters
+    and_i_filter_on_questions_created_via_the_api
+    then_i_see_question_created_via_the_api
+
+    when_i_clear_the_filters
     and_i_search_for_recent_questions
-    then_i_see_the_recent_question
+    then_i_see_the_recent_questions
 
     when_i_clear_the_filters
     and_i_search_for_old_questions
@@ -83,10 +87,13 @@ RSpec.describe "Admin user filters questions" do
 
   def and_there_are_questions
     conversation = build(:conversation)
+    api_conversation = build(:conversation, source: :api)
     @question1 = create(:question, conversation:, message: "Hello world", created_at: 2.years.ago)
     @question2 = create(:question, message: "World", conversation:)
-    answer2 = create(:answer, question: @question2, question_routing_label: "non_english")
-    create(:answer_feedback, answer: answer2, useful: true)
+    @question3 = create(:question, conversation: api_conversation, message: "Greetings world", created_at: 1.minute.ago)
+    answer = create(:answer, question: @question2, question_routing_label: "non_english")
+    create(:answer_feedback, answer: answer, useful: true)
+    create(:answer, status: :clarification, question: @question3)
   end
 
   def and_there_are_questions_associated_with_users
@@ -143,7 +150,7 @@ RSpec.describe "Admin user filters questions" do
 
   def then_i_see_all_the_questions
     within(".govuk-table") do
-      expect(page).to have_content(/World.*Hello world./)
+      expect(page).to have_content(/World.*Greetings world.*Hello world./)
     end
   end
 
@@ -195,7 +202,7 @@ RSpec.describe "Admin user filters questions" do
 
   def then_i_see_the_ordering_has_changed
     within(".govuk-table") do
-      expect(page).to have_content(/Hello world.*World./)
+      expect(page).to have_content(/Hello world.*Greetings world.*World./)
     end
   end
 
@@ -213,6 +220,18 @@ RSpec.describe "Admin user filters questions" do
   def then_i_see_questions_related_to_my_search
     expect(page).to have_content(@question1.message)
     expect(page).not_to have_content(@question2.message)
+    expect(page).not_to have_content(@question3.message)
+  end
+
+  def and_i_filter_on_questions_created_via_the_api
+    select "API", from: "source"
+    click_button "Filter"
+  end
+
+  def then_i_see_question_created_via_the_api
+    expect(page).to have_content(@question3.message)
+    expect(page).not_to have_content(@question1.message)
+    expect(page).not_to have_content(@question2.message)
   end
 
   def and_i_search_for_recent_questions
@@ -224,8 +243,9 @@ RSpec.describe "Admin user filters questions" do
     click_button "Filter"
   end
 
-  def then_i_see_the_recent_question
+  def then_i_see_the_recent_questions
     expect(page).to have_content(@question2.message)
+    expect(page).to have_content(@question3.message)
     expect(page).not_to have_content(@question1.message)
   end
 
@@ -241,6 +261,7 @@ RSpec.describe "Admin user filters questions" do
   def then_i_see_the_old_question
     expect(page).to have_content(@question1.message)
     expect(page).not_to have_content(@question2.message)
+    expect(page).not_to have_content(@question3.message)
   end
 
   def and_i_filter_by_questions_with_useful_answers
@@ -251,6 +272,7 @@ RSpec.describe "Admin user filters questions" do
   def then_i_see_the_useful_question
     expect(page).to have_content(@question2.message)
     expect(page).not_to have_content(@question1.message)
+    expect(page).not_to have_content(@question3.message)
   end
 
   def and_i_filter_by_questions_with_a_specific_question_routing_label
@@ -261,6 +283,7 @@ RSpec.describe "Admin user filters questions" do
   def then_i_see_the_question_with_that_routing_label
     expect(page).to have_content(@question2.message)
     expect(page).not_to have_content(@question1.message)
+    expect(page).not_to have_content(@question3.message)
   end
 
   def when_i_view_the_questions_conversation
