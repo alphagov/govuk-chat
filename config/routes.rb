cb816a5fe3cb196b17767fd0048b3dc9c9ab5ffd
Sunny Shafiq <sunny.shafiq@digital.cabinet-office.gov.uk>
2025-05-08 11:40:21 +0100

    WIP 1

--- config/routes.rb
+++ config/routes.rb
@@ -94,26 +94,6 @@ Rails.application.routes.draw do
       end
     end
 
-    resources :early_access_users, path: "/early-access-users" do
-      member do
-        get "/delete", to: "early_access_users#delete", as: :delete
-        get "/access/revoke", to: "early_access_users/access#revoke", as: :revoke
-        patch "/access/revoke", to: "early_access_users/access#revoke_confirm"
-        get "/access/shadow-ban", to: "early_access_users/access#shadow_ban", as: :shadow_ban
-        patch "/access/shadow-ban", to: "early_access_users/access#shadow_ban_confirm"
-        get "/access/restore", to: "early_access_users/access#restore", as: :restore
-        patch "/access/restore", to: "early_access_users/access#restore_confirm"
-      end
-    end
-
-    resources :waiting_list_users, path: "/waiting-list-users" do
-      member do
-        get "/delete", to: "waiting_list_users#delete", as: :delete
-        get "/promote", to: "waiting_list_users#promote", as: :promote
-        post "/promote", to: "waiting_list_users#promote_confirm"
-      end
-    end
-
     scope :settings do
       get "", to: "settings#show", as: :settings
 
