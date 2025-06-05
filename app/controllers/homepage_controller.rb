ed2b7d86572b31ac4c4140d82149ef2c38214385
James Chan <james.chan@digital.cabinet-office.gov.uk>
2025-06-05 12:02:40 +0100

    Chat UI design changes

--- app/controllers/homepage_controller.rb
+++ app/controllers/homepage_controller.rb
@@ -1,7 +1,7 @@
 class HomepageController < BaseController
-  skip_before_action :ensure_early_access_user_if_required
+  #skip_before_action :ensure_early_access_user_if_required
   skip_forgery_protection # as we cache the form we can't verify the token
-  before_action :cache_if_not_logged_in, only: :index
+  #before_action :cache_if_not_logged_in, only: :index
 
   def index
     render :ut_landing_page
