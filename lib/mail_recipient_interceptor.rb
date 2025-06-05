5059c597772678cfd662a22b54732bb7e23c5bee
David Gisbey <david.gisbey@digital.cabinet-office.gov.uk>
2025-06-05 16:31:38 +0100

    Remove Notify integration
    This removes all the code related to the Notify service integration. It
    doesn't remove ActionMailer and related config. This will be done in
    the next commit.

--- lib/mail_recipient_interceptor.rb
+++
@@ -1,8 +0,0 @@
-class MailRecipientInterceptor
-  def self.delivering_email(message)
-    body_prefix = "Intended recipient(s): #{message.to.join(', ')}\n\n"
-
-    message.personalisation[:body] = message.personalisation[:body].prepend(body_prefix)
-    message.to = ENV.fetch("EMAIL_ADDRESS_OVERRIDE")
-  end
-end
