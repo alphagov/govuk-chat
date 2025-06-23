0833de5ea646677a3ee91c1c2ec7b432945c289e
Jack Weeden <jack@ajack.org>
2025-06-23 15:46:31 +0100

    Rename Device ID header
    Renames the `HTTP_GOVUK_CHAT_CLIENT_DEVICE_ID` header to
    `HTTP_GOVUK_CHAT_CLIENT_USER_ID` to make it a bit more clear as to its
    purpose.
    
    The "client device ID" part has confused people, as the app expects a user
    to have same id on multiple devices.
    
    This renames the header and all the supporting constant names, OpenAPI
    spec, and so on.
    
    It also bumps the API version as we're making a breaking change.

--- lib/api/rate_limit.rb
+++ lib/api/rate_limit.rb
@@ -1,6 +1,6 @@
 class Api::RateLimit
   GOVUK_API_USER_READ_THROTTLE_NAME = "read requests to Conversations API with token".freeze
   GOVUK_API_USER_WRITE_THROTTLE_NAME = "write method requests to Conversations API with token".freeze
-  GOVUK_CLIENT_DEVICE_READ_THROTTLE_NAME = "read requests to Conversations API with device id".freeze
-  GOVUK_CLIENT_DEVICE_WRITE_THROTTLE_NAME = "write requests to Conversations API with device id".freeze
+  GOVUK_CLIENT_USER_READ_THROTTLE_NAME = "read requests to Conversations API with user id".freeze
+  GOVUK_CLIENT_USER_WRITE_THROTTLE_NAME = "write requests to Conversations API with user id".freeze
 end
