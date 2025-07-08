91967a83b0d3561fbfe5e15bfe3da136e6fa15eb
Kevin Dew <kevindew@me.com>
2025-07-08 13:04:15 +0100

    Merge pull request #355 from alphagov/claude-as-default
    Switch to using claude_structured_answer as default

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 # It's strongly recommended that you check this file into your version control system.
 
 ActiveRecord::Schema[8.0].define(version: 2025_07_09_105644) do
-  # These are extensions that must be enabled in order to support this database
+ # These are extensions that must be enabled in order to support this database
   enable_extension "citext"
   enable_extension "pg_catalog.plpgsql"
   enable_extension "pgcrypto"
