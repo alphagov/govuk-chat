78565a39b7f22d3b66390f758f358b5a7beca21f
Kevin Dew <kevindew@me.com>
2025-07-11 11:44:54 +0100

    Configure gds-sso for API
    This changes the configuration so that any requests under the /api/
    endpoint are treated as API calls and thus expect only bearer token
    authentication.
    
    As part of this change we now have different signon users used for
    dev/test depending on whether it is session or bearer auth. Because of
    this we now no longer need conversation-api as a permission on the basic
    chat user. We do however need the dummy api user to have the
    conversation-api permission and, for confusing reasons lost to history,
    this is configured in a totally different way to the mock session user.

--- config/initializers/gds_sso.rb
+++ config/initializers/gds_sso.rb
@@ -1,4 +1,7 @@
 GDS::SSO.config do |config|
   config.intercept_401_responses = false
   config.user_model = "SignonUser"
+  config.api_request_matcher = ->(request) { request.path.start_with?("/api/") }
+  # despite the broad name this config option only applies to the dummy bearer token user created in dev/test
+  config.additional_mock_permissions_required = %w[conversation-api]
 end
