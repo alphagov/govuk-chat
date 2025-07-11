7ba372e102273f819a1a9f641c06202206f4f707
James Chan <james.chan@digital.cabinet-office.gov.uk>
2025-07-11 16:39:07 +0100

    Remove call out to 'business' on homepage

--- spec/views/components/_chat_introduction.html.erb_spec.rb
+++ spec/views/components/_chat_introduction.html.erb_spec.rb
@@ -7,7 +7,7 @@ RSpec.describe "components/_chat_introduction.html.erb" do
       .to have_selector(".app-c-chat-introduction")
       .and have_selector(".app-c-chat-introduction-title__svg-container")
       .and have_selector(".app-c-chat-introduction-title__title", text: "Try GOV.UK Chat")
-      .and have_selector(".app-c-chat-introduction-title__lead-paragraph", text: "An experimental new way to find answers to your business questions, powered by AI")
+      .and have_selector(".app-c-chat-introduction-title__lead-paragraph", text: "An experimental new way to find answers to your questions, powered by AI")
       .and have_link("Try GOV.UK Chat")
       .and have_selector("#info-text", text: "Your chat history will be available for #{max_days} days")
   end
