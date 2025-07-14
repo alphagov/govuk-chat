87dd43baa869164d74877564ae632f914ac8411d
David Gisbey <david.gisbey@digital.cabinet-office.gov.uk>
2025-07-14 17:12:17 +0100

    Return model in metrics for OpenAI answer composition steps
    Much like the last commit this updates the rephraser, question router
    and structured answer composition steps to return the model used in the metrics.
    
    It excludes the jailbreak_checker and multiple_checker which will be added
    in the next commit.

--- spec/lib/answer_composition/pipeline/openai/question_router_spec.rb
+++ spec/lib/answer_composition/pipeline/openai/question_router_spec.rb
@@ -104,6 +104,7 @@ RSpec.describe AnswerComposition::Pipeline::OpenAI::QuestionRouter do # rubocop:
         llm_prompt_tokens: 13,
         llm_completion_tokens: 7,
         llm_cached_tokens: 10,
+        model: "gpt-4o-mini-2024-07-18",
       })
     end
 
