ecbaa87de982bf34cb72287db2b930f0a86f9e85
James Chan <james.chan@digital.cabinet-office.gov.uk>
2025-07-16 10:59:39 +0100

    Update homepage
    Update homepage icon and content (as per the design catch up, the homepage content has been updated to match the app)

--- spec/views/components/_chat_introduction_title.html.erb_spec.rb
+++ spec/views/components/_chat_introduction_title.html.erb_spec.rb
@@ -1,12 +1,12 @@
 RSpec.describe "components/_chat_introduction_title.html.erb" do
   it "renders the chat introduction title component correctly" do
     render("components/chat_introduction_title", {
-      title: "Try GOV.UK Chat",
+      title: "Get quick answers from GOV.UK Chatt",
     })
 
     expect(rendered)
       .to have_selector(".app-c-chat-introduction-title")
-      .and have_selector(".app-c-chat-introduction-title__title", text: "Try GOV.UK Chat")
+      .and have_selector(".app-c-chat-introduction-title__title", text: "Get quick answers from GOV.UK Chat")
       .and have_selector(".app-c-chat-introduction-title__svg-container")
       .and have_selector(".app-c-chat-introduction-title__svg")
   end
