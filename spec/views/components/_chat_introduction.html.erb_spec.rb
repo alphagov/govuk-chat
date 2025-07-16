e8d21f449b4e3fdb6758b25e928e2c685194f5c9
James Chan <james.chan@digital.cabinet-office.gov.uk>
2025-07-16 10:59:39 +0100

    Update homepage
    Update homepage icon and content (as per the design catch up, the homepage content has been updated to match the app)

--- spec/views/components/_chat_introduction.html.erb_spec.rb
+++ spec/views/components/_chat_introduction.html.erb_spec.rb
@@ -6,9 +6,9 @@ RSpec.describe "components/_chat_introduction.html.erb" do
     expect(rendered)
       .to have_selector(".app-c-chat-introduction")
       .and have_selector(".app-c-chat-introduction-title__svg-container")
-      .and have_selector(".app-c-chat-introduction-title__title", text: "Try GOV.UK Chat")
-      .and have_selector(".app-c-chat-introduction-title__lead-paragraph", text: "An experimental new way to find answers to your questions, powered by AI")
-      .and have_link("Try GOV.UK Chat")
+      .and have_selector(".app-c-chat-introduction-title__title", text: "Get quick answers from GOV.UK Chat")
+      .and have_selector(".app-c-chat-introduction-title__lead-paragraph", text: "Use GOV.UK's experimental AI tool to easily find out more about topics, services and information on GOV.UK.")
+      .and have_link("Ask a question")
       .and have_selector("#info-text", text: "Your chat history will be available for #{max_days} days")
   end
 end
