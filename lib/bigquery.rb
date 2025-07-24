54fcc91f75ad5a172f72902522fcdec9bc60b320
David Gisbey <david.gisbey@digital.cabinet-office.gov.uk>
2025-07-24 11:43:56 +0100

    Add AnswerAnalysis model to BigQuery export
    We want to be able to export analysis data to BigQuery. This:
    
    - adds an exportable scope to the AnswerAnalysis model
    - adds a serialize_for_export method to the AnswerAnalysis model
    - updates the TABLES_TO_EXPORT constant to include answer analyses

--- lib/bigquery.rb
+++ lib/bigquery.rb
@@ -4,5 +4,6 @@ module Bigquery
   TABLES_TO_EXPORT = [
     ExportTable.new(name: "questions", time_partitioning_field: "created_at"),
     ExportTable.new(name: "answer_feedback", time_partitioning_field: "created_at"),
+    ExportTable.new(name: "answer_analysis", time_partitioning_field: "created_at"),
   ].freeze
 end
