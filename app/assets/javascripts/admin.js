71e259be87e6a622175831b208e46cb6dddd77e5
David Gisbey <david.gisbey@digital.cabinet-office.gov.uk>
2025-08-06 11:30:58 +0100

    Use service navigation component in admin pages
    In the changelog for the govuk_publishing_components gem there is a breaking
    change. [See this PR](https://github.com/alphagov/govuk_publishing_components/pull/4934).
    
    The layout_header menu has been replaced with a service navigation component.
    This means that we need to import the service navigation component in our
    admin stylesheet and JS files, and update our tests to use the new
    components class names.

--- app/assets/javascripts/admin.js
+++ app/assets/javascripts/admin.js
@@ -5,6 +5,7 @@
 //= require govuk_publishing_components/components/layout-header
 //= require govuk_publishing_components/components/table
 //= require govuk_publishing_components/components/radio
+//= require govuk_publishing_components/components/service-navigation
 
 //= require chartkick
 //= require Chart.bundle
--- spec/requests/admin/navigation_spec.rb
+++ spec/requests/admin/navigation_spec.rb
@@ -3,8 +3,9 @@ RSpec.describe "Navigation bar" do
     it "renders an item which links to the Settings when the user has the admin-area-settings permission" do
       login_as(create(:signon_user, :admin_area_settings))
       get admin_homepage_path
+
       expect(response.body)
-        .to have_selector(".govuk-header__navigation-item", text: "Settings")
+        .to have_selector(".govuk-service-navigation__item", text: "Settings")
         .and have_link("Settings", href: admin_settings_path)
     end
 
@@ -12,7 +13,7 @@ RSpec.describe "Navigation bar" do
       login_as(create(:signon_user, :admin))
       get admin_homepage_path
       expect(response.body)
-        .not_to have_selector(".govuk-header__navigation-item", text: "Settings")
+        .not_to have_selector(".govuk-service-navigation__item", text: "Settings")
     end
   end
 end
