749bbc74bf9bf457e7160ea49c7084c40e3ef49c
James Chan <james.chan@digital.cabinet-office.gov.uk>
2025-08-21 10:44:26 +0100

    Remove chat history message on homepage

--- spec/views/components/_chat_introduction.html.erb_spec.rb
+++ spec/views/components/_chat_introduction.html.erb_spec.rb
@@ -2,13 +2,11 @@ RSpec.describe "components/_chat_introduction.html.erb" do
   it "renders the chat introduction component correctly" do
     render("components/chat_introduction")
 
-    max_days = Rails.configuration.conversations.max_question_age_days
     expect(rendered)
       .to have_selector(".app-c-chat-introduction")
       .and have_selector(".app-c-chat-introduction-title__svg-container")
       .and have_selector(".app-c-chat-introduction-title__title", text: "Get quick answers from GOV.UK Chat")
       .and have_selector(".app-c-chat-introduction-title__lead-paragraph", text: "Use GOV.UK's experimental AI tool to easily find out more about topics, services and information on GOV.UK.")
       .and have_link("Ask a question")
-      .and have_selector("#info-text", text: "Your chat history will be available for #{max_days} days")
   end
 end
