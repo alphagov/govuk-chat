fab38e24c24f19820b41b1de3a4883c33f6a001b
Kevin Dew <kevindew@me.com>
2025-08-26 09:19:42 +0100

    Fix missing space in exception message
    Thanks to Jack 🕵🏻‍♂️ for spotting.

--- spec/support/web_mock_not_allowed_override_message.rb
+++ spec/support/web_mock_not_allowed_override_message.rb
@@ -3,7 +3,7 @@ module WebMockNotAllowedMessage
     return super unless ENV["CI"]
 
     "An unregistered HTTP request was made which WebMock raised an exception for.\n\n" \
-    "The details of this exception have been suppressed in CI to prevent" \
+    "The details of this exception have been suppressed in CI to prevent " \
     "public disclosure. Replicate this exception locally for further details."
   end
 end
