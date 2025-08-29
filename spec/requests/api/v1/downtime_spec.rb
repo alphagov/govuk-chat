2d83b59c86ef6aafb91a4b0354c0fd7e16d42e0c
Jack Weeden <jack@ajack.org>
2025-08-29 13:40:55 +0100

    API: Require end user ID header
    This header value is currently optional. If it's set then it'll
    associate a conversation with the user ID specified. If it's not, then
    the conversation is just associated with the signon user.
    
    One DPIA requirement for Chat is that a user can request deletion of
    their data. Without requiring this header value we wouldn't be able to
    find the conversations for a given user. So we need to require it.

--- spec/requests/api/v1/downtime_spec.rb
+++ spec/requests/api/v1/downtime_spec.rb
@@ -1,19 +1,27 @@
 RSpec.describe "toggling downtime with Settings.instance.api_access_enabled" do
   let(:api_user) { create(:signon_user, :conversation_api) }
-  let(:conversation) { create(:conversation, :api, :with_history, signon_user: api_user) }
+  let(:conversation) do
+    create(:conversation, :api, :with_history, signon_user: api_user, end_user_id: "end-user-123")
+  end
 
   before { login_as(api_user) }
 
   it "returns the right status when api_access_enabled is false" do
     create(:settings, api_access_enabled: false)
-    get api_v1_show_conversation_path(conversation)
+    get(
+      api_v1_show_conversation_path(conversation),
+      headers: { "HTTP_GOVUK_CHAT_END_USER_ID" => "end-user-123" },
+    )
     expect(response).to have_http_status(:service_unavailable)
     expect(JSON.parse(response.body)["message"]).to eq("Service unavailable")
   end
 
   it "doesn't impact routes when api_access_enabled is true" do
     create(:settings, api_access_enabled: true)
-    get api_v1_show_conversation_path(conversation)
+    get(
+      api_v1_show_conversation_path(conversation),
+      headers: { "HTTP_GOVUK_CHAT_END_USER_ID" => "end-user-123" },
+    )
     expect(response).to have_http_status(:ok)
   end
 end
