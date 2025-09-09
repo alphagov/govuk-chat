5cbd4e9bd37cac34b668f76dfb5b7f8aefd46a34
Jack Weeden <jack@ajack.org>
2025-09-09 09:32:54 +0100

    Daily Slack notification for chat activity
    When the next trial goes live, we want to post a daily message to Slack
    which shows an overview of the questions asked the previous day. This
    gives everyone visibility of how the app is being used.
    
    This extends the existing SlackPoster class to add a method which posts
    the previous day's Chat activity.
    
    The building of the message is done in a separate class as it's a little
    complex, and was a bit much for a single method in the SlackPoster
    class.
    
    It finds answers created any time during the previous day which have a
    certain status and builds a mesage to post to Slack with links to the
    right place in the admin panel for those answers.

---
+++ app/models/daily_api_activity_message.rb
@@ -0,0 +1,56 @@
+class DailyApiActivityMessage
+  def initialize(date)
+    @date = date
+  end
+
+  def message
+    list_items = []
+
+    statuses.sort_by { |_, v| -v }.each do |status, count|
+      status_config = Rails.configuration.answer_statuses[status]
+      status_text = status_config&.label_and_description || status_config&.label
+      url_text = "#{count} #{status_text}"
+      list_items << "* #{admin_url_markdown(url_text, status)}"
+    end
+
+    <<~MSG.strip
+      Yesterday GOV.UK Chat API received #{total_count} #{total_count == 1 ? 'question' : 'questions'}#{total_count.zero? ? '.' : ':'}
+
+      #{list_items.join("\n")}
+    MSG
+  end
+
+private
+
+  attr_reader :date
+
+  def statuses
+    @statuses ||= Answer.joins(question: :conversation)
+                        .where(created_at: date.beginning_of_day..date.end_of_day)
+                        .where("conversations.source": "api")
+                        .group(:status)
+                        .count
+  end
+
+  def total_count
+    @total_count ||= statuses.values.sum
+  end
+
+  def admin_url_markdown(text, status)
+    date_params = {
+      day: date.day,
+      month: date.month,
+      year: date.year,
+    }
+
+    url = Rails.application.routes.url_helpers.admin_questions_url(
+      source: :api,
+      start_date_params: date_params,
+      end_date_params: date_params,
+      status:,
+      host: Plek.external_url_for(:chat),
+    )
+
+    "[#{text}](#{url})"
+  end
+end
---
+++ spec/models/daily_api_activity_message_spec.rb
@@ -0,0 +1,87 @@
+RSpec.describe DailyApiActivityMessage do
+  describe ".message" do
+    let(:web_conversation) { create(:conversation, source: :web) }
+    let(:api_conversation) { create(:conversation, source: :api) }
+    let(:yesterday) { 1.day.ago }
+
+    def create_answer(status, created_at, conversation)
+      create(:answer, status:, created_at:, question: build(:question, conversation:))
+    end
+
+    def admin_url(status)
+      url_params = {
+        source: :api,
+        start_date_params: { day: yesterday.day, month: yesterday.month, year: yesterday.year },
+        end_date_params: { day: yesterday.day, month: yesterday.month, year: yesterday.year },
+        host: Plek.external_url_for(:chat),
+      }
+
+      Rails.application.routes.url_helpers.admin_questions_url(
+        url_params.merge(status:),
+      )
+    end
+
+    around do |example|
+      travel_to(Time.zone.local(2000, 1, 1, 13, 0, 0)) do
+        example.run
+      end
+    end
+
+    it "builds the message with zero questions" do
+      message = described_class.new(Date.yesterday).message
+      expect(message).to eq("Yesterday GOV.UK Chat API received 0 questions.")
+    end
+
+    it "only includes non-zero question counts" do
+      create_answer(:clarification, yesterday + 2.hours, api_conversation)
+      expected_message = <<~MSG.strip
+        Yesterday GOV.UK Chat API received 1 question:
+
+        * [1 Clarification - question routing requested more information](#{admin_url(:clarification)})
+      MSG
+
+      message = described_class.new(Date.yesterday).message
+      expect(message).to eq(expected_message)
+    end
+
+    def label_for_status(status)
+      Rails.configuration.answer_statuses[status][:label_and_description]
+    end
+
+    it "builds the message with various question counts" do
+      create_answer(:answered, 2.days.ago, api_conversation)
+      create_answer(:answered, 2.days.ago, web_conversation)
+
+      2.times do
+        create_answer(:answered, yesterday + 4.hours, api_conversation)
+      end
+
+      3.times do
+        create_answer(:clarification, yesterday + 2.hours, api_conversation)
+      end
+
+      2.times do
+        create_answer(:unanswerable_no_govuk_content, yesterday + 2.hours, api_conversation)
+      end
+
+      4.times do
+        create_answer(:error_non_specific, yesterday + 4.hours, api_conversation)
+      end
+
+      create_answer(:guardrails_forbidden_terms, yesterday + 4.hours, api_conversation)
+
+      expected_message = <<~MSG.strip
+        Yesterday GOV.UK Chat API received 12 questions:
+
+        * [4 #{label_for_status(:error_non_specific)}](#{admin_url(:error_non_specific)})
+        * [3 #{label_for_status(:clarification)}](#{admin_url(:clarification)})
+        * [2 Answered](#{admin_url(:answered)})
+        * [2 #{label_for_status(:unanswerable_no_govuk_content)}](#{admin_url(:unanswerable_no_govuk_content)})
+        * [1 #{label_for_status(:guardrails_forbidden_terms)}](#{admin_url(:guardrails_forbidden_terms)})
+      MSG
+
+      message = described_class.new(Date.yesterday).message
+      expect(message).to eq(expected_message)
+    end
+  end
+end
