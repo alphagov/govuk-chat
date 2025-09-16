3fc62923b0ced2375831d94b86e9761c813e8316
Chae Cramb <chae.cramb@digital.cabinet-office.gov.uk>
2025-09-16 09:59:54 +0100

    Stop marking documents as ingested when indexing was skipped

--- lib/message_queue/message_processor.rb
+++ lib/message_queue/message_processor.rb
@@ -27,12 +27,13 @@ module MessageQueue
         if base_path_version.payload_version <= payload_version
           result = ContentSynchroniser.call(payload)
           logger.info("#{content_identifier(payload)} synched: #{result}")
-          PrometheusMetrics.gauge(
-            "last_document_ingested",
-            1,
-          )
-
           base_path_version.update!(payload_version:)
+          if result.content_indexed?
+            PrometheusMetrics.gauge(
+              "last_document_ingested",
+              1,
+            )
+          end
         else
           logger.info("#{content_identifier(payload)} ignored as it's older than the last version synched")
         end
