221fac9feb1709de4439612b4d2893963a05b52a
Jack Weeden <jack@ajack.org>
2025-10-09 13:45:18 +0100

    Add rota summary to daily API activity Slack message
    This pulls the rota data out of govuk_chat_private and adds some details
    about the rota to the Slack message we send out each day.

--- spec/models/daily_api_activity_message_spec.rb
+++ spec/models/daily_api_activity_message_spec.rb
@@ -1,5 +1,5 @@
 RSpec.describe DailyApiActivityMessage do
-  describe ".message" do
+  describe "#message" do
     let(:web_conversation) { create(:conversation, source: :web) }
     let(:api_conversation) { create(:conversation, source: :api) }
     let(:yesterday) { 1.day.ago }
@@ -29,61 +29,153 @@ RSpec.describe DailyApiActivityMessage do
       end
     end
 
-    it "builds the message with zero questions" do
-      message = described_class.new(Date.yesterday).message
-      expect(message).to eq("Yesterday GOV.UK Chat API received 0 questions.")
-    end
-
-    it "only includes non-zero question counts" do
-      create_answer(:clarification, yesterday + 2.hours, api_conversation)
-      expected_message = <<~MSG.strip
-        Yesterday GOV.UK Chat API received <#{admin_url}|1 question>:
-
-        - <#{admin_url(:clarification)}|1 Clarification - question routing requested more information>
-      MSG
-
-      message = described_class.new(Date.yesterday).message
-      expect(message).to eq(expected_message)
-    end
-
-    def label_for_status(status)
-      Rails.configuration.answer_statuses[status][:label_and_description]
-    end
-
-    it "builds the message with various question counts" do
-      create_answer(:answered, 2.days.ago, api_conversation)
-      create_answer(:answered, 2.days.ago, web_conversation)
-
-      2.times do
-        create_answer(:answered, yesterday + 4.hours, api_conversation)
+    describe "monitoring rota" do
+      before do
+        allow(Rails.configuration.govuk_chat_private).to receive(:experiment_monitoring_rota).and_return(
+          monitoring_rota_config,
+        )
       end
 
-      3.times do
+      context "when there is no rota config available" do
+        let(:monitoring_rota_config) { nil }
+
+        it "omits the rota summary" do
+          message = described_class.new(Date.yesterday).message
+          expect(message).not_to include("it's your turn to be responsible for monitoring Chat")
+        end
+      end
+
+      context "when there is rota config available for the current day and the following day" do
+        let(:monitoring_rota_config) do
+          {
+            rota: {
+              "2025-01-01" => "Alice",
+              "2025-01-02" => "Bob",
+            },
+            slack_usernames: {
+              "Alice" => "alice",
+              "Bob" => "bob",
+            },
+          }
+        end
+
+        it "includes the rota summary" do
+          message = described_class.new(Date.yesterday).message
+
+          expected_message = <<~MSG.strip
+            Yesterday GOV.UK Chat API received 0 questions.
+
+            Alice (@alice) - it's your turn to be responsible for monitoring Chat.
+            Bob (@bob) - it's your turn tomorrow. Let us know if you're unavailable for your slot so we can find a backup person.
+
+            Guidance on the daily monitoring of Chat can be found <https://docs.google.com/document/d/1OijsFLKh7azOmOFMWlyZWoqW4PNmE-gffXlPE-qito8/edit?tab=t.0|here>.
+          MSG
+
+          expect(message).to eq(expected_message)
+        end
+      end
+
+      context "when there is rota config available for the current day but not the following day" do
+        let(:monitoring_rota_config) do
+          {
+            rota: {
+              "2025-01-01" => "Alice",
+            },
+            slack_usernames: {
+              "Alice" => "alice",
+              "Bob" => "bob",
+            },
+          }
+        end
+
+        it "includes the rota summary for today only" do
+          message = described_class.new(Date.yesterday).message
+          expected_message = <<~MSG.strip
+            Yesterday GOV.UK Chat API received 0 questions.
+
+            Alice (@alice) - it's your turn to be responsible for monitoring Chat.
+
+            Guidance on the daily monitoring of Chat can be found <https://docs.google.com/document/d/1OijsFLKh7azOmOFMWlyZWoqW4PNmE-gffXlPE-qito8/edit?tab=t.0|here>.
+          MSG
+
+          expect(message).to eq(expected_message)
+        end
+      end
+
+      context "when there is no rota config available for the current day" do
+        let(:monitoring_rota_config) do
+          {
+            rota: {
+              "2025-01-02" => "Alice",
+              "2025-01-03" => "Bob",
+            },
+          }
+        end
+
+        it "omits the rota summary" do
+          message = described_class.new(Date.yesterday).message
+          expect(message).not_to include("it's your turn to be responsible for monitoring Chat")
+        end
+      end
+    end
+
+    describe "questions status" do
+      it "builds the message with zero questions" do
+        message = described_class.new(Date.yesterday).message
+        expect(message).to eq("Yesterday GOV.UK Chat API received 0 questions.")
+      end
+
+      it "only includes non-zero question counts" do
         create_answer(:clarification, yesterday + 2.hours, api_conversation)
+        expected_message = <<~MSG.strip
+          Yesterday GOV.UK Chat API received <#{admin_url}|1 question>:
+
+          - <#{admin_url(:clarification)}|1 Clarification - question routing requested more information>
+        MSG
+
+        message = described_class.new(Date.yesterday).message
+        expect(message).to eq(expected_message)
       end
 
-      2.times do
-        create_answer(:unanswerable_no_govuk_content, yesterday + 2.hours, api_conversation)
+      def label_for_status(status)
+        Rails.configuration.answer_statuses[status][:label_and_description]
       end
 
-      4.times do
-        create_answer(:error_non_specific, yesterday + 4.hours, api_conversation)
+      it "builds the message with various question counts" do
+        create_answer(:answered, 2.days.ago, api_conversation)
+        create_answer(:answered, 2.days.ago, web_conversation)
+
+        2.times do
+          create_answer(:answered, yesterday + 4.hours, api_conversation)
+        end
+
+        3.times do
+          create_answer(:clarification, yesterday + 2.hours, api_conversation)
+        end
+
+        2.times do
+          create_answer(:unanswerable_no_govuk_content, yesterday + 2.hours, api_conversation)
+        end
+
+        4.times do
+          create_answer(:error_non_specific, yesterday + 4.hours, api_conversation)
+        end
+
+        create_answer(:guardrails_forbidden_terms, yesterday + 4.hours, api_conversation)
+
+        expected_message = <<~MSG.strip
+          Yesterday GOV.UK Chat API received <#{admin_url}|12 questions>:
+
+          - <#{admin_url(:error_non_specific)}|4 #{label_for_status(:error_non_specific)}>
+          - <#{admin_url(:clarification)}|3 #{label_for_status(:clarification)}>
+          - <#{admin_url(:answered)}|2 Answered>
+          - <#{admin_url(:unanswerable_no_govuk_content)}|2 #{label_for_status(:unanswerable_no_govuk_content)}>
+          - <#{admin_url(:guardrails_forbidden_terms)}|1 #{label_for_status(:guardrails_forbidden_terms)}>
+        MSG
+
+        message = described_class.new(Date.yesterday).message
+        expect(message).to eq(expected_message)
       end
-
-      create_answer(:guardrails_forbidden_terms, yesterday + 4.hours, api_conversation)
-
-      expected_message = <<~MSG.strip
-        Yesterday GOV.UK Chat API received <#{admin_url}|12 questions>:
-
-        - <#{admin_url(:error_non_specific)}|4 #{label_for_status(:error_non_specific)}>
-        - <#{admin_url(:clarification)}|3 #{label_for_status(:clarification)}>
-        - <#{admin_url(:answered)}|2 Answered>
-        - <#{admin_url(:unanswerable_no_govuk_content)}|2 #{label_for_status(:unanswerable_no_govuk_content)}>
-        - <#{admin_url(:guardrails_forbidden_terms)}|1 #{label_for_status(:guardrails_forbidden_terms)}>
-      MSG
-
-      message = described_class.new(Date.yesterday).message
-      expect(message).to eq(expected_message)
     end
   end
 end
