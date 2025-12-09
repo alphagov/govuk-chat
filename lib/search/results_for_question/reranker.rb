0228c20472c819b84d0a5281605c5b5473385946
Jack Weeden <jack@ajack.org>
2025-12-09 11:41:13 +0000

    Reranking: use weights from schema data in config file
    We've merged the document weighting in the schema file into a larger
    config (in the same file) which maps schemas -> document types ->
    weightings.
    
    It's a bit trickier to put this into a constant now so it's better
    suited as a memoized class method.

--- lib/search/results_for_question/reranker.rb
+++ lib/search/results_for_question/reranker.rb
@@ -1,9 +1,21 @@
 module Search
   class ResultsForQuestion
     class Reranker
-      DOCUMENT_TYPE_WEIGHTINGS = Rails.configuration.search.document_type_weightings.to_h.freeze
+      DEFAULT_WEIGHTING = 1.0
 
       def self.call(...) = new(...).call
+
+      def self.document_type_weighting(schema_name, document_type, parent_document_type: nil)
+        document_types = Rails.configuration.search.document_types_by_schema.fetch(schema_name)["document_types"]
+        document_type_config = document_types.fetch(document_type)
+
+        if parent_document_type
+          parent_document_config = document_type_config.fetch("requires_parent_document_type")
+          parent_document_config.fetch(parent_document_type)&.fetch("weight") || DEFAULT_WEIGHTING
+        else
+          (document_type_config || {}).fetch("weight", DEFAULT_WEIGHTING)
+        end
+      end
 
       def initialize(search_results)
         @search_results = search_results
@@ -19,10 +31,11 @@ module Search
 
       def weight_result(result)
         document_type_weight = if result.document_type == "html_publication"
-                                 DOCUMENT_TYPE_WEIGHTINGS.fetch(result.parent_document_type, 1.0)
+                                 self.class.document_type_weighting(result.schema_name, result.document_type, parent_document_type: result.parent_document_type)
                                else
-                                 DOCUMENT_TYPE_WEIGHTINGS.fetch(result.document_type, 1.0)
+                                 self.class.document_type_weighting(result.schema_name, result.document_type)
                                end
+
         Search::ResultsForQuestion::WeightedResult.new(
           result:,
           weighted_score: result.score * document_type_weight,
