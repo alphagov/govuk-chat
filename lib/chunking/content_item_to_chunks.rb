5f586d064741ed60a4972c87131ea8c529d33016
Jack Weeden <jack@ajack.org>
2025-12-09 11:30:43 +0000

    Use parser class name from search config
    Now that we've got the parser name for each schema defined in the
    search.yml config file, we can change this class to use that data.
    
    We just load the config file for that schema name and return the class
    defined within it.

--- lib/chunking/content_item_to_chunks.rb
+++ lib/chunking/content_item_to_chunks.rb
@@ -1,21 +1,11 @@
 module Chunking
   class ContentItemToChunks
-    PARSERS_FOR_SCHEMAS = [
-      ContentItemParsing::BodyContentArrayParser,
-      ContentItemParsing::BodyContentParser,
-      ContentItemParsing::PartsContentParser,
-      ContentItemParsing::StepByStepNavParser,
-      ContentItemParsing::TransactionParser,
-      ContentItemParsing::TravelGuideParser,
-    ].freeze
-
     def self.call(content_item)
       unless supported_content_item?(content_item)
         raise "Content item not supported for parsing: #{non_indexable_content_item_reason(content_item)}"
       end
 
-      parser_class = parsers_by_schema_name[content_item["schema_name"]]
-      parser_class.call(content_item)
+      parser_class(content_item["schema_name"]).call(content_item)
     end
 
     def self.supported_content_item?(content_item)
@@ -24,20 +14,22 @@ module Chunking
 
     def self.non_indexable_content_item_reason(content_item)
       schema_name = content_item["schema_name"]
-      parser = parsers_by_schema_name[schema_name]
 
-      return "#{schema_name} is not a supported schema" if parser.nil?
+      return "#{schema_name} is not a supported schema" unless supported_schema?(schema_name)
+
+      parser = parser_class(schema_name)
+
       return unless parser.respond_to?(:non_indexable_content_item_reason)
 
       parser.non_indexable_content_item_reason(content_item)
     end
 
-    def self.parsers_by_schema_name
-      parser_list = []
-      PARSERS_FOR_SCHEMAS.each do |parser|
-        parser_list += parser.allowed_schemas.map { |schema| [schema, parser] }
-      end
-      parser_list.to_h
+    def self.supported_schema?(schema_name)
+      Rails.configuration.search.document_types_by_schema[schema_name].present?
+    end
+
+    def self.parser_class(schema_name)
+      Rails.configuration.search.document_types_by_schema.fetch(schema_name).parser.constantize
     end
   end
 end
