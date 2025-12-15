503a774d12949e78a60204c3fc45c236f4f5253d
Jack Weeden <jack@ajack.org>
2025-12-15 16:27:13 +0000

    Refactor indexable content item logic
    Previously the logic as to whether a parser could parse a content item
    was included in that parser's class itself.
    
    This commit moves the logic into the `ContentItemToChunks` class
    instead.
    
    The reasoning here is that we can now have this logic in one place so
    it's easier to understand. There was also some duplicate logic in some
    parser classes.
    
    If we wanted to add more parser classes in the future, we'd need to
    duplicate more logic which seems unnecessary.

--- lib/chunking/content_item_to_chunks.rb
+++ lib/chunking/content_item_to_chunks.rb
@@ -14,14 +14,20 @@ module Chunking
 
     def self.non_indexable_content_item_reason(content_item)
       schema_name = content_item["schema_name"]
+      schema_config = Rails.configuration.search.document_types_by_schema[schema_name]
+      return "#{schema_name} is not a supported schema" unless schema_config
 
-      return "#{schema_name} is not a supported schema" unless supported_schema?(schema_name)
+      document_type = content_item["document_type"]
+      return "document type: #{document_type} not supported for schema: #{schema_name}" unless schema_config.document_types.key?(document_type)
 
-      parser = parser_class(schema_name)
+      document_type_config = schema_config.document_types[document_type]
+      return unless document_type_config&.requires_parent_document_type
 
-      return unless parser.respond_to?(:non_indexable_content_item_reason)
+      parent_document_type = content_item.dig("expanded_links", "parent", 0, "document_type")
+      return "content item lacks a parent document_type" unless parent_document_type
 
-      parser.non_indexable_content_item_reason(content_item)
+      supported_parent = document_type_config.requires_parent_document_type.keys.include?(parent_document_type)
+      "#{schema_name} items with parent document type: #{parent_document_type} are not supported" unless supported_parent
     end
 
     def self.supported_schema?(schema_name)
