887db22895daa3c3852f465fc1f0b9e157b85540
David Gisbey <david.gisbey@digital.cabinet-office.gov.uk>
2025-12-22 12:38:33 +0000

    Add Coherence metric
    This adds the Coherence metric to the auto-evaluation module. Much like
    the answer relevancy metric, the Coherence metric uses
    BedrockOpenAIOssInvoke to make a tool call the LLM.
    
    It also returns a result object with the same attributes as the answer
    relevancy metric result object. I'll move this into a shared data class
    in a follow up commit.
    
    There is a difference in score required to normalise the rubric score
    to a 0-1 scale. This has been directly ported from our eval codebase
    and follows these mappings:
    
    1 - 0.0
    2 - 0.25
    3 - 0.5
    4 - 0.75
    5 - 1.0

---
+++ lib/auto_evaluation/coherence.rb
@@ -0,0 +1,60 @@
+module AutoEvaluation
+  class Coherence
+    Result = Data.define(
+      :score,
+      :reason,
+      :success,
+      :llm_responses,
+      :metrics,
+    )
+
+    THRESHOLD = 0.75
+
+    def self.call(...) = new(...).call
+
+    def initialize(question_message:, answer_message:)
+      @question_message = question_message
+      @answer_message = answer_message
+    end
+
+    def call
+      result = BedrockOpenAIOssInvoke.call(user_prompt, tools)
+      score = normalise_rubric_score(result.evaluation_data.fetch("score"))
+
+      Result.new(
+        score:,
+        reason: result.evaluation_data.fetch("reason").strip,
+        success: score >= THRESHOLD,
+        llm_responses: { coherence: result.llm_response },
+        metrics: { coherence: result.metrics },
+      )
+    end
+
+  private
+
+    attr_reader :question_message, :answer_message
+
+    def llm_prompts
+      Prompts.config.coherence
+    end
+
+    def user_prompt
+      sprintf(
+        llm_prompts.fetch(:user_prompt),
+        answer: answer_message,
+        question: question_message,
+      )
+    end
+
+    def tools
+      [llm_prompts.fetch(:tool_spec)]
+    end
+
+    def normalise_rubric_score(rubric_score)
+      min_rubric_score = llm_prompts.fetch(:config).fetch(:min_rubric_score)
+      max_rubric_score = llm_prompts.fetch(:config).fetch(:max_rubric_score)
+
+      (rubric_score.to_d - min_rubric_score) / (max_rubric_score - min_rubric_score)
+    end
+  end
+end
