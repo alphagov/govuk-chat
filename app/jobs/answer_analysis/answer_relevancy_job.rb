6701a6c9a1db4a6b2d7b042c6b611242d9287823
David Gisbey <david.gisbey@digital.cabinet-office.gov.uk>
2026-01-08 13:56:05 +0000

    Update answer relevancy job to use runs instead of aggregate
    This removes references of aggreates from the answer relevancy job and its tests,
    and replaces them with runs, reflecting the updated data model.
    
    We've removed the code that covers runs getting persisted mid-job execution.
    Since the indexes aren't unqiue we're not really worried about the edge
    case of extra runs being created during job execution.

--- app/jobs/answer_analysis/answer_relevancy_job.rb
+++ app/jobs/answer_analysis/answer_relevancy_job.rb
@@ -4,24 +4,16 @@ module AnswerAnalysis
       return unless eligible_for_answer_analysis?(answer_id)
       return if quota_limit_reached?
 
-      answer = Answer.includes(:question, :answer_relevancy_aggregate).find(answer_id)
-      return logger.warn(aggregate_exists_warn_message(answer.id)) if answer.answer_relevancy_aggregate.present?
+      answer = Answer.includes(:question, :answer_relevancy_runs).find(answer_id)
+      if answer.answer_relevancy_runs.present?
+        return logger.warn("Answer #{answer_id} has already been evaluated for relevancy")
+      end
 
       results = NUMBER_OF_RUNS.times.map { AutoEvaluation::AnswerRelevancy.call(answer) }
 
-      begin
-        AnswerAnalysis::AnswerRelevancyRun.create_runs_from_score_results(
-          answer, results, :answer_relevancy_runs
-        )
-      rescue ActiveRecord::RecordNotUnique
-        logger.warn(aggregate_exists_warn_message(answer.id))
-      end
-    end
-
-  private
-
-    def aggregate_exists_warn_message(answer_id)
-      "Answer #{answer_id} has already been evaluated for relevancy"
+      AnswerAnalysis::AnswerRelevancyRun.create_runs_from_score_results(
+        answer, results, :answer_relevancy_runs
+      )
     end
   end
 end
