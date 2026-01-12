ae0435b341f41268fbf97cdaad2692279c4637fd
David Gisbey <david.gisbey@digital.cabinet-office.gov.uk>
2026-01-12 17:22:28 +0000

    Add answer_count counter to PrometheusMetrics
    We're going to alert when we generate an elevated number of answers with
    error statuses, so we need to add a counter metric to track the number of
    answers created, labelled by their status.

--- lib/prometheus_metrics.rb
+++ lib/prometheus_metrics.rb
@@ -31,10 +31,23 @@ class PrometheusMetrics
     },
   ].freeze
 
+  COUNTERS = [
+    {
+      name: "answer_count",
+      description: "The total number of answers",
+    },
+  ].freeze
+
   def self.register
     GAUGES.each do |gauge|
       PrometheusExporter::Client.default.register(
         :gauge, name_with_prefix(gauge[:name]), gauge[:description]
+      )
+    end
+
+    COUNTERS.each do |counter|
+      PrometheusExporter::Client.default.register(
+        :counter, name_with_prefix(counter[:name]), counter[:description]
       )
     end
   end
@@ -48,6 +61,17 @@ class PrometheusMetrics
 
     metric = PrometheusExporter::Client.default.find_registered_metric(name_with_prefix(name))
     metric.observe(value, labels)
+  end
+
+  def self.increment_counter(name, labels = {})
+    if COUNTERS.none? { |counter| counter[:name] == name }
+      error = "#{name} is not defined in PrometheusMetrics::COUNTERS"
+      Rails.env.production? ? GovukError.notify(error) : (raise error)
+      return
+    end
+
+    metric = PrometheusExporter::Client.default.find_registered_metric(name_with_prefix(name))
+    metric.observe(1, labels)
   end
 
   def self.name_with_prefix(name)
