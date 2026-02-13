2bbaca35a210bc66f36122a6c5d43d4e2413b2e6
David Gisbey <david.gisbey@digital.cabinet-office.gov.uk>
2026-02-13 09:25:28 +0000

    Update conversation_session_id to be non-nullable
    Now we've backfilled conversation_session_id for all existing questions,
    we can make the column non-nullable to ensure all future questions have a
    conversation_session_id.

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[8.0].define(version: 2026_02_12_115413) do
+ActiveRecord::Schema[8.0].define(version: 2026_02_13_092349) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "citext"
   enable_extension "pg_catalog.plpgsql"
