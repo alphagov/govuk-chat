09da5320af4a359330e05f1b76903b3bded7dbed
David Gisbey <david.gisbey@digital.cabinet-office.gov.uk>
2026-03-24 11:57:08 +0000

    Fix issues with question show page related to error statuses
    It's unlikely that all runs with be an error status, but in the case
    that they are there would be an issue with the mean score calculation
    scoring an error.
    
    Instead of setting it to nil I think it makes more sense to just conditionally
    render the row based on there being at least one run with a non-error status.
    
    Secondly, there's a potential issue where we render metrics and llm repsponses
    when there are none.
    
    If all 3 runs have no llm responses or metrics then we shouldn't render
    the section at all.
    
    At first i checked to see if all the runs had an error status, but then
    realised there's actually some situations when error statuses have llm
    responses and metrics. Consider the following:
    
    1. the answer relevancy run finds, statements and verdicts, but fails on
    the reasoning call.
    2. the BedrockOpenAIOssInvoke client throws an InvalidToolCallError
    3. the answer relevancy rescue catches the error and creates a run with an
    error status and the current llm responses and metrics.
    
    That model will have an error status, but it will also have llm responses
    and metrics that we want to render.
    
    Instead we can just check if any of the runs have llm responses or metrics
    before rendering those sections.
    
    For each run we can then check to see if there are llm responses or metrics before
    trying to render them.

--- spec/factories/answer_relevancy_run_factory.rb
+++ spec/factories/answer_relevancy_run_factory.rb
@@ -4,5 +4,12 @@ FactoryBot.define do
     score { 0.5 }
     status { "success" }
     reason { "The answer was okay." }
+
+    trait :with_error do
+      status { "error" }
+      reason { nil }
+      score { nil }
+      error_message { "An error occurred during evaluation." }
+    end
   end
 end
