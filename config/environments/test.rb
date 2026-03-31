81b641cbd9d3ed4bab256d4c4bb849430adac9ba
David Gisbey <david.gisbey@digital.cabinet-office.gov.uk>
2026-03-31 14:00:08 +0100

    run rails app:update
    This follows the guidance on how to update a Rails app:
    https://docs.publishing.service.gov.uk/manual/how-to-upgrade-rails.html.
    
    I've chosen to add in the bin/ci and config/ci.rb files despite us having
    our own CI setup. I could definitely remove them though.
    
    I've retained the
    
    ```
    "config.assets.compile = false"
    "config.assets.quiet = true"
    ```
    
    values since it looks like they've been removed under the assumption
    that the application is using Propshaft:
    https://github.com/rails/rails/commit/0f43feda04d45aec064aedc312d265a529e4915d

--- config/environments/test.rb
+++ config/environments/test.rb
@@ -15,12 +15,11 @@ Rails.application.configure do
   # loading is working properly before deploying your code.
   config.eager_load = ENV["CI"].present?
 
-  # Configure public file server for tests with Cache-Control for performance.
+  # Configure public file server for tests with cache-control for performance.
   config.public_file_server.headers = { "cache-control" => "public, max-age=3600" }
 
   # Don't show error reports and disable caching.
   config.consider_all_requests_local = false
-  config.action_controller.perform_caching = false
   config.cache_store = :null_store
 
   # Render exception templates for rescuable exceptions and raise for other exceptions.
@@ -28,14 +27,6 @@ Rails.application.configure do
 
   # Disable request forgery protection in test environment.
   config.action_controller.allow_forgery_protection = false
-
-  # Tell Action Mailer not to deliver emails to the real world.
-  # The :test delivery method accumulates sent emails in the
-  # ActionMailer::Base.deliveries array.
-  # config.action_mailer.delivery_method = :test
-
-  # Set host to be used by links generated in mailer templates.
-  # config.action_mailer.default_url_options = { host: "example.com" }
 
   # Print deprecation notices to the stderr.
   config.active_support.deprecation = :stderr
@@ -46,7 +37,7 @@ Rails.application.configure do
   # Annotate rendered view with file names.
   # config.action_view.annotate_rendered_view_with_filenames = true
 
-  # Raise error when a before_action's only/except options reference missing actions
+  # Raise error when a before_action's only/except options reference missing actions.
   config.action_controller.raise_on_missing_callback_actions = true
 
   # Use a hardcoded config value for testing
