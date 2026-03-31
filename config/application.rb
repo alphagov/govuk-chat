c4dc0e4753329a3e4b3d0918be3ef927afe3d4c7
David Gisbey <david.gisbey@digital.cabinet-office.gov.uk>
2026-03-31 14:20:17 +0100

    Set load_defaults to 8.1
    Since the tests are passing with the new defaults we can remove the
    new_framework_defaults initializer and set the load_defaults to 8.1.

--- config/application.rb
+++ config/application.rb
@@ -29,7 +29,7 @@ Dotenv::Rails.files << ".env.aws.local" if Rails.env.development?
 module GovukChat
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
-    config.load_defaults 8.0
+    config.load_defaults 8.1
 
     # Please, add to the `ignore` list any other `lib` subdirectories that do
     # not contain `.rb` files, or that should not be reloaded or eager loaded.
