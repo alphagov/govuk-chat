5fda5fb49d2b7f20a6444aa6113189efe7329b45
David Gisbey <david.gisbey@digital.cabinet-office.gov.uk>
2026-04-23 12:23:29 +0100

    Switch the QuestionRephraser default model to Sonnet 4.5
    This updates the QuestionRephraser to set the default model to `:claude_sonnet_4_5`
    and updates the stubs to reflect this change.

--- lib/answer_composition/pipeline/question_rephraser.rb
+++ lib/answer_composition/pipeline/question_rephraser.rb
@@ -4,7 +4,7 @@ module AnswerComposition
       Result = Data.define(:llm_response, :rephrased_question, :metrics)
 
       SUPPORTED_MODELS = %i[claude_sonnet_4_0 claude_sonnet_4_5].freeze
-      DEFAULT_MODEL = :claude_sonnet_4_0
+      DEFAULT_MODEL = :claude_sonnet_4_5
 
       def self.call(...) = new(...).call
 
