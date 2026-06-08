8724a0f67b821a0025131a2b98a2bf0044a74bd5
David Gisbey <david.gisbey@digital.cabinet-office.gov.uk>
2026-06-08 11:56:54 +0100

    Remove remaining references to Sonnet 4.0 from answer pipeline
    This commit removes the remaining references in the Answer Composition
    pipeline to Sonnet 4.0.
    
    I've updated these tests to use a contrived model so that it
    doesn't need to be updated every time we remove a model from BedrockModels.

--- spec/lib/answer_composition/pipeline/prompts_spec.rb
+++ spec/lib/answer_composition/pipeline/prompts_spec.rb
@@ -5,20 +5,26 @@ RSpec.describe AnswerComposition::Pipeline::Prompts do
         system_prompt: "The system prompt",
         user_prompt: "The user prompt ",
       }
-      allow(Rails.configuration.govuk_chat_private.llm_prompts.answer_composition).to receive(:fetch).with(:test_prompt).and_return({ claude_sonnet_4_0: config })
+      allow(Rails.configuration
+                 .govuk_chat_private
+                 .llm_prompts.answer_composition)
+                 .to receive(:fetch).with(:test_prompt).and_return({ claude_model: config })
 
-      result = described_class.config(:test_prompt, :claude_sonnet_4_0)
+      result = described_class.config(:test_prompt, :claude_model)
 
       expect(result).to eq(config)
     end
 
     it "raises an error if no prompt configuration is found for the given component name" do
-      expect { described_class.config(:non_existent_component, :claude_sonnet_4_0) }
+      expect { described_class.config(:non_existent_component, :claude_model) }
         .to raise_error("No LLM prompts found for non_existent_component")
     end
 
     it "raises an error if no prompt configuration is found for the given model" do
-      allow(Rails.configuration.govuk_chat_private.llm_prompts.answer_composition).to receive(:fetch).with(:test_prompt).and_return({ claude_sonnet_4_0: {} })
+      allow(Rails.configuration
+                 .govuk_chat_private
+                 .llm_prompts.answer_composition)
+                 .to receive(:fetch).with(:test_prompt).and_return({ claude_model: {} })
 
       expect { described_class.config(:test_prompt, :non_existent_model) }
       .to raise_error("No LLM prompts found for the non_existent_model model in the test_prompt prompt configuration")
